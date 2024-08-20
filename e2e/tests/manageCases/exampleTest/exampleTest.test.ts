1c719f0cccdd34037dbc6ec0c947c720cd7c4300
Jake Gowler <jakecgowler@gmail.com>
2024-08-20 11:56:07 +0100

    Final change

--- e2e/tests/manageCases/exampleTest/exampleTest.test.ts
+++ e2e/tests/manageCases/exampleTest/exampleTest.test.ts
@@ -1,7 +1,7 @@
 import { test } from "@playwright/test";
 import { ExampleJourney } from "../../../journeys/manageCases/exampleEvent/exampleJourney";
 
-test.describe("Manage cases example tests. @manageCases", () => {
+test.describe("Manage cases example tests. @manageCases", (): void => {
   test("Manage cases example test with event name and parameters. @crossbrowserManageCases", async ({
     page,
   }): Promise<void> => {
