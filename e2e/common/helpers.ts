0796b3d98eb9ee7dc7a35c9ae8ea954a8701dfa6
Ollie Thurston <65600338+OLT2000@users.noreply.github.com>
2024-09-05 15:58:23 +0100

    Casenumber regex helper (#5)
    * Added Helper: Case Number Regex

--- e2e/common/helpers.ts
+++ e2e/common/helpers.ts
@@ -1,6 +1,7 @@
 import { expect, Locator, Page } from "@playwright/test";
 import { Events, UserRole } from "./types";
 import idamLoginHelper from "./idamLoginHelper";
+import { Selectors } from "./selectors.ts";
 
 export class Helpers {
   public static async chooseEventFromDropdown(
@@ -153,5 +154,32 @@ export class Helpers {
       throw new Error("Month index out of range");
     }
     return Helpers.months[index - 1].substring(0, 3);
+  }
+
+  private static async checkCaseNumberRegex(page: Page): Promise<void> {
+    const caseNumberRegex = /^Casenumber: \d{4}-\d{4}-\d{4}-\d{4}$/;
+    try {
+      const visibilityPromises: Promise<void>[] = Array.from(
+        { length: 1 },
+        (_, i: number) =>
+          expect
+            .soft(
+              page
+                .locator(`${Selectors.h2}`, { hasText: caseNumberRegex })
+                .nth(i),
+            )
+            .toBeVisible(),
+      );
+      const countPromise: Promise<void> = expect
+        .soft(page.locator(`${Selectors.h2}`, { hasText: caseNumberRegex }))
+        .toHaveCount(1);
+      await Promise.all([...visibilityPromises, countPromise]);
+    } catch (error) {
+      console.error(
+        `An error occurred while checking visibility and accuracy of the case number heading:`,
+        error,
+      );
+      throw error;
+    }
   }
 }
