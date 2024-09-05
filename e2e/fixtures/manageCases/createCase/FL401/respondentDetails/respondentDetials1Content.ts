8e2f18bd5b3f5e9133897f388218c97ab35050a3
louisware <louis.ware@solirius.com>
2024-09-05 11:04:01 +0100

    Add respondentDetails1Content.ts

---
+++ e2e/fixtures/manageCases/createCase/FL401/respondentDetails/respondentDetials1Content.ts
@@ -0,0 +1,19 @@
+export enum RespondentDetails1Content {
+  pageTitle = "Respondent details",
+  // subTitle = "Confidentiality Statement",
+  p1 = "You can save and return to this page at any time. Questions marked with a * need to be completed before you can send your application.",
+  p2 = "*First name(s) (Optional)",
+  p3 = "*Last name (Optional)",
+  p4 = "Previous name (if any) (Optional)",
+  p5 = "*Is date of birth known? (Optional)",
+  p6 = "*Does the respondent live with the applicant? (Optional)",
+  p7 = "*Is current address known? (Optional)",
+  p8 = "*Can you provide email address? (Optional)",
+  p9 = "*Email address (Optional)",
+  p10 = "*Can you provide a contact number? (Optional)",
+
+  continue = "Continue",
+
+  errorBanner = "There is a problem",
+  errorText = "Field is required",
+}
