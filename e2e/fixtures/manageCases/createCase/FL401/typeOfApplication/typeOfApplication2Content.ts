881d1dff8548a02d8de2f4b21d3f09d8d1ecbc63
OLT2000 <olliethurston1@gmail.com>
2024-09-05 09:51:25 +0100

    PRL-6212: Added content for second page

---
+++ e2e/fixtures/manageCases/createCase/FL401/typeOfApplication/typeOfApplication2Content.ts
@@ -0,0 +1,12 @@
+export enum TypeOfApplication2ContentFL401 {
+  pageTitle = 'Type of Application',
+  familyManHeading = 'FamilyMan ID:',
+  caseNumberHeading = 'Casenumber: ',
+  p1 = 'You can save and return to this page at any time. Questions marked with a * need to be completed before you can send your application.',
+  formLabel1 = '*Is this linked to a C100 application? (Optional)',
+  formLabel2 = 'Yes',
+  formLabel3 = 'No',
+
+  pLinkedC100 = "If you have also completed a Child Arrangements Order application enter the case number below.",
+  formLabelLinkedC100 = "Child Arrangements Case Number (FamilyMan cases not supported) (Optional)",
+}
