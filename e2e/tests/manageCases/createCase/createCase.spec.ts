03f8ec79f67e97e75fd160f00478f805d5a571ab
OLT2000 <olliethurston1@gmail.com>
2024-09-05 11:06:35 +0100

    PRL-6212: Updated spec to use FL401

--- e2e/tests/manageCases/createCase/createCase.spec.ts
+++ e2e/tests/manageCases/createCase/createCase.spec.ts
@@ -1,5 +1,6 @@
 import { test } from "@playwright/test";
 import { SolicitorCreateInitial } from "../../../journeys/manageCases/createCase/solicitorCreateInitial";
+import { FL401 } from "../../../journeys/manageCases/createCase/FL401";
 
 test.describe("Manage cases case solicitor create case tests. @manageCases", (): void => {
   test("Create a C100 case. @crossbrowserManageCases", async ({
@@ -17,13 +18,13 @@ test.describe("Manage cases case solicitor create case tests. @manageCases", ():
   test("Create a FL401 case. @crossbrowserManageCases", async ({
     page,
   }): Promise<void> => {
-    await SolicitorCreateInitial.createInitialCase(
+    await FL401.fl401(
       page,
       "solicitor",
       false,
       "FL401",
-      false,
-    );
+      false
+    )
   });
 
   test("Check the errors of a C100 solicitor create journey. @crossbrowserManageCases", async ({
