4b00b6969c88a38fc1aabfc7ef3368fa9cfb0291
OLT2000 <olliethurston1@gmail.com>
2024-09-05 09:52:16 +0100

    PRL-6212: Added submit Content file

---
+++ e2e/fixtures/manageCases/createCase/FL401/typeOfApplication/submitContent.ts
@@ -0,0 +1,13 @@
+export enum SubmitContent {
+  pageTitle = "Type of application",
+  familyManHeading = 'FamilyMan ID:',
+  caseNumberHeading = 'Casenumber: ',
+  h2 = "Check your answers",
+  text161 = "Check the information below carefully.",
+  text162 = "Case Name",
+  text163 = "Change",
+
+  previousButton = 'Previous',
+  saveAndContinue = "Save and continue",
+  cancelLink = 'Cancel'
+}
