43af849e47aaec53e85236021ddb4c189e1d5b9c
OLT2000 <olliethurston1@gmail.com>
2024-09-05 11:06:27 +0100

    PRL-6212: Added private method for clicking link

--- e2e/journeys/manageCases/createCase/FL401.ts
+++ e2e/journeys/manageCases/createCase/FL401.ts
@@ -1,23 +1,45 @@
-import { solicitorCaseCreateType, UserRole } from "../../../common/types";
+import {solicitorCaseCreateType, UserRole } from "../../../common/types";
 import { Page } from "@playwright/test";
 import { SolicitorCreateInitial } from "./solicitorCreateInitial";
+import {
+  TypeOfApplication1Page
+} from "../../../pages/manageCases/createCase/FL401/typeOfApplication/typeOfApplication1Page";
+import { Selectors } from "../../../common/selectors";
 
 export class FL401 {
   public static async fl401 (
     page: Page,
     user: UserRole,
     accessibilityTest: boolean,
-    solicitorCaseType: solicitorCaseCreateType,
+    solicitorCaseType: solicitorCaseCreateType = "FL401",
     errorMessaging: boolean,
   ): Promise<void> {
     await SolicitorCreateInitial.createInitialCase(
       page,
-      "solicitor",
-      false,
-      "FL401",
-      false,
+      user,
+      accessibilityTest,
+      solicitorCaseType,
+      errorMessaging,
     );
 
+    await this.navigateToEvent(
+      page,
+      "Type of application"
+    )
+
+    await TypeOfApplication1Page.typeOfApplication1Page(
+      page,
+      errorMessaging,
+      accessibilityTest
+    );
   }
+
+  private static async navigateToEvent(
+    page: Page,
+    linkText: string
+  ): Promise<void> {
+    let linkSelector = `a:text-is("${linkText}")`;
+    await page.click(linkSelector);
+  };
 
 }
