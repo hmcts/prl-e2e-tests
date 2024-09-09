50e74392665f00a80b2b7cc6208f262a9a81dda2
OLT2000 <olliethurston1@gmail.com>
2024-09-09 16:21:58 +0100

    PRL-6212: Updated Tasks Tab Page to fit into end of flows

--- e2e/journeys/manageCases/createCase/FL401.ts
+++ e2e/journeys/manageCases/createCase/FL401.ts
@@ -2,7 +2,6 @@ import { UserRole } from "../../../common/types";
 import { Page } from "@playwright/test";
 import { FL401TypeOfApplication } from "./FL401TypeOfApplication/FL401TypeOfApplication";
 import { SolicitorCreateInitial } from "./solicitorCreateInitial";
-import { Fl401TasksTabPage } from "../../../pages/manageCases/caseTabs/fl401TasksTabPage";
 
 export class FL401 {
   public static async fl401(
@@ -19,7 +18,6 @@ export class FL401 {
       "FL401",
       errorMessaging,
     );
-    await Fl401TasksTabPage.fl401TasksTabPage(page, accessibilityTest);
     await FL401TypeOfApplication.fl401TypeOfApplication(
       page,
       accessibilityTest,
