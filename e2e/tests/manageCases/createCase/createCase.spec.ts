9d342a93ed01aaa57867a01ddc30211d14a68b0b
JakeCG <jakecgowler@gmail.com>
2024-09-09 15:00:47 +0100

    Adding static checks for FL401 landing page (#11)

--- e2e/tests/manageCases/createCase/createCase.spec.ts
+++ e2e/tests/manageCases/createCase/createCase.spec.ts
@@ -1,34 +1,30 @@
 import { test } from "@playwright/test";
-import { SolicitorCreateInitial } from "../../../journeys/manageCases/createCase/solicitorCreateInitial";
 import { C100 } from "../../../journeys/manageCases/createCase/C100";
+import { FL401 } from "../../../journeys/manageCases/createCase/FL401";
 
 test.describe("Manage cases case solicitor create case tests. @manageCases", (): void => {
-  test("Create a C100 case with the following options:" +
-    "Hearing urgency: yes to all values " +
-    "@crossbrowserManageCases", async ({
-    page,
-  }): Promise<void> => {
-    await C100.c100(page, "solicitor", false, false, true);
-  });
+  test(
+    "Create a C100 case with the following options:" +
+      "Hearing urgency: yes to all values " +
+      "@crossbrowserManageCases",
+    async ({ page }): Promise<void> => {
+      await C100.c100(page, "solicitor", false, false, true);
+    },
+  );
 
-  test("Create a C100 case with the following options:" +
-    "Hearing urgency: no to all values " +
-    "@crossbrowserManageCases", async ({
-    page,
-  }): Promise<void> => {
-    await C100.c100(page, "solicitor", false, false, false);
-  });
+  test(
+    "Create a C100 case with the following options:" +
+      "Hearing urgency: no to all values " +
+      "@crossbrowserManageCases",
+    async ({ page }): Promise<void> => {
+      await C100.c100(page, "solicitor", false, false, false);
+    },
+  );
 
   test("Create a FL401 case. @crossbrowserManageCases", async ({
     page,
   }): Promise<void> => {
-    await SolicitorCreateInitial.createInitialCase(
-      page,
-      "solicitor",
-      false,
-      "FL401",
-      false,
-    );
+    await FL401.fl401(page, "solicitor", false, "FL401", false);
   });
 
   test("Check the errors of a C100 solicitor create journey. @crossbrowserManageCases", async ({
@@ -40,13 +36,7 @@ test.describe("Manage cases case solicitor create case tests. @manageCases", ():
   test("Check the errors of a FL401 solicitor create journey. @crossbrowserManageCases", async ({
     page,
   }): Promise<void> => {
-    await SolicitorCreateInitial.createInitialCase(
-      page,
-      "solicitor",
-      false,
-      "FL401",
-      true,
-    );
+    await FL401.fl401(page, "solicitor", false, "FL401", true);
   });
 });
 
@@ -59,11 +49,5 @@ test("Check the accessibility of a C100 solicitor create journey. @accessibility
 test("Check the accessibility of a FL401 solicitor create journey. @accessibilityManageCases", async ({
   page,
 }): Promise<void> => {
-  await SolicitorCreateInitial.createInitialCase(
-    page,
-    "solicitor",
-    true,
-    "FL401",
-    false,
-  );
+  await FL401.fl401(page, "solicitor", true, "FL401", false);
 });
