8451179f2a0440f228d0235345edbb08fafb7c0a
Jake Gowler <jakecgowler@gmail.com>
2024-09-09 09:19:16 +0100

    adding taskTabPage

--- e2e/journeys/manageCases/createCase/C100.ts
+++ e2e/journeys/manageCases/createCase/C100.ts
@@ -2,6 +2,7 @@ import { UserRole } from "../../../common/types";
 import { Page } from "@playwright/test";
 import { SolicitorCreateInitial } from "./solicitorCreateInitial";
 import { C100HearingUrgency } from "./C100HearingUrgency/C100HearingUrgency";
+import { C100TasksTabPage } from "../../../pages/manageCases/caseTabs/c100TasksTabPage";
 
 export class C100 {
   public static async c100(
@@ -18,6 +19,7 @@ export class C100 {
       "C100",
       false,
     );
+    await C100TasksTabPage.c100TasksTabPage(page, accessibilityTest);
     await C100HearingUrgency.c100HearingUrgency(
       page,
       "solicitor",
