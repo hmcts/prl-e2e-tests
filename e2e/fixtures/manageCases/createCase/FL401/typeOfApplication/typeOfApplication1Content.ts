c51cd5855f1d9e23b0c73bff5659185090057ad6
OLT2000 <olliethurston1@gmail.com>
2024-09-09 10:40:11 +0100

    PRL-6212: Removed unneeded content

--- e2e/fixtures/manageCases/createCase/FL401/typeOfApplication/typeOfApplication1Content.ts
+++ e2e/fixtures/manageCases/createCase/FL401/typeOfApplication/typeOfApplication1Content.ts
@@ -1,7 +1,5 @@
 export enum TypeOfApplication1Content {
   pageTitle = "Type of application",
-  familyManHeading = "FamilyMan ID:",
-  caseNumberHeading = "Casenumber: ",
   p1 = "You can save and return to this page at any time. Questions marked with a * need to be completed before you can send your application.",
   formLabel1 = "*Which order(s) are you applying for?",
   formLabel2 = "Non-molestation order",
