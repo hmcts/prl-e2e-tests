62d6901fca525c1cc03a1330ca962dfe2e95c421
OLT2000 <olliethurston1@gmail.com>
2024-09-12 16:50:35 +0100

    PRL-6229: Updated submit page

---
+++ e2e/fixtures/manageCases/createCase/FL401/withoutNoticeOrder/withoutNoticeOrderSubmitContent.ts
@@ -0,0 +1,9 @@
+export enum WithoutNoticeOrderSubmitContent {
+  pageTitle = 'Without notice order',
+  pageHeading = 'Check your answers',
+  checkInfo =  'Check the information below carefully.',
+  continue = ' Save and continue ',
+  text16No1 = '*Do you want to apply for the order without giving notice to the respondent?',
+  text16No2 = 'No',
+  textChange = ' Change '
+}
