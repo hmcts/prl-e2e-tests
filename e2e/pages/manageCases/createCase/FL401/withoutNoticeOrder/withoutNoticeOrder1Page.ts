996a665de0d74db0c1e26e63053ae7f801f28416
OLT2000 <olliethurston1@gmail.com>
2024-09-12 16:50:25 +0100

    PRL-6229: First Page Tests with selection No

---
+++ e2e/pages/manageCases/createCase/FL401/withoutNoticeOrder/withoutNoticeOrder1Page.ts
@@ -0,0 +1,108 @@
+import { Page } from "@playwright/test";
+import { Selectors } from "../../../../../common/selectors";
+import {
+  WithoutNoticeOrderDetails1Content
+} from "../../../../../fixtures/manageCases/createCase/FL401/withoutNoticeOrder/withoutNoticeOrderDetails1Content";
+import { Helpers } from "../../../../../common/helpers";
+import accessibilityTestHelper from "../../../../../common/accessibilityTestHelper";
+
+enum WithoutNoticeOrderRadioIDs {
+  radioYes = '#orderWithoutGivingNoticeToRespondent_orderWithoutGivingNotice_Yes',
+  radioNo = '#orderWithoutGivingNoticeToRespondent_orderWithoutGivingNotice_No'
+}
+
+export class WithoutNoticeOrder1Page{
+  public static async withoutOrderNotice1Page(
+    page: Page,
+    accessibilityTest: boolean,
+    errorMessaging: boolean,
+    isWithoutNotice: boolean
+  ): Promise<void> {
+    await this.checkPageLoads(page, accessibilityTest);
+    if (errorMessaging) {
+      await this.checkErrorMessaging(page)
+    }
+    await this.fillInFields(page, isWithoutNotice);
+  }
+
+  private static async checkPageLoads(
+    page: Page,
+    accessibilityTest: boolean
+  ): Promise<void> {
+    await page.waitForSelector(
+      `${Selectors.GovukHeadingL}:text-is("${WithoutNoticeOrderDetails1Content.pageTitle}")`
+    );
+    await Promise.all(
+      [
+        Helpers.checkVisibleAndPresent(
+          page,
+          `${Selectors.p}:text-is("${WithoutNoticeOrderDetails1Content.p1}")`,
+          1
+        ),
+        Helpers.checkVisibleAndPresent(
+          page,
+          `${Selectors.GovukFormLabel}:text-is("${WithoutNoticeOrderDetails1Content.formLabel}")`,
+          1
+        ),
+        Helpers.checkGroup(
+          page,
+          2,
+          WithoutNoticeOrderDetails1Content,
+          'formControl',
+          `${Selectors.GovukFormLabel}`
+        )
+      ]
+    )
+    if (accessibilityTest) {
+      await accessibilityTestHelper.run(
+        page
+      )
+    }
+  }
+
+  private static async checkErrorMessaging(
+    page: Page
+  ): Promise<void> {
+    await page.click(
+      `${Selectors.button}:text-is("${WithoutNoticeOrderDetails1Content.continue}")`
+    )
+    await Promise.all(
+      [
+        Helpers.checkVisibleAndPresent(
+          page,
+          `${Selectors.GovukErrorSummary}:text-is("${WithoutNoticeOrderDetails1Content.errorSummary}")`,
+          1
+        ),
+        Helpers.checkVisibleAndPresent(
+          page,
+          `${Selectors.GovukErrorValidation}:text-is("${WithoutNoticeOrderDetails1Content.validationError}")`,
+          1
+        ),
+        Helpers.checkVisibleAndPresent(
+          page,
+          `${Selectors.GovukErrorMessage}:text-is("${WithoutNoticeOrderDetails1Content.errorMessage}")`,
+          1
+        ),
+      ]
+    )
+  }
+
+  private static async fillInFields(
+    page: Page,
+    isWithoutNotice: boolean
+  ): Promise<void> {
+    if (isWithoutNotice) {
+      await page.click(
+        WithoutNoticeOrderRadioIDs.radioYes
+      );
+    } else {
+      await page.click(
+
+        WithoutNoticeOrderRadioIDs.radioNo
+      );
+    }
+    await page.click(
+      `${Selectors.button}:text-is("${WithoutNoticeOrderDetails1Content.continue}")`
+    );
+  }
+}
