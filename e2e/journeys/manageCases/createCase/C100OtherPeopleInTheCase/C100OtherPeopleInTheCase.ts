1a05a62e054425225e77eeabd195326ebdcd1b09
scralfie <kellyalfie6@gmail.com>
2024-09-18 14:43:09 +0100

    ticket: PRL-6219 - Starting the Journey

--- e2e/journeys/manageCases/createCase/C100OtherPeopleInTheCase/C100OtherPeopleInTheCase.ts
+++ e2e/journeys/manageCases/createCase/C100OtherPeopleInTheCase/C100OtherPeopleInTheCase.ts
@@ -1,7 +1,6 @@
 import { Page } from "@playwright/test";
 import { ApplicantGender } from "../../../../pages/manageCases/createCase/C100/applicantDetails/applicantDetails1Page";
 
-
 interface c100OtherPeopleInTheCase {
   page: Page;
   // user: userRole;
