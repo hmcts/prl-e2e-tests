9d6cae08158f122040094eddf0244daf28a55126
scralfie <kellyalfie6@gmail.com>
2024-09-20 11:33:46 +0100

    ticket: PRL-6251 - Dirs and files created. Page 1 content complete and started page1 logic

---
+++ e2e/pages/manageCases/createCase/C100/childrenAndRespondents/childrenAndRespondents1Page.ts
@@ -0,0 +1,82 @@
+import { Page } from "@playwright/test";
+import { Selectors } from "../../../../../common/selectors";
+import { ChildrenAndRespondents1Content } from "../../../../../fixtures/manageCases/createCase/C100/childrenAndRespondents/childrenAndRespondents1Content";
+import { Helpers } from "../../../../../common/helpers";
+import AccessibilityTestHelper from "../../../../../common/accessibilityTestHelper";
+
+enum UniqueSelectors {}
+
+enum dtAndDdTags {
+  respondentsFullNameLabel = "#buffChildAndRespondentRelations_0_0 > fieldset > ccd-field-read > div > ccd-field-read-label > div > .case-field > dt", // this picks up both labels
+  respondentsFullName = "",
+  childNameLabel = "",
+  childName = "",
+}
+
+export class ChildrenAndRespondents1Page {
+  public static async childrenAndRespondents1Page(
+    page: Page,
+    accessibilityTest: boolean,
+  ): Promise<void> {
+    await this.checkPageLoads(page, accessibilityTest);
+    // if (errorMessaging) {
+    //   await this.triggerErrorMessages(page);
+    // }
+    // await this.fillInFields(page);
+  }
+
+  private static async checkPageLoads(
+    page: Page,
+    accessibilityTest: boolean,
+  ): Promise<void> {
+    await page.waitForSelector(
+      `${Selectors.h2}:text-is("${ChildrenAndRespondents1Content.h21}")`,
+    );
+    await Promise.all([
+      await Helpers.checkVisibleAndPresent(
+        page,
+        `${Selectors.GovukHeadingL}:text-is("${ChildrenAndRespondents1Content.pageTitle}")`,
+        1,
+      ),
+      await Helpers.checkVisibleAndPresent(
+        page,
+        `${Selectors.h2}:text-is("${ChildrenAndRespondents1Content.h22}")`,
+        1,
+      ),
+      await Helpers.checkVisibleAndPresent(
+        page,
+        `${Selectors.h3}:text-is("${ChildrenAndRespondents1Content.h3}")`,
+        1,
+      ),
+      await Helpers.checkVisibleAndPresent(
+        page,
+        `${Selectors.p}:text-is("${ChildrenAndRespondents1Content.p}")`,
+        1,
+      ),
+      Helpers.checkGroup(
+        page,
+        2,
+        ChildrenAndRespondents1Content,
+        "formLabel",
+        Selectors.GovukFormLabel,
+      ),
+      await Helpers.checkVisibleAndPresent(
+        page,
+        `${Selectors.GovukFormLabel}:text-is("${ChildrenAndRespondents1Content.formLabelYes}")`,
+        1,
+      ),
+      await Helpers.checkVisibleAndPresent(
+        page,
+        `${Selectors.GovukFormLabel}:text-is("${ChildrenAndRespondents1Content.formLabelNo}")`,
+        1,
+      ),
+    ]);
+    if (accessibilityTest) {
+      await AccessibilityTestHelper.run(page);
+    }
+  }
+
+  private static async triggerErrorMessages(page: Page): Promise<void> {}
+
+  private static async fillInFields(page: Page): Promise<void> {}
+}
