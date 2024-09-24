8fcb2fd4d0731d399179096face2c215a1478e72
OLT2000 <olliethurston1@gmail.com>
2024-09-24 11:41:07 +0100

    PRL-6233: Page 1 Content

---
+++ e2e/fixtures/manageCases/createCase/FL401/attendingTheHearing/attendingTheHearing1Content.ts
@@ -0,0 +1,65 @@
+export enum AttendingTheHearing1Content {
+  pageTitle = "Attending the hearing",
+  p1 = "You can save and return to this page at any time. Questions marked with a * need to be completed before you can send your application.",
+  p2 = 'In some cases, the court can make special arrangements for an adult or child involved in the case.',
+  p3 = 'For example - the court may provide a separate waiting room that is set apart from the respondent.',
+  p4 = 'The court can appoint an intermediary for vulnerable applicants.',
+  p5 = 'The intermediary helps the applicant to communicate and give evidence during the case.',
+
+  p6 = '*Describe the adjustments that the court needs to make.',
+  p7 = 'For example - someone with a hearing impairment may need an induction loop to be fitted in the courtroom.',
+  p8 = '*Give details of the special arrangements that are required.',
+  p9 = 'For example, a screen to separate the applicant from the respondent.',
+  p10 = '*Describe their relationship to the case.',
+  p11 = 'For example - grandmother of the applicant.',
+  p12 = '*Describe the adjustments that the court needs to make.',
+  p13 = 'For example - someone with a hearing impairment may need an induction loop to be fitted in the courtroom.',
+
+  yes = 'Yes',
+  no = 'No',
+  addNew = 'Add new',
+
+  h31 = 'Language requirements',
+  h32 = 'Accessibility',
+  h33 = 'Special arrangements',
+
+  h34 = 'Welsh needs',
+  h35 = ' Interpreter needs ',
+
+  h21 = ' Welsh needs ',
+  h22 = ' Interpreter needs ',
+  h23 = 'Add new welsh need',
+  h24 = 'Add new interpreter need',
+
+
+  formLabel1 = '*Will the applicant, or anyone else attending court, need to speak Welsh or read and write in Welsh during the proceedings?',
+  formLabel2 = '*Do you know if an interpreter will be needed in the court to explain information in a certain language?',
+  formLabel3 = '*Does the applicant, or anyone else attending the court, have a disability?',
+  formLabel4 = '*Will the court need to make special arrangements for the applicant, or any child involved in the case?',
+  formLabel5 = '*Do you know if an intermediary will be required?',
+
+  formLabel6 = '*Set out the reasons that an intermediary is required.',
+  formLabel7 = '*Provide the names of the people involved in the case who need to speak Welsh or read and write in Welsh.',
+  formLabel8 = 'Will need to speak Welsh',
+  formLabel9 = 'Will need to read and write in Welsh',
+  formLabel10 = '*Who will require the interpreter?',
+  formLabel11 = 'Applicant',
+  formLabel12 = 'Respondent',
+  formLabel13 = 'Other people in the case',
+  formLabel14 = '*Enter details of the language or dialect required.',
+  formLabel15 = 'Any other assistance required (e.g. sign language) (Optional)',
+
+  errorSummaryTitle = "There is a problem",
+  inputErrorSummary1 = " *Will the applicant, or anyone else attending court, need to speak Welsh or read and write in Welsh during the proceedings? is required ",
+  inputErrorSummary2 = " *Does the applicant, or anyone else attending the court, have a disability? is required ",
+  inputErrorSummary3 = " *Will the court need to make special arrangements for the applicant, or any child involved in the case? is required ",
+  inputErrorSummary4 = " *Do you know if an intermediary will be required? is required ",
+  inputErrorSummary5 = " *Do you know if an interpreter will be needed in the court to explain information in a certain language? is required ",
+  inputErrorMessage1 = " *Will the applicant, or anyone else attending court, need to speak Welsh or read and write in Welsh during the proceedings? is required ", "*Do you know if an interpreter will be needed in the court to explain information in a certain language? is required ",
+  inputErrorMessage2 = " *Does the applicant, or anyone else attending the court, have a disability? is required ",
+  inputErrorMessage3 = " *Will the court need to make special arrangements for the applicant, or any child involved in the case? is required ",
+  inputErrorMessage4 = " *Do you know if an intermediary will be required? is required ",
+  inputErrorMessage5 = " *Do you know if an interpreter will be needed in the court to explain information in a certain language? is required ",
+
+  continue = 'Continue'
+}
