efe68ba4bd0046fff27f6693487cd952e1a91591
OLT2000 <olliethurston1@gmail.com>
2024-09-24 10:33:09 +0100

    PRL-6232: Reset config

--- playwright.config.ts
+++ playwright.config.ts
@@ -10,9 +10,9 @@ module.exports = defineConfig({
   /* Retry on CI only */
   retries: 4, // Set the number of retries for all projects
 
-  timeout: 6 * 15 * 1000,
+  timeout: 6 * 60 * 1000,
   expect: {
-    timeout: 5 * 10 * 1000,
+    timeout: 5 * 60 * 1000,
   },
   reportSlowTests: null,
 
