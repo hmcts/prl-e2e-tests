9ae0363470ecbb161faec2caea191402e0dbc0b1
scralfie <kellyalfie6@gmail.com>
2024-09-25 13:23:09 +0100

    ticket: PRL-6251 - changes to config.ts

--- e2e/fixtures/manageCases/createCase/C100/miamPolicyUpgrade/miamPolicyUpgradeSubmitContent.ts
+++ e2e/fixtures/manageCases/createCase/C100/miamPolicyUpgrade/miamPolicyUpgradeSubmitContent.ts
@@ -1,1 +1,8 @@
-export enum MiamPolicyUpgradeSubmitContent {}
+export enum MiamPolicyUpgradeSubmitContent {
+  pageTitle = "",
+  h2 = "Check your answers",
+  text16CheckInfo = "",
+  text16formLabel = "",
+  text16Yes = "Yes",
+  text16No = "no",
+}
