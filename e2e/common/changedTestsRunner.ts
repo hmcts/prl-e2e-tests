36ccc92643fc414522db317996a96d5a64b5d5e9
Jake Gowler <jakecgowler@gmail.com>
2024-09-25 09:52:28 +0100

    changedTestsRunner.ts

--- e2e/common/changedTestsRunner.ts
+++ e2e/common/changedTestsRunner.ts
@@ -3,6 +3,7 @@ import { execSync } from "child_process";
 export class ChangedTestsRunner {
   public static run(): void {
     try {
+      this.fetchMasterBranch();
       const changedTestFiles: string[] = this.getChangedTestFiles();
       this.runPlaywrightTests(changedTestFiles);
     } catch (error) {
@@ -11,14 +12,26 @@ export class ChangedTestsRunner {
     }
   }
 
+  private static fetchMasterBranch(): void {
+    try {
+      console.log('Fetching master branch...');
+      execSync('git fetch origin master', { stdio: 'inherit' });
+      console.log('Master branch fetched successfully.');
+    } catch (error) {
+      console.error('Error fetching master branch: ', error);
+      process.exit(1);
+    }
+  }
+
   private static getChangedTestFiles(): string[] {
     try {
-      const diff: string =  execSync('git diff --name-only origin/master...HEAD').toString();
+      console.log('Getting changed test files...');
+      const diff: string = execSync('git diff --name-only origin/master...HEAD').toString();
       const changedFiles: string[] = diff.split(`\n`).filter(Boolean);
       return changedFiles.filter(file => file.endsWith('.spec.ts'));
     } catch (error) {
       console.error('Error detecting changed test files: ', error);
-      process.exit(1)
+      process.exit(1);
     }
   }
 
@@ -28,12 +41,12 @@ export class ChangedTestsRunner {
       return;
     }
     try {
-      const command: string = `yarn playwright test ${testFiles.join(' ')} --project chromium`
-      console.log(`Running playwright tests on": ${testFiles.join(', ')}`)
-      execSync(command, { stdio: 'inherit'});
+      const command: string = `yarn playwright test ${testFiles.join(' ')} --project chromium`;
+      console.log(`Running Playwright tests on: ${testFiles.join(', ')}`);
+      execSync(command, { stdio: 'inherit' });
     } catch (error) {
       console.error('Error running Playwright tests: ', error);
-      process.exit(1)
+      process.exit(1);
     }
   }
 }
