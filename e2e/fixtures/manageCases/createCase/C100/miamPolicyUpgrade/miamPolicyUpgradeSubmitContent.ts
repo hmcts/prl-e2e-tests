c99793bad8b6a22ed3b771f358d1df225325ce91
scralfie <kellyalfie6@gmail.com>
2024-09-26 10:51:42 +0100

    ticket: PRL-6254

--- e2e/fixtures/manageCases/createCase/C100/miamPolicyUpgrade/miamPolicyUpgradeSubmitContent.ts
+++ e2e/fixtures/manageCases/createCase/C100/miamPolicyUpgrade/miamPolicyUpgradeSubmitContent.ts
@@ -1,8 +1,37 @@
 export enum MiamPolicyUpgradeSubmitContent {
-  pageTitle = "",
+  pageTitle = "MIAM",
   h2 = "Check your answers",
-  text16CheckInfo = "",
-  text16formLabel = "",
+  text16CheckInfo = "Check the information below carefully.",
+
+
+  text16 = "*Are the children involved in any emergency protection, care or supervision proceedings (or have they been)?",
+
+  text16Attended1 = "*Are the children involved in any emergency protection, care or supervision proceedings (or have they been)?",
+  text16Attended2 = "*Has the applicant attended a Mediation Information & Assessment Meeting (MIAM)?",
+  text16Attended3 = "*Mediator Registration Number (URN)",
+  text16Attended4 = "*Family Mediation Service Name",
+  text16Attended5 = "*Sole Trader Name",
+  text16Attended6 = "*MIAM certificate",
+
+  text16Exemption1 = "*Are the children involved in any emergency protection, care or supervision proceedings (or have they been)?",
+  text16Exemption2 = "*Has the applicant attended a Mediation Information & Assessment Meeting (MIAM)?",
+  text16Exemption3 = "*Is the applicant claiming exemption from the requirement to attend a MIAM ?",
+  text16Exemption4 = "Why is the applicant not attending a MIAM?",
+  text16Exemption5 = "*What evidence of domestic abuse does the applicant have?",
+  text16Exemption6 = "*Can you provide evidence?",
+  text16Exemption7 = "Evidence",
+  text16Exemption8 = "*The applicant confirms that a child who would be the subject of the application or another child of the family who is living with that child is currently",
+  text16Exemption9 = "Why must the application be made urgently?",
+  text162Exemption10 = "Has, there been previous attendance of a MIAM or non-court dispute resolution?",
+  text162Exemption11 = "*What evidence of MIAM attendance are you submitting?",
+  text162Exemption12 = "*Upload the MIAM certificate signed by the mediator.",
+  text162Exemption13 = "What other grounds of exemption apply?",
+  text162Exemption14 = "*Explain why the applicant is unable to attend MIAM",
+
+
   text16Yes = "Yes",
   text16No = "no",
+
+
+  continue = "Save and continue"
 }
