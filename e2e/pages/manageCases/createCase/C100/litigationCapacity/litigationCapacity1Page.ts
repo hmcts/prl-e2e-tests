cd9b95c9bdbe63710d0ef99e749da0509ea7c790
scralfie <kellyalfie6@gmail.com>
2024-10-01 14:40:30 +0100

    ticket: PRL-6258 - Started SubmitPage

---
+++ e2e/pages/manageCases/createCase/C100/litigationCapacity/litigationCapacity1Page.ts
@@ -0,0 +1,106 @@
+import { Page } from "@playwright/test";
+import { LitigationCapacity1Content } from "../../../../../fixtures/manageCases/createCase/C100/litigationCapacity/litigationCapacity1Content";
+import { Selectors } from "../../../../../common/selectors";
+import { Helpers } from "../../../../../common/helpers";
+
+interface LitigationCapacity1PageOptions {
+  page: Page;
+  accessibilityTest: boolean;
+  yesNoLitigationCapacity: boolean;
+}
+
+interface checkPageLoadsOptions {
+  page: Page;
+  accessibilityTest: boolean;
+}
+
+interface fillInFieldsOptions {
+  page: Page;
+  yesNoLitigationCapacity: boolean;
+}
+
+enum UniquesSelectors {
+  litigationCapacityFactors = "#litigationCapacityFactors",
+  litigationCapacityReferrals = "#litigationCapacityReferrals",
+  litigationCapacityOtherFactors_Yes = "#litigationCapacityOtherFactors_Yes",
+  litigationCapacityOtherFactors_No = "#litigationCapacityOtherFactors_No",
+  litigationCapacityOtherFactorsDetails = "#litigationCapacityOtherFactorsDetails",
+}
+
+export class LitigationCapacity1Page {
+  public static async litigationCapacity1Page({
+    page: page,
+    accessibilityTest: accessibilityTest,
+    yesNoLitigationCapacity: yesNoLitigationCapacity,
+  }: LitigationCapacity1PageOptions): Promise<void> {
+    await this.checkPageLoads({
+      page: page,
+      accessibilityTest: accessibilityTest,
+    });
+    await this.fillInFields({
+      page: page,
+      yesNoLitigationCapacity: yesNoLitigationCapacity,
+    });
+  }
+
+  private static async checkPageLoads({
+    page: page,
+    accessibilityTest: accessibilityTest,
+  }: checkPageLoadsOptions): Promise<void> {
+    await page.waitForSelector(
+      `${Selectors.p}:text-is("${LitigationCapacity1Content.p}")`,
+    );
+    await Promise.all([
+      Helpers.checkVisibleAndPresent(
+        page,
+        `${Selectors.GovukHeadingL}:text-is("${LitigationCapacity1Content.pageTitle}")`,
+        1,
+      ),
+      Helpers.checkGroup(
+        page,
+        3,
+        LitigationCapacity1Content,
+        "formLabel",
+        `${Selectors.GovukFormLabel}`,
+      ),
+      Helpers.checkVisibleAndPresent(
+        page,
+        `${Selectors.GovukFormLabel}:text-is("${LitigationCapacity1Content.formLabelYes}")`,
+        1,
+      ),
+      Helpers.checkVisibleAndPresent(
+        page,
+        `${Selectors.GovukFormLabel}:text-is("${LitigationCapacity1Content.formLabelNo}")`,
+        1,
+      ),
+    ]);
+  }
+
+  private static async fillInFields({
+    page: page,
+    yesNoLitigationCapacity: yesNoLitigationCapacity,
+  }: fillInFieldsOptions): Promise<void> {
+    await page.fill(
+      `${UniquesSelectors.litigationCapacityFactors}`,
+      LitigationCapacity1Content.loremIpsum,
+    );
+    await page.fill(
+      `${UniquesSelectors.litigationCapacityReferrals}`,
+      LitigationCapacity1Content.loremIpsum,
+    );
+    if (yesNoLitigationCapacity) {
+      await page.click(
+        `${UniquesSelectors.litigationCapacityOtherFactors_Yes}`,
+      );
+      await page.fill(
+        `${UniquesSelectors.litigationCapacityOtherFactorsDetails}`,
+        LitigationCapacity1Content.loremIpsum,
+      );
+    } else {
+      await page.click(`${UniquesSelectors.litigationCapacityOtherFactors_No}`);
+    }
+    await page.click(
+      `${Selectors.button}:text-is("${LitigationCapacity1Content.continue}")`,
+    );
+  }
+}
---
+++ e2e/pages/manageCases/createCase/C100/litigationCapacity/litigationCapacitySubmitPage.ts
@@ -0,0 +1,82 @@
+import { Page } from "@playwright/test";
+import { C100ChildrenAndApplicantsRelationship } from "../childrenAndApplicants/childrenAndApplicants1Page";
+import AccessibilityTestHelper from "../../../../../common/accessibilityTestHelper";
+import { Selectors } from "../../../../../common/selectors";
+import {
+  LitigationCapacitySubmitContent
+} from "../../../../../fixtures/manageCases/createCase/C100/litigationCapacity/litigationCapacitySubmitContent";
+
+interface LitigationCapacityOptions {
+  page: Page;
+  accessibilityTest: boolean;
+  yesNoLitigationCapacity: boolean;
+}
+
+interface checkFieldsOptions {
+  page: Page;
+  accessibilityTest: boolean;
+  yesNoLitigationCapacity: boolean;
+}
+
+interface checkFilledDataOptions {
+  page: Page;
+  yesNoLitigationCapacity: boolean;
+}
+
+export class LitigationCapacitySubmitPage {
+  public static async litigationCapacitySubmitPage({
+    page: page,
+    accessibilityTest: accessibilityTest,
+    yesNoLitigationCapacity: yesNoLitigationCapacity,
+  }: LitigationCapacityOptions): Promise<void> {
+    await this.checkPageLoads({
+      page: page,
+      accessibilityTest: accessibilityTest,
+      yesNoLitigationCapacity: yesNoLitigationCapacity,
+    });
+    await this.continue(page);
+  }
+
+  private static async checkPageLoads({
+    page: page,
+    accessibilityTest: accessibilityTest,
+    yesNoLitigationCapacity: yesNoLitigationCapacity,
+  }: LitigationCapacityOptions): Promise<void> {
+    await page.waitForSelector(`${Selectors.h2}:text-is("${LitigationCapacitySubmitContent.h2}")`);
+    await Promise.all([
+    this.checkPageFields({
+      page: page,
+      accessibilityTest: accessibilityTest,
+      yesNoLitigationCapacity: yesNoLitigationCapacity,
+    }),
+    this.checkPageData({
+      page: page,
+      yesNoLitigationCapacity: yesNoLitigationCapacity,
+    }),
+  ]);
+    if (accessibilityTest) {
+      await AccessibilityTestHelper.run(page);
+    }
+  }
+
+  private static async checkPageFields({
+                                         page: page,
+                                         yesNoLitigationCapacity: yesNoLitigationCapacity,
+                                       }:checkFieldsOptions): Promise<void> {
+    await Promise.all([
+
+    ]);
+  }
+
+
+  private static async checkPageData({
+                                       page: page,
+                                       yesNoLitigationCapacity: yesNoLitigationCapacity,
+                                     }:checkFilledDataOptions): Promise<void> {
+
+  }
+
+  private static async continue(page: Page): Promise<void> {
+    await page.click(`${Selectors.button}:text-is("${LitigationCapacitySubmitContent.continue}")`)
+  }
+}
