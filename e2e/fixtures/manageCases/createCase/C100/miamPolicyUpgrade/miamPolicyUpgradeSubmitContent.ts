aa069c5df0d8299c16c46283cea09a4f6e71b18d
scralfie <kellyalfie6@gmail.com>
2024-10-01 14:46:21 +0100

    ticket: PRL-6254 - removed whitespace

--- e2e/fixtures/manageCases/createCase/C100/miamPolicyUpgrade/miamPolicyUpgradeSubmitContent.ts
+++ e2e/fixtures/manageCases/createCase/C100/miamPolicyUpgrade/miamPolicyUpgradeSubmitContent.ts
@@ -2,9 +2,7 @@ export enum MiamPolicyUpgradeSubmitContent {
   pageTitle = "MIAM",
   h2 = "Check your answers",
   text16CheckInfo = "Check the information below carefully.",
-
   text16ChildrenInvolvedInEmergencyProtection = "*Are the children involved in any emergency protection, care or supervision proceedings (or have they been)?",
-
   text16Attended1 = "*Are the children involved in any emergency protection, care or supervision proceedings (or have they been)?",
   text16Attended2 = "*Has the applicant attended a Mediation Information & Assessment Meeting (MIAM)?",
   text16Attended3 = "*Mediator Registration Number (URN)",
@@ -38,10 +36,8 @@ export enum MiamPolicyUpgradeSubmitContent {
   text162Exemption6 = "*Provide the mediator details",
   text162Exemption7 = "What other grounds of exemption apply?",
   text162Exemption8 = "*Explain why the applicant is unable to attend MIAM",
-
   text16Yes = "Yes",
   text16No = "No",
   text16Change = "Change",
-
   continue = "Save and continue",
 }
