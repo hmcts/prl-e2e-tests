7910fe4564fdc6e0989a89241f3928c3bff326d2
scralfie <kellyalfie6@gmail.com>
2024-10-01 10:09:11 +0100

    ticket: PRL-6257 - Files created for pages/fixtures

---
+++ e2e/fixtures/manageCases/createCase/C100/internationalElement/internationalElement1Page.ts
@@ -0,0 +1,12 @@
+export enum InternationalElement1Page {
+  pageTitle = "International element",
+  p1 = "You can save and return to this page at any time. Questions marked with a * need to be completed before you can send your application.",
+  formLabel1 = "Do you have any reason to believe that any child, parent or potentially significant adult in the child's life may be habitually resident in another state? (Optional)",
+  formLabel2 = "Do you have any reason to believe that there may be an issue as to jurisdiction in this case? (Optional)",
+  formLabel3 = "Has a request been made or should a request be made to a Central Authority or other competent authority in a foreign state or a consular authority in England and Wales? (Optional)",
+  additionalFormLabel = "*Give reason (Optional)",
+  loremIpsum = "loremIpsum",
+  formLabelYes = "Yes",
+  formLabelNo = "No",
+  continue = "Continue"
+}
---
+++ e2e/fixtures/manageCases/createCase/C100/internationalElement/internationalElementSubmitPage.ts
@@ -0,0 +1,11 @@
+export enum InternationalElementSubmitPage {
+  pageTitle = "International element",
+  h2 = "Check your answers",
+  text161 = "Check the information below carefully.\n",
+  text162 = "Do you have any reason to believe that any child, parent or potentially significant adult in the child's life may be habitually resident in another state?",
+  text163 = "Do you have any reason to believe that there may be an issue as to jurisdiction in this case?",
+  text164 = "Has a request been made or should a request be made to a Central Authority or other competent authority in a foreign state or a consular authority in England and Wales?",
+  text16GiveReasons = "*Give reason",
+  text16Change = "Change",
+  continue = "Save and continue"
+}
