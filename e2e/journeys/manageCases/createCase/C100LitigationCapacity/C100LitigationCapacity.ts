50992d4626a2e3a8d6f1186764845081b9a04444
scralfie <kellyalfie6@gmail.com>
2024-10-01 15:18:24 +0100

    ticket: PRL-6258 - Journey Written

---
+++ e2e/journeys/manageCases/createCase/C100LitigationCapacity/C100LitigationCapacity.ts
@@ -0,0 +1,51 @@
+import { Page } from "@playwright/test";
+import { UserRole } from "../../../../common/types";
+import { SolicitorCreateInitial } from "../solicitorCreateInitial";
+import { Helpers } from "../../../../common/helpers";
+import {
+  LitigationCapacity1Page
+} from "../../../../pages/manageCases/createCase/C100/litigationCapacity/litigationCapacity1Page";
+import {
+  LitigationCapacitySubmitPage
+} from "../../../../pages/manageCases/createCase/C100/litigationCapacity/litigationCapacitySubmitPage";
+import { C100TasksTabPage } from "../../../../pages/manageCases/caseTabs/c100TasksTabPage";
+
+interface C100LitigationCapacityOptions {
+  page: Page;
+  user: UserRole;
+  accessibilityTest: boolean;
+  yesNoLitigationCapacity: boolean;
+  subJourney: boolean;
+}
+
+export class C100LitigationCapacity {
+  public static async c100LitigationCapacity({
+    page,
+    user,
+    accessibilityTest,
+    yesNoLitigationCapacity,
+    subJourney,
+  }: C100LitigationCapacityOptions): Promise<void> {
+    if (subJourney) {
+      await SolicitorCreateInitial.createInitialCase({
+        page: page,
+        user: user,
+        accessibilityTest: false,
+        solicitorCaseType: "C100",
+        errorMessaging: false,
+      });
+    }
+    await Helpers.selectSolicitorEvent(page, "Litigation capacity");
+    await LitigationCapacity1Page.litigationCapacity1Page({
+      page: page,
+      accessibilityTest: accessibilityTest,
+      yesNoLitigationCapacity: yesNoLitigationCapacity,
+    });
+    await LitigationCapacitySubmitPage.litigationCapacitySubmitPage({
+      page: page,
+      accessibilityTest: accessibilityTest,
+      yesNoLitigationCapacity: yesNoLitigationCapacity,
+    });
+    await C100TasksTabPage.c100TasksTabPage(page, accessibilityTest);
+  }
+}
--- e2e/pages/manageCases/createCase/C100/litigationCapacity/litigationCapacitySubmitPage.ts
+++ e2e/pages/manageCases/createCase/C100/litigationCapacity/litigationCapacitySubmitPage.ts
@@ -3,6 +3,7 @@ import { C100ChildrenAndApplicantsRelationship } from "../childrenAndApplicants/
 import AccessibilityTestHelper from "../../../../../common/accessibilityTestHelper";
 import { Selectors } from "../../../../../common/selectors";
 import { LitigationCapacitySubmitContent } from "../../../../../fixtures/manageCases/createCase/C100/litigationCapacity/litigationCapacitySubmitContent";
+import { Helpers } from "../../../../../common/helpers";
 
 interface LitigationCapacityOptions {
   page: Page;
@@ -63,13 +64,71 @@ export class LitigationCapacitySubmitPage {
     page: page,
     yesNoLitigationCapacity: yesNoLitigationCapacity,
   }: checkFieldsOptions): Promise<void> {
-    await Promise.all([]);
+    await Promise.all([
+      Helpers.checkVisibleAndPresent(
+        page,
+        `${Selectors.GovukHeadingL}:text-is("${LitigationCapacitySubmitContent.pageTitle}")`,
+        1,
+      ),
+      Helpers.checkGroup(
+        page,
+        4,
+        LitigationCapacitySubmitContent,
+        "formLabel",
+        `${Selectors.GovukText16}`,
+      ),
+    ]);
+    if (yesNoLitigationCapacity) {
+      await Helpers.checkVisibleAndPresent(
+        page,
+        `${Selectors.GovukText16}:text-is("${LitigationCapacitySubmitContent.additionalText16}")`,
+        1,
+      );
+    }
   }
 
   private static async checkPageData({
     page: page,
     yesNoLitigationCapacity: yesNoLitigationCapacity,
-  }: checkFilledDataOptions): Promise<void> {}
+  }: checkFilledDataOptions): Promise<void> {
+    if (yesNoLitigationCapacity) {
+      await Promise.all([
+        Helpers.checkVisibleAndPresent(
+          page,
+          `${Selectors.GovukText16}:text-is("${LitigationCapacitySubmitContent.text16Yes}")`,
+          1,
+        ),
+        Helpers.checkVisibleAndPresent(
+          page,
+          `${Selectors.Span}:text-is("${LitigationCapacitySubmitContent.loremIpsum}")`,
+          3,
+        ),
+        Helpers.checkVisibleAndPresent(
+          page,
+          `${Selectors.GovukText16}:text-is("${LitigationCapacitySubmitContent.text16Change}")`,
+          4,
+        ),
+      ]);
+    } else {
+      await Promise.all([
+        Helpers.checkVisibleAndPresent(
+          page,
+          `${Selectors.GovukText16}:text-is("${LitigationCapacitySubmitContent.text16No}")`,
+          1,
+        ),
+        Helpers.checkVisibleAndPresent(
+          page,
+          `${Selectors.Span}:text-is("${LitigationCapacitySubmitContent.loremIpsum}")`,
+          2,
+        ),
+        Helpers.checkVisibleAndPresent(
+          page,
+          `${Selectors.GovukText16}:text-is("${LitigationCapacitySubmitContent.text16Change}")`,
+          3,
+        ),
+      ]);
+    }
+  }
 
   private static async continue(page: Page): Promise<void> {
     await page.click(
