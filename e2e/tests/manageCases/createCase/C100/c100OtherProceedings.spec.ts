875d0b34cad3fb00c45487a6757a0c08c4da6823
OLT2000 <olliethurston1@gmail.com>
2024-10-02 09:45:40 +0100

    PRL-6255: update accessibility

--- e2e/tests/manageCases/createCase/C100/c100OtherProceedings.spec.ts
+++ e2e/tests/manageCases/createCase/C100/c100OtherProceedings.spec.ts
@@ -92,14 +92,14 @@ test.describe("C100 Create case other proceedings tests @manageCases", (): void 
 test(`Accessibility test the C100 other proceedings event as a solicitor with the following options:
   Accessibility testing,
   Not Error message testing,
-  Saying yes to previous or ongoing family court proceedings involving the applicant and respondent, @accessibilityManageCases`, async ({
+  Saying no to previous or ongoing proceedings for the child(ren)?, @accessibilityManageCases`, async ({
   page,
 }): Promise<void> => {
   await C100OtherProceedings.c100OtherProceedings({
     page: page,
     accessibilityTest: true,
     errorMessaging: false,
-    c100OtherProceedings: "Yes",
+    c100OtherProceedings: "No",
     subJourney: true,
   });
 });
