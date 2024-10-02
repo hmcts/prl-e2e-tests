0a1010d542dad2e62dd684d50c63b5247262bb2a
scralfie <kellyalfie6@gmail.com>
2024-10-02 15:54:53 +0100

    ticket: PRL-6259 - Content files complete

--- e2e/fixtures/manageCases/createCase/C100/welshLanguageRequirements/welshLanguageRequirements1Content.ts
+++ e2e/fixtures/manageCases/createCase/C100/welshLanguageRequirements/welshLanguageRequirements1Content.ts
@@ -1,1 +1,12 @@
-export enum WelshLanguageRequirements1Content {}
+export enum WelshLanguageRequirements1Content {
+  pageTitle = "Welsh language requirements",
+  p = "You can save and return to this page at any time. Questions marked with a * need to be completed before you can send your application.",
+  formLabel1 = "Does any person in this case need orders or documents in Welsh? (Optional)",
+  hiddenFormLabel1 = "*Which language are you using to complete this application? (Optional)",
+  hiddenFormLabel = "*Does this application need to be translated into Welsh? (Optional)",
+  formLabelEnglish = "English",
+  formLabelWelsh = "Welsh",
+  formLabelYes = "Yes",
+  formLabelNo = "No",
+  continue = "Continue"
+}
--- e2e/fixtures/manageCases/createCase/C100/welshLanguageRequirements/welshLanguageRequirementsSubmitContent.ts
+++ e2e/fixtures/manageCases/createCase/C100/welshLanguageRequirements/welshLanguageRequirementsSubmitContent.ts
@@ -1,1 +1,14 @@
-export enum WelshLanguageRequirementsSubmitContent {}
+export enum WelshLanguageRequirementsSubmitContent {
+  pageTitle = "Welsh language requirements",
+  h2 = "Check your answers",
+  text161 = "Check the information below carefully.",
+  text162 = "Does any person in this case need orders or documents in Welsh?",
+  text163 = "*Which language are you using to complete this application?",
+  text164 = "*Does this application need to be translated into Welsh?",
+  text16English = "English",
+  text16Welsh = "Welsh",
+  text16Yes = "Yes",
+  text16No = "No",
+  text16Change = "Change",
+  continue = "Save and continue"
+}
