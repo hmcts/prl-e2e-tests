3ed4cb96ed45218ee00474119e6e788415541a6c
JakeCG <jakecgowler@gmail.com>
2024-10-04 13:32:00 +0100

    PRL6253 (#44)
    * allegations Of Harm

---
+++ e2e/fixtures/manageCases/createCase/C100/allegationsOfHarm/allegationsOfHarmRevised4Content.ts
@@ -0,0 +1,12 @@
+export enum AllegationsOfHarmRevised4Content {
+  pageTitle = "Allegations of harm",
+  h2 = "Child abuse - Behaviours",
+  p = "Give a short description of what happened and any relevant information so the court can decide what needs to be done.",
+  formLabel1 = "Type of abuse (Optional)",
+  formLabel2 = "Physical abuse",
+  formLabel3 = "Psychological abuse",
+  formLabel4 = "Sexual abuse",
+  formLabel5 = "Emotional abuse",
+  formLabel6 = "Financial abuse",
+  button = "Continue",
+}
---
+++ e2e/pages/manageCases/createCase/C100/allegationsOfHarm/allegationsOfHarmRevised3Page.ts
@@ -0,0 +1,148 @@
+import { Page } from "@playwright/test";
+import { Selectors } from "../../../../../common/selectors";
+import { AllegationsOfHarmRevised3Content } from "../../../../../fixtures/manageCases/createCase/C100/allegationsOfHarm/allegationsOfHarmRevised3Content";
+import { Helpers } from "../../../../../common/helpers";
+import AccessibilityTestHelper from "../../../../../common/accessibilityTestHelper";
+
+interface AllegationsOfHarmRevised3PageOptions {
+  page: Page;
+  accessibilityTest: boolean;
+  c100DomesticAbuseTypePage3: C100AllegationsOfHarmTypeOfDomesticAbuse;
+}
+
+interface checkPageLoadsOptions {
+  page: Page;
+  accessibilityTest: boolean;
+}
+
+interface fillInFieldsOptions {
+  page: Page;
+  c100DomesticAbuseTypePage3: C100AllegationsOfHarmTypeOfDomesticAbuse;
+}
+
+enum uniqueSelectors {
+  physicalRadio = "#domesticBehaviours_0_typeOfAbuse-TypeOfAbuseEnum_value_1",
+  psychologicalRadio = "#domesticBehaviours_0_typeOfAbuse-TypeOfAbuseEnum_value_2",
+  sexualRadio = "#domesticBehaviours_0_typeOfAbuse-TypeOfAbuseEnum_value_3",
+  emotionalRadio = "#domesticBehaviours_0_typeOfAbuse-TypeOfAbuseEnum_value_4",
+  financialRadio = "#domesticBehaviours_0_typeOfAbuse-TypeOfAbuseEnum_value_5",
+  natureField = "#domesticBehaviours_0_newAbuseNatureDescription",
+  durationField = "#domesticBehaviours_0_newBehavioursStartDateAndLength",
+  applicantHelpRadioYes = "#domesticBehaviours_0_newBehavioursApplicantSoughtHelp_Yes",
+  helpSoughtField = "#domesticBehaviours_0_newBehavioursApplicantHelpSoughtWho",
+}
+
+export type C100AllegationsOfHarmTypeOfDomesticAbuse =
+  | "Physical abuse"
+  | "Psychological abuse"
+  | "Sexual abuse"
+  | "Emotional abuse"
+  | "Financial abuse";
+
+export class AllegationsOfHarmRevised3Page {
+  public static async allegationsOfHarmRevised3Page({
+    page: page,
+    accessibilityTest: accessibilityTest,
+    c100DomesticAbuseTypePage3: c100DomesticAbuseTypePage3,
+  }: AllegationsOfHarmRevised3PageOptions): Promise<void> {
+    await this.checkPageLoads({
+      page: page,
+      accessibilityTest: accessibilityTest,
+    });
+    await this.fillInFields({
+      page: page,
+      c100DomesticAbuseTypePage3: c100DomesticAbuseTypePage3,
+    });
+  }
+
+  private static async checkPageLoads({
+    page: page,
+    accessibilityTest: accessibilityTest,
+  }: checkPageLoadsOptions): Promise<void> {
+    await page.click(
+      `${Selectors.button}:text-is("${AllegationsOfHarmRevised3Content.addNewButton}")`,
+    );
+    await page.waitForSelector(
+      `${Selectors.p}:text-is("${AllegationsOfHarmRevised3Content.p}")`,
+    );
+    await Promise.all([
+      Helpers.checkVisibleAndPresent(
+        page,
+        `${Selectors.GovukHeadingL}:text-is("${AllegationsOfHarmRevised3Content.pageTitle}")`,
+        1,
+      ),
+      Helpers.checkGroup(
+        page,
+        3,
+        AllegationsOfHarmRevised3Content,
+        `h2`,
+        `${Selectors.h2}`,
+      ),
+      Helpers.checkVisibleAndPresent(
+        page,
+        `${Selectors.h3}:text-is("${AllegationsOfHarmRevised3Content.h3}")`,
+        1,
+      ),
+      Helpers.checkGroup(
+        page,
+        10,
+        AllegationsOfHarmRevised3Content,
+        `formLabel`,
+        `${Selectors.GovukFormLabel}`,
+      ),
+    ]);
+    if (accessibilityTest) {
+      await AccessibilityTestHelper.run(page);
+    }
+  }
+
+  private static async fillInFields({
+    page: page,
+    c100DomesticAbuseTypePage3: c100DomesticAbuseTypePage3,
+  }: fillInFieldsOptions): Promise<void> {
+    switch (c100DomesticAbuseTypePage3) {
+      case "Physical abuse":
+        await page.click(`${uniqueSelectors.physicalRadio}`);
+        await page.click(`${uniqueSelectors.physicalRadio}`);
+        break;
+      case "Psychological abuse":
+        await page.click(`${uniqueSelectors.psychologicalRadio}`);
+        await page.click(`${uniqueSelectors.psychologicalRadio}`);
+        break;
+      case "Sexual abuse":
+        await page.click(`${uniqueSelectors.sexualRadio}`);
+        await page.click(`${uniqueSelectors.sexualRadio}`);
+        break;
+      case "Emotional abuse":
+        await page.click(`${uniqueSelectors.emotionalRadio}`);
+        await page.click(`${uniqueSelectors.emotionalRadio}`);
+        break;
+      case "Financial abuse":
+        await page.click(`${uniqueSelectors.financialRadio}`);
+        await page.click(`${uniqueSelectors.financialRadio}`);
+        break;
+    }
+    await page.fill(
+      `${uniqueSelectors.natureField}`,
+      `${AllegationsOfHarmRevised3Content.natureOfBehaviour}`,
+    );
+    await page.fill(
+      `${uniqueSelectors.durationField}`,
+      `${AllegationsOfHarmRevised3Content.behaviourLength}`,
+    );
+    await page.click(`${uniqueSelectors.applicantHelpRadioYes}`);
+    await page.click(`${uniqueSelectors.applicantHelpRadioYes}`);
+    await Helpers.checkVisibleAndPresent(
+      page,
+      `${Selectors.GovukFormLabel}:text-is("${AllegationsOfHarmRevised3Content.formLabelYesExtra}")`,
+      1,
+    );
+    await page.fill(
+      `${uniqueSelectors.helpSoughtField}`,
+      `${AllegationsOfHarmRevised3Content.helpSoughtFrom}`,
+    );
+    await page.click(
+      `${Selectors.button}:text-is("${AllegationsOfHarmRevised3Content.continueButton}")`,
+    );
+  }
+}
