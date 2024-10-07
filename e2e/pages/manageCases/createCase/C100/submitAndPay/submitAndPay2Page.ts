95afd6ae645582d097b12ffa843457c967c3893f
scralfie <kellyalfie6@gmail.com>
2024-10-07 16:46:12 +0100

    Updated submitAndPay2Page.ts, Confirmed both tests now pass after changes

--- e2e/pages/manageCases/createCase/C100/submitAndPay/submitAndPay2Page.ts
+++ e2e/pages/manageCases/createCase/C100/submitAndPay/submitAndPay2Page.ts
@@ -7,6 +7,7 @@ import AccessibilityTestHelper from "../../../../../common/accessibilityTestHelp
 interface SubmitAndPay2PageOptions {
   page: Page;
   accessibilityTest: boolean;
+  yesNoWelshLanguage: boolean;
 }
 
 interface checkPageLoadsOptions {
@@ -16,6 +17,12 @@ interface checkPageLoadsOptions {
 
 interface fillInFieldsOptions {
   page: Page;
+  yesNoWelshLanguage: boolean;
+}
+
+interface checkHiddenContentLoads {
+  page: Page;
+  yesNoWelshLanguage: boolean;
 }
 
 enum UniqueSelectors {
@@ -26,6 +33,7 @@ export class SubmitAndPay2Page {
   public static async submitAndPay2Page({
     page: page,
     accessibilityTest: accessibilityTest,
+    yesNoWelshLanguage: yesNoWelshLanguage,
   }: SubmitAndPay2PageOptions): Promise<void> {
     await this.checkPageLoads({
       page: page,
@@ -33,6 +41,7 @@ export class SubmitAndPay2Page {
     });
     await this.fillInFields({
       page: page,
+      yesNoWelshLanguage: yesNoWelshLanguage,
     });
   }
 
@@ -68,15 +77,22 @@ export class SubmitAndPay2Page {
 
   private static async fillInFields({
     page: page,
+    yesNoWelshLanguage: yesNoWelshLanguage,
   }: fillInFieldsOptions): Promise<void> {
     await page.click(`${UniqueSelectors.payAgreeStatementAgree}`);
-    await this.checkHiddenContentLoads(page);
+    await this.checkHiddenContentLoads({
+      page: page,
+      yesNoWelshLanguage: yesNoWelshLanguage,
+    });
     await page.click(
       `${Selectors.button}:text-is("${SubmitAndPay2Content.continue}")`,
     );
   }
 
-  private static async checkHiddenContentLoads(page: Page): Promise<void> {
+  private static async checkHiddenContentLoads({
+    page: page,
+    yesNoWelshLanguage: yesNoWelshLanguage,
+  }: checkHiddenContentLoads): Promise<void> {
     await Promise.all([
       Helpers.checkGroup(
         page,
@@ -97,17 +113,40 @@ export class SubmitAndPay2Page {
         "hiddenP",
         `${Selectors.p}`,
       ),
-      Helpers.checkVisibleAndPresent(
-        page,
-        `${Selectors.GovukTextFieldLabel}:text-is("${SubmitAndPay2Content.formLabelDraftConsent}")`,
-        1,
-      ),
-      Helpers.checkVisibleAndPresent(
-        page,
-        `${Selectors.GovukTextFieldLabel}:text-is("${SubmitAndPay2Content.formLabelDraftDocumentWelsh}")`,
-        1,
-      ),
-      Helpers.checkGroup(page, 2, SubmitAndPay2Content, "a", `${Selectors.a}`),
     ]);
+    if (yesNoWelshLanguage) {
+      await Promise.all([
+        Helpers.checkVisibleAndPresent(
+          page,
+          `${Selectors.GovukTextFieldLabel}:text-is("${SubmitAndPay2Content.formLabelDraftConsent}")`,
+          1,
+        ),
+        Helpers.checkVisibleAndPresent(
+          page,
+          `${Selectors.GovukTextFieldLabel}:text-is("${SubmitAndPay2Content.formLabelDraftDocumentWelsh}")`,
+          1,
+        ),
+        Helpers.checkGroup(
+          page,
+          2,
+          SubmitAndPay2Content,
+          "a",
+          `${Selectors.a}`,
+        )
+      ]);
+    } else {
+      await Promise.all([
+        Helpers.checkVisibleAndPresent(
+          page,
+          `${Selectors.GovukTextFieldLabel}:text-is("${SubmitAndPay2Content.formLabelDraftConsent}")`,
+          1,
+        ),
+        Helpers.checkVisibleAndPresent(
+          page,
+          `${Selectors.a}:text-is("${SubmitAndPay2Content.a1}")`,
+          1,
+        ),
+      ]);
+    }
   }
 }
