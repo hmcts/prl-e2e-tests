a6df6af9b36360b29b42927a8568e1d15f8ceee0
abigailflorence97 <abigail.smith@hmcts.net>
2024-10-09 15:37:12 +0100

    add urls and create case name

---
+++ e2e/common/apiHelper/urls.ts
@@ -0,0 +1,15 @@
+// aat, demo, perftest, ithc
+const env = process.env.ENVIRONMENT || "aat";
+
+interface UrlConfig {
+    [key: string]: string;
+}
+
+export const urlConfig: UrlConfig = {
+  env:env,
+  idamUrl: process.env.IDAM_API_URL || `https://idam-api.${env}.platform.hmcts.net`,
+  serviceUrl: process.env.CASE_SERVICE_URL || `http://prl-cos-${env}.service.core-compute-${env}.internal`,
+  frontEndBaseURL: process.env.FE_BASE_URL || `https://manage-case.${env}.platform.hmcts.net`,
+  // You can add other URLs as needed
+
+};
