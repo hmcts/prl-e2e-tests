73e724d31f1b9ebcb9afa4d6dba4ffc02e9ce85a
OLT2000 <olliethurston1@gmail.com>
2024-10-09 09:23:30 +0100

    PRL-6358: Legal representation Content

---
+++ e2e/fixtures/c100ScreeningSections/c100ScreeningSectionLegalRepresentation.ts
@@ -0,0 +1,8 @@
+export enum C100ScreeningSectionLegalRepresentation {
+  pageTitle = "Will you be using a legal representative in these proceedings?",
+  formLabel1 = "Yes",
+  formLabel2 = "No",
+  link1 = 'Find legal representation',
+  link2 = 'Do you need legal aid?',
+  continue = " Continue "
+}
