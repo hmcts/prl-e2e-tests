b688340ef04ea874a42e46b48686b3c1f267345a
RobL681 <robert.lacey@justice.gov.uk>
2024-10-10 15:41:22 +0100

    Update and create dummy payment content and pages

--- e2e/fixtures/manageCases/caseWorker/dummyPaymentAwp1Content.ts
+++ e2e/fixtures/manageCases/caseWorker/dummyPaymentAwp1Content.ts
@@ -1,13 +1,16 @@
-export enum DummyPaymentAwpContent {
+export enum DummyPaymentAwp1Content {
   pageTitle = "Dummy Payment for AwP",
   h2 = "FamilyMan ID:",
-  formLabel1 = "Payment Status",
+  formLabel1 = "Payment status",
   formLabel2 = "Payment service reference number",
   formHint1 = "Paid for confirmation, anything else for Failed",
   formHint2 = "Enter the service reference number you want to process for additional application",
-  paymentStatusConfirm = "Paid",
+  paymentStatusPaid = "Paid",
   paymentStatusFail = "Fail",
   paymentServiceRef = "123456789",
   submit = "Submit",
   prev = "Previous",
+  errorBanner = "There is a problem",
+  errorMessagePaymentService = "Payment service reference number is required",
+  errorMessagePaymentStatus = "Payment status is required",
 }
--- e2e/fixtures/manageCases/caseWorker/dummyPaymentAwpSubmitContent.ts
+++ e2e/fixtures/manageCases/caseWorker/dummyPaymentAwpSubmitContent.ts
@@ -1,12 +1,12 @@
-export enum DummyPaymentAwpPaid {
+export enum DummyPaymentAwpSubmitContent {
   pageTitle = "Dummy Payment for AwP",
   h2 = "Check your answers",
   text161 = "Check the information below carefully.",
   text162 = "Payment status",
-  text163 = "Paid",
-  text164 = "Change",
-  text165 = "Payment service reference number",
-  text166 = "123456789",
+  text163 = "Payment service reference number",
+  text164 = "123456789",
+  text16paymentStatusPaid = "Paid",
+  text16paymentStatusFail = "Fail",
   submit = "Submit",
   prev = "Previous",
 }
