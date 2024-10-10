5a9fcb3702d6cc8a6f740df300cfbf08944e6f9b
OLT2000 <olliethurston1@gmail.com>
2024-10-10 11:31:47 +0100

    PRL-6358: Added additional journeys for legal representation

---
+++ e2e/journeys/citizen/createCase/C100.ts
@@ -0,0 +1,53 @@
+import { Page } from "@playwright/test";
+import { AlternativeResolutionPage } from "../../../pages/c100ScreeningSections/alternativeResolutionPage";
+import { AlternativeRoutesPage } from "../../../pages/c100ScreeningSections/alternativeRoutesPage";
+import { LegalRepresentationPage } from "../../../pages/c100ScreeningSections/legalRepresentationPage";
+import {
+  LegalRepresentationApplicationPage
+} from "../../../pages/c100ScreeningSections/legalRepresentationApplicationPage";
+
+interface C100Options {
+  page: Page;
+  accessibilityTest: boolean;
+  errorMessaging: boolean;
+  c100ScreeningWrittenAgreementReview: boolean;
+  c100LegalRepresentation: boolean;
+}
+
+export class C100 {
+  public static async c100({
+    page,
+    accessibilityTest,
+    errorMessaging,
+    c100ScreeningWrittenAgreementReview,
+    c100LegalRepresentation
+  }: C100Options): Promise<void> {
+    // Start Pages
+    if (c100ScreeningWrittenAgreementReview) {
+      // Type Of Order Journey
+    } else {
+      await AlternativeResolutionPage.alternativeResolutionPage({
+        page,
+        accessibilityTest
+      });
+      await AlternativeRoutesPage.alternativeRoutesPage({
+        page,
+        accessibilityTest
+      });
+      await LegalRepresentationPage.legalRepresentationPage({
+        page,
+        accessibilityTest,
+        errorMessaging,
+        c100LegalRepresentation
+      });
+      if (c100LegalRepresentation) {
+        await LegalRepresentationApplicationPage.legalRepresentationApplicationPage({
+          page: page,
+          accessibilityTest: accessibilityTest,
+          errorMessaging: errorMessaging,
+          c100ApplicationCompletedForYou: false // false for this journey because otherwise the application ends
+        });
+      }
+    }
+  }
+}
