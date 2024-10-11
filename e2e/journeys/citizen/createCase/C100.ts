b37a5c76c7297c5a8f2c6e3724465af0a5690e90
OLT2000 <olliethurston1@gmail.com>
2024-10-11 15:39:09 +0100

    PRL-6416: added c100 Journey

---
+++ e2e/journeys/citizen/createCase/C100.ts
@@ -0,0 +1,50 @@
+import { Page } from "@playwright/test";
+
+interface C100Options {
+  page: Page;
+  accessibilityTest: boolean;
+  errorMessaging: boolean;
+  c100ScreeningWrittenAgreementReview: boolean; // If true -> Type Of Order
+}
+
+export class C100 {
+  public static async c100({
+    page,
+    accessibilityTest,
+    errorMessaging,
+    c100ScreeningWrittenAgreementReview,
+  }: C100Options): Promise<void> {
+    // PRL-6357
+    if (c100ScreeningWrittenAgreementReview) {
+      // Type Of Order Journey
+      // Consent Order Upload
+      // Urgency & Without Notice
+      // People
+      // Applicant Details
+      // Confidentiality C7
+      // Case Parties
+      // Other Proceedings
+      // C1A - Safety Concerns
+      // International Element
+      // Reasonable Adjustments
+      // Help with Fees
+    } else {
+      // PRL-6358 Screening 2 (Done in screening journey)
+      // PRL-6359 Screening 3 (done in screening journey)
+      // MIAM journey
+      // if children involved in emergency protection
+        // Other Proceedings
+        // Type Of Order
+        // Urgency & Without Notice
+        // People
+        // Confidentiality C7
+        // Case Parties
+        // C1A - Safety Concerns
+        // International Element
+        // Reasonable Adjustments
+        // Help with Fees
+      // else
+        // ?
+    }
+  }
+}
