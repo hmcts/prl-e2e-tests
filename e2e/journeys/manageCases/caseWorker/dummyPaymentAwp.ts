08f6759d7378a5972a207328bc0c3c32d914cedd
RobL681 <robert.lacey@justice.gov.uk>
2024-10-14 10:11:31 +0100

    Update journey and add test cases

--- e2e/journeys/manageCases/caseWorker/dummyPaymentAwp.ts
+++ e2e/journeys/manageCases/caseWorker/dummyPaymentAwp.ts
@@ -2,6 +2,7 @@ import { DummyPaymentAwp1Page } from "../../../pages/manageCases/caseWorker/dumm
 import { Page } from "@playwright/test";
 import { DummyPaymentAwpSubmitPage } from "../../../pages/manageCases/caseWorker/dummyPaymentAwpSubmitPage";
 import { Selectors } from "../../../common/selectors";
+import { Fl401StatementOfTruth } from "../createCase/FL401StatementOfTruth/fl401StatementOfTruth";
 
 interface DummyPaymentAwpParams {
   page: Page;
@@ -14,7 +15,7 @@ const nextStepDropdownSelector = "#next-step";
 
 enum NextStepSelectorContent {
   dummyPaymentAwp = "Dummy Payment for AwP",
-  go = "Go"
+  go = "Go",
 }
 
 export class DummyPaymentAwp {
@@ -24,9 +25,15 @@ export class DummyPaymentAwp {
     accessibilityTest,
     paymentStatusPaid,
   }: DummyPaymentAwpParams): Promise<void> {
-    await page.selectOption(`${nextStepDropdownSelector}`, `${NextStepSelectorContent.dummyPaymentAwp}`);
-    await page.waitForTimeout(2000);
-    await page.click(`${Selectors.button}:text-is("${NextStepSelectorContent.go}")`);
+    // submit a case before dummy payment Awp journey
+    await Fl401StatementOfTruth.fl401StatementOfTruth({
+      page: page,
+      accessibilityTest: false,
+      errorMessaging: false,
+      fl401YesNoToEverything: false,
+      subJourney: true,
+    });
+    await this.goToDummyPaymentAwp(page);
     await DummyPaymentAwp1Page.dummyPaymentAwp1Page(
       page,
       errorMessaging,
@@ -37,6 +44,17 @@ export class DummyPaymentAwp {
       page,
       accessibilityTest,
       paymentStatusPaid,
+    );
+  }
+
+  private static async goToDummyPaymentAwp(page: Page): Promise<void> {
+    await page.selectOption(
+      `${nextStepDropdownSelector}`,
+      `${NextStepSelectorContent.dummyPaymentAwp}`,
+    );
+    await page.waitForTimeout(2000);
+    await page.click(
+      `${Selectors.button}:text-is("${NextStepSelectorContent.go}")`,
     );
   }
 }
--- e2e/tests/manageCases/caseWorker/dummyPaymentAwp.spec.ts
+++ e2e/tests/manageCases/caseWorker/dummyPaymentAwp.spec.ts
@@ -1,22 +1,61 @@
 import { test } from "@playwright/test";
 import { DummyPaymentAwp } from "../../../journeys/manageCases/caseWorker/dummyPaymentAwp";
 import Config from "../../../config";
-import { Fl401StatementOfTruth } from "../../../journeys/manageCases/createCase/FL401StatementOfTruth/fl401StatementOfTruth";
 
 test.use({ storageState: Config.sessionStoragePath + "solicitor.json" });
 
-test.describe("test dummyAwpPayment", (): void => {
-  test(`test dummyAwpPayment`, async ({ page }): Promise<void> => {
-    await Fl401StatementOfTruth.fl401StatementOfTruth({
-      page: page,
-      accessibilityTest: false,
+test.describe("Dummy payment for AWP tests @manageCases", (): void => {
+  test(`Complete the Dummy payment for AWP action as a solicitor with the following options:
+  Not Accessibility testing,
+  Not Error message testing,
+  Payment status paid. @crossbrowserManageCases`, async ({
+    page,
+  }): Promise<void> => {
+    await DummyPaymentAwp.dummyPaymentAwp({
+      page,
       errorMessaging: false,
-      fl401YesNoToEverything: false,
-      subJourney: true,
+      accessibilityTest: false,
+      paymentStatusPaid: true,
     });
+  });
+
+  test(`Complete the Dummy payment for AWP action  as a solicitor with the following options:
+  Not Accessibility testing,
+  Not Error message testing,
+  Not Payment status paid. @crossbrowserManageCases`, async ({
+    page,
+  }): Promise<void> => {
+    await DummyPaymentAwp.dummyPaymentAwp({
+      page,
+      errorMessaging: false,
+      accessibilityTest: false,
+      paymentStatusPaid: false,
+    });
+  });
+
+  test(`Complete the Dummy payment for AWP action  as a solicitor with the following options:
+  Not Accessibility testing,
+  Error message testing,
+  Payment status is paid. @crossbrowserManageCases`, async ({
+    page,
+  }): Promise<void> => {
     await DummyPaymentAwp.dummyPaymentAwp({
       page,
       errorMessaging: true,
+      accessibilityTest: false,
+      paymentStatusPaid: true,
+    });
+  });
+
+  test(`Complete the Dummy payment for AWP action  as a solicitor with the following options:
+  Accessibility testing,
+  Not Error message testing,
+  Payment status is paid. @crossbrowserManageCases`, async ({
+    page,
+  }): Promise<void> => {
+    await DummyPaymentAwp.dummyPaymentAwp({
+      page,
+      errorMessaging: false,
       accessibilityTest: true,
       paymentStatusPaid: true,
     });
