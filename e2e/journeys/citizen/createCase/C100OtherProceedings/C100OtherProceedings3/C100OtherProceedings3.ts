510fec339325dd488e9a7af293dad0c901cfb971
scralfie <kellyalfie6@gmail.com>
2024-10-17 14:22:37 +0100

    Journey created for ticket and all OtherProceeding folders are nested inside one dir

--- e2e/journeys/citizen/createCase/C100OtherProceedings/C100OtherProceedings3/C100OtherProceedings3.ts
+++ e2e/journeys/citizen/createCase/C100OtherProceedings/C100OtherProceedings3/C100OtherProceedings3.ts
@@ -1,22 +1,10 @@
 import { Page } from "@playwright/test";
-import {
-  ChildAbductionOrderDetailsPage
-} from "../../../../../pages/citizen/createCase/C100/otherProceedings/otherProceedings3/childAbduction/childAbductionOrderDetailsPage";
-import {
-  ChildAbductionDocumentUploadPage
-} from "../../../../../pages/citizen/createCase/C100/otherProceedings/otherProceedings3/childAbduction/childAbductionDocumentUploadPage";
-import {
-  ContactOrderForDivorceOrderDetailsPage
-} from "../../../../../pages/citizen/createCase/C100/otherProceedings/otherProceedings3/contactOrderForDivorce/contactOrderForDivorceOrderDetailsPage";
-import {
-  ContactOrderForDivorceDocumentUploadPage
-} from "../../../../../pages/citizen/createCase/C100/otherProceedings/otherProceedings3/contactOrderForDivorce/contactOrderForDivorceDocumentUploadPage";
-import {
-  ContactOrderForAdoptionOrderDetailsPage
-} from "../../../../../pages/citizen/createCase/C100/otherProceedings/otherProceedings3/contactOrderForChildAbduction/contactOrderForChildAbductionOrderDetailsPage";
-import {
-  ContactOrderForAdoptionDocumentUploadPage
-} from "../../../../../pages/citizen/createCase/C100/otherProceedings/otherProceedings3/contactOrderForChildAbduction/contactOrderForChildAbductionDocumentUploadPage";
+import { ChildAbductionOrderDetailsPage } from "../../../../../pages/citizen/createCase/C100/otherProceedings/otherProceedings3/childAbduction/childAbductionOrderDetailsPage";
+import { ChildAbductionDocumentUploadPage } from "../../../../../pages/citizen/createCase/C100/otherProceedings/otherProceedings3/childAbduction/childAbductionDocumentUploadPage";
+import { ContactOrderForDivorceOrderDetailsPage } from "../../../../../pages/citizen/createCase/C100/otherProceedings/otherProceedings3/contactOrderForDivorce/contactOrderForDivorceOrderDetailsPage";
+import { ContactOrderForDivorceDocumentUploadPage } from "../../../../../pages/citizen/createCase/C100/otherProceedings/otherProceedings3/contactOrderForDivorce/contactOrderForDivorceDocumentUploadPage";
+import { ContactOrderForAdoptionOrderDetailsPage } from "../../../../../pages/citizen/createCase/C100/otherProceedings/otherProceedings3/contactOrderForChildAbduction/contactOrderForChildAbductionOrderDetailsPage";
+import { ContactOrderForAdoptionDocumentUploadPage } from "../../../../../pages/citizen/createCase/C100/otherProceedings/otherProceedings3/contactOrderForChildAbduction/contactOrderForChildAbductionDocumentUploadPage";
 
 interface C100OtherProceedings3Options {
   page: Page;
@@ -27,48 +15,57 @@ interface C100OtherProceedings3Options {
   yesNoContactOrderForDivorceOrderDetails: boolean;
 }
 
-
 export class C100OtherProceedings3 {
   public static async c100OtherProceedings3({
-                                              page,
-                                              accessibilityTest,
-                                              errorMessaging,
-                                              yesNoChildAbductionOrderDetails,
-                                              yesNoContactOrderForAdoptionOrderDetails,
-                                              yesNoContactOrderForDivorceOrderDetails
-                                            }: C100OtherProceedings3Options): Promise<void> {
+    page,
+    accessibilityTest,
+    errorMessaging,
+    yesNoChildAbductionOrderDetails,
+    yesNoContactOrderForAdoptionOrderDetails,
+    yesNoContactOrderForDivorceOrderDetails,
+  }: C100OtherProceedings3Options): Promise<void> {
     await ChildAbductionOrderDetailsPage.childAbductionOrderDetailsPage({
       page,
       accessibilityTest,
       errorMessaging,
       yesNoChildAbductionOrderDetails,
     });
-    await ChildAbductionDocumentUploadPage.childAbductionDocumentUploadPageOptions({
-      page,
-      accessibilityTest,
-      errorMessaging,
-    });
-    await ContactOrderForDivorceOrderDetailsPage.contactOrderForDivorceOrderDetailsPageOptions({
-      page,
-      accessibilityTest,
-      errorMessaging,
-      yesNoContactOrderForDivorceOrderDetails,
-    });
-    await ContactOrderForDivorceDocumentUploadPage.contactOrderForDivorceDocumentUploadPage({
-      page,
-      accessibilityTest,
-      errorMessaging,
-    });
-    await ContactOrderForAdoptionOrderDetailsPage.contactOrderForAdoptionOrderDetailsPageOptions({
-      page,
-      accessibilityTest,
-      errorMessaging,
-      yesNoContactOrderForAdoptionOrderDetails,
-    });
-    await ContactOrderForAdoptionDocumentUploadPage.contactOrderForAdoptionDocumentUploadPage({
-      page,
-      accessibilityTest,
-      errorMessaging,
-    });
+    await ChildAbductionDocumentUploadPage.childAbductionDocumentUploadPageOptions(
+      {
+        page,
+        accessibilityTest,
+        errorMessaging,
+      },
+    );
+    await ContactOrderForDivorceOrderDetailsPage.contactOrderForDivorceOrderDetailsPageOptions(
+      {
+        page,
+        accessibilityTest,
+        errorMessaging,
+        yesNoContactOrderForDivorceOrderDetails,
+      },
+    );
+    await ContactOrderForDivorceDocumentUploadPage.contactOrderForDivorceDocumentUploadPage(
+      {
+        page,
+        accessibilityTest,
+        errorMessaging,
+      },
+    );
+    await ContactOrderForAdoptionOrderDetailsPage.contactOrderForAdoptionOrderDetailsPageOptions(
+      {
+        page,
+        accessibilityTest,
+        errorMessaging,
+        yesNoContactOrderForAdoptionOrderDetails,
+      },
+    );
+    await ContactOrderForAdoptionDocumentUploadPage.contactOrderForAdoptionDocumentUploadPage(
+      {
+        page,
+        accessibilityTest,
+        errorMessaging,
+      },
+    );
   }
 }
