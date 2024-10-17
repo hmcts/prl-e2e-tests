c27a15669a8206674a3cfa857be49a743688b80e
scralfie <kellyalfie6@gmail.com>
2024-10-17 14:14:14 +0100

    all pages completed

---
+++ e2e/pages/citizen/createCase/C100/otherProceedings/otherProceedings3/contactOrderForChildAbduction/contactOrderForChildAbductionDocumentUploadPage.ts
@@ -0,0 +1,156 @@
+import { Selectors } from "../../../../../../../common/selectors";
+import AccessibilityTestHelper from "../../../../../../../common/accessibilityTestHelper";
+import { Page } from "@playwright/test";
+import { Helpers } from "../../../../../../../common/helpers";
+import config from "../../../../../../../config";
+import { CommonStaticText } from "../../../../../../../common/commonStaticText";
+import { ContactOrderForAdoptionDocumentUploadContent } from "../../../../../../../fixtures/citizen/createCase/C100/otherProceedings/otherProceedings3/contactOrderForAdoption/contactOrderForAdoptionDocumentUploadContent";
+
+interface ContactOrderForAdoptionDocumentUploadPageOptions {
+  page: Page;
+  accessibilityTest: boolean;
+  errorMessaging: boolean;
+}
+
+interface checkPageLoadsOptions {
+  page: Page;
+  accessibilityTest: boolean;
+}
+
+interface fillInFieldsOptions {
+  page: Page;
+}
+
+enum UniqueSelectors {
+  documentUpload = "#document",
+  uploadConfirmationSelector = ".govuk-summary-list__value",
+}
+
+export class ContactOrderForAdoptionDocumentUploadPage {
+  public static async contactOrderForAdoptionDocumentUploadPage({
+    page: page,
+    accessibilityTest: accessibilityTest,
+    errorMessaging: errorMessaging,
+  }: ContactOrderForAdoptionDocumentUploadPageOptions): Promise<void> {
+    await this.checkPageLoads({
+      page: page,
+      accessibilityTest: accessibilityTest,
+    });
+    if (errorMessaging) {
+      await this.triggerErrorMessages(page);
+    }
+    await this.fillInFields({
+      page: page,
+    });
+  }
+
+  private static async checkPageLoads({
+    page: page,
+    accessibilityTest: accessibilityTest,
+  }: checkPageLoadsOptions): Promise<void> {
+    await page.waitForSelector(
+      `${Selectors.GovukHeadingXL}:text-is("${ContactOrderForAdoptionDocumentUploadContent.pageTitle}")`,
+    );
+    await Promise.all([
+      Helpers.checkGroup(
+        page,
+        2,
+        ContactOrderForAdoptionDocumentUploadContent,
+        "p",
+        `${Selectors.p}`,
+      ),
+      Helpers.checkVisibleAndPresent(
+        page,
+        `${Selectors.Span}:text-is("${ContactOrderForAdoptionDocumentUploadContent.spanA}")`,
+        1,
+      ),
+      Helpers.checkVisibleAndPresent(
+        page,
+        `${Selectors.GovukLabel}:text-is("${ContactOrderForAdoptionDocumentUploadContent.formLabel}")`,
+        1,
+      ),
+    ]);
+    await page.click(
+      `${Selectors.Span}:text-is("${ContactOrderForAdoptionDocumentUploadContent.spanA}")`,
+    );
+    await Helpers.checkGroup(
+      page,
+      5,
+      ContactOrderForAdoptionDocumentUploadContent,
+      "li",
+      `${Selectors.li}`,
+    );
+    if (accessibilityTest) {
+      await AccessibilityTestHelper.run(page);
+    }
+  }
+
+  private static async triggerErrorMessages(page: Page): Promise<void> {
+    await page.click(
+      `${Selectors.button}:text-is("${CommonStaticText.paddedContinue}")`,
+    );
+    await Promise.all([
+      Helpers.checkVisibleAndPresent(
+        page,
+        `${Selectors.GovukErrorSummaryTitle}:text-is("${CommonStaticText.errorSummaryTitle}")`,
+        1,
+      ),
+      Helpers.checkVisibleAndPresent(
+        page,
+        `${Selectors.a}:text-is("${ContactOrderForAdoptionDocumentUploadContent.errorMessageChooseFile}")`,
+        1,
+      ),
+      Helpers.checkVisibleAndPresent(
+        page,
+        `${Selectors.ErrorMessage}:text-is("${ContactOrderForAdoptionDocumentUploadContent.errorMessageChooseFile}")`,
+        1,
+      ),
+    ]);
+    let fileInput = page.locator(`${UniqueSelectors.documentUpload}`);
+    await fileInput.setInputFiles(config.testOdtFile);
+
+    // # Not sure if you need to press the button
+    // await page.click(
+    //   `${Selectors.button}:text-is("${CareOrderDocumentUploadContent.uploadFile}")`,
+    // );
+
+    await Promise.all([
+      Helpers.checkVisibleAndPresent(
+        page,
+        `${Selectors.a}:text-is("${ContactOrderForAdoptionDocumentUploadContent.errorMessageUploadCorrectFile}")`,
+        1,
+      ),
+      Helpers.checkVisibleAndPresent(
+        page,
+        `${Selectors.ErrorMessage}:text-is("${ContactOrderForAdoptionDocumentUploadContent.errorMessageUploadCorrectFile}")`,
+        1,
+      ),
+    ]);
+  }
+
+  private static async fillInFields({
+    page: page,
+  }: fillInFieldsOptions): Promise<void> {
+    let fileInput = page.locator(`${UniqueSelectors.documentUpload}`);
+    await fileInput.setInputFiles(config.testPdfFile);
+    await page.click(
+      `${Selectors.button}:text-is("${ContactOrderForAdoptionDocumentUploadContent.uploadFile}")`,
+    );
+    await page.waitForSelector(
+      `${UniqueSelectors.uploadConfirmationSelector}`,
+      { timeout: 5000 },
+    );
+    const isUploaded = await page.isVisible(
+      `${UniqueSelectors.uploadConfirmationSelector}`,
+    );
+    expect(isUploaded).toBeTruthy();
+    await Helpers.checkVisibleAndPresent(
+      page,
+      `${Selectors.a}:text-is("${ContactOrderForAdoptionDocumentUploadContent.remove}")`,
+      1,
+    );
+    await page.click(
+      `${Selectors.button}:text-is("${CommonStaticText.paddedContinue}")`,
+    );
+  }
+}
