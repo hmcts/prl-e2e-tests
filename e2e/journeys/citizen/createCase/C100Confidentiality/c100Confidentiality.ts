c3ab3977cb37c1dfa184db49e27062e5c57801af
OLT2000 <olliethurston1@gmail.com>
2024-10-22 09:26:11 +0100

    PRL-6368: Update confidentiality journey

--- e2e/journeys/citizen/createCase/C100Confidentiality/c100Confidentiality.ts
+++ e2e/journeys/citizen/createCase/C100Confidentiality/c100Confidentiality.ts
@@ -2,6 +2,12 @@ import { Page } from "@playwright/test";
 import { yesNoDontKnow } from "../../../../common/types";
 import { DetailsKnowPage } from "../../../../pages/citizen/createCase/C100/confidentiality/detailsKnowPage";
 import { StartPage } from "../../../../pages/citizen/createCase/C100/confidentiality/startPage";
+import {
+  StartAlternativeContent
+} from "../../../../fixtures/citizen/createCase/C100/confidentiality/startAlternativeContent";
+import { StartAlternativePage } from "../../../../pages/citizen/createCase/C100/confidentiality/startAlternativePage";
+import { FeedbackPage } from "../../../../pages/citizen/createCase/C100/confidentiality/feedbackPage";
+import { FeedbackNoPage } from "../../../../pages/citizen/createCase/C100/confidentiality/feedbackNoPage";
 
 interface C100ConfidentialityOptions {
   page: Page;
@@ -25,11 +31,38 @@ export class C100Confidentiality {
       errorMessaging: errorMessaging,
       c100OthersKnowApplicantsContact: c100OthersKnowApplicantsContact,
     });
-    await StartPage.startPage({
-      page: page,
-      accessibilityTest: accessibilityTest,
-      errorMessaging: errorMessaging,
-      c100PrivateDetails: c100PrivateDetails,
-    });
+    if (c100OthersKnowApplicantsContact === 'yes') {
+      await StartPage.startPage({
+        page: page,
+        accessibilityTest: accessibilityTest,
+        errorMessaging: errorMessaging,
+        c100PrivateDetails: c100PrivateDetails,
+      });
+    } else if (
+      c100OthersKnowApplicantsContact === 'no' ||
+      c100OthersKnowApplicantsContact === 'dontKnow'
+    ) {
+      await StartAlternativePage.startAlternativePage({
+        page: page,
+        accessibilityTest: accessibilityTest,
+        errorMessaging: errorMessaging,
+        c100PrivateDetails: c100PrivateDetails
+      });
+    } else {
+      throw new Error(
+        `Unrecognised value for c100OthersKnowApplicantsContact: ${c100OthersKnowApplicantsContact}`
+      )
+    }
+    if (c100PrivateDetails) {
+      await FeedbackPage.feedbackPage({
+        page: page,
+        accessibilityTest: accessibilityTest
+      })
+    } else {
+      await FeedbackNoPage.feedbackNoPage({
+        page: page,
+        accessibilityTest: accessibilityTest
+      })
+    }
   }
 }
