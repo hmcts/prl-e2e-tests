7f5fe3d4b8f6da78b3db726381d479767b3d60a9
OLT2000 <olliethurston1@gmail.com>
2024-10-22 09:26:30 +0100

    PRL-6368: feedback yes

---
+++ e2e/fixtures/citizen/createCase/C100/confidentiality/feedbackContent.ts
@@ -0,0 +1,9 @@
+export enum FeedbackContent {
+  pageTitle = 'The court will keep your contact details private',
+  bodyM1 = 'You have told us you want to keep these contact details private',
+  bodyM2 = 'The court will hold this information securely and will not share it with anyone except Cafcass (Children and Family Court Advisory and Support Service) or Cafcass Cymru unless it is by order of the court.',
+  li1 = 'Address',
+  li2 = 'Telephone number',
+  li3 = 'Email',
+  headingM = 'What the court will do',
+}
