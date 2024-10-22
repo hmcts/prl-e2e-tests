7983abf8f8a10519ad19db74cf103da9eb2d04c3
OLT2000 <olliethurston1@gmail.com>
2024-10-22 09:26:21 +0100

    PRL-6368: feedback no

---
+++ e2e/fixtures/citizen/createCase/C100/confidentiality/feedbackNoContent.ts
@@ -0,0 +1,4 @@
+export enum FeedbackNoContent {
+  pageTitle = 'The court will not keep your contact details private',
+  bodyM = 'You have told us you do not want to keep your contact details private from the other people in this application.',
+}
