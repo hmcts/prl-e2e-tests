86fc64c0e917114ca5aeb1cec5afa55b34d53436
OLT2000 <olliethurston1@gmail.com>
2024-10-23 12:04:07 +0100

    PRL-6405: added passport amount error messages

--- e2e/pages/citizen/createCase/C100/safetyConcerns/passportAmountContent.ts
+++ e2e/pages/citizen/createCase/C100/safetyConcerns/passportAmountContent.ts
@@ -3,6 +3,10 @@ export enum PassportAmountContent {
   caption = 'Safety concerns',
   legendM = 'Do the children have more than one passport?',
   legendS = 'Who is in possession of the children’s passports?',
+  errorSummaryList1 = 'Select yes if the children have more than one passport',
+  errorSummaryList2 = "Specify who is in possession of the children's passports",
+  errorMessage1 = 'Select yes if the children have more than one passport',
+  errorMessage2 = "Specify who is in possession of the children's passports",
   otherErrorSummaryList = 'Please provide the details',
   otherErrorMessage = 'Please provide the details',
   otherDetails = 'Automated Other Passport Possession'
