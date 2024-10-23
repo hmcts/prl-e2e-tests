99735a35bde1499dbfb07ec354361772034c1d8e
OLT2000 <olliethurston1@gmail.com>
2024-10-23 11:55:06 +0100

    PRL-6405: passport office content

---
+++ e2e/pages/citizen/createCase/C100/safetyConcerns/passportOfficeContent.ts
@@ -0,0 +1,6 @@
+export enum PassportOfficeContent {
+  pageTitle = 'Do any of the children have a passport?',
+  caption = 'Safety concerns',
+  errorSummaryList = 'Select yes if any of the children have a passport',
+  errorMessage = 'Select yes if any of the children have a passport',
+}
