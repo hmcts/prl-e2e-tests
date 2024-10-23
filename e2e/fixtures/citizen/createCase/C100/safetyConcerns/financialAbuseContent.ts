49c548152e92b9ea494ec0e0a8ae6dce3921a93c
OLT2000 <olliethurston1@gmail.com>
2024-10-23 10:59:04 +0100

    PRL-6404: financial pom

---
+++ e2e/fixtures/citizen/createCase/C100/safetyConcerns/financialAbuseContent.ts
@@ -0,0 +1,6 @@
+export enum FinancialAbuseContent {
+  pageTitle = 'Briefly describe the financial abuse against the children if you feel able to',
+  behaviourDetails = "Automated Financial Behaviours to be aware of",
+  behaviourStartDate = "Automated Financial behaviour length",
+  seekHelpDetails = 'Automated Financial seek help details'
+}
---
+++ e2e/pages/citizen/createCase/C100/safetyConcerns/financialAbusePage.ts
@@ -0,0 +1,89 @@
+import AccessibilityTestHelper from "../../../../../common/accessibilityTestHelper";
+import { Page } from "@playwright/test";
+import { Selectors } from "../../../../../common/selectors";
+import { CommonStaticText } from "../../../../../common/commonStaticText";
+import { ReportAbuseHelpers } from "./reportAbuseHelpers";
+import {
+  reportAbuseCheckboxIDs, reportAbuseInputIDs
+} from "../../../../../journeys/citizen/createCase/C100SafetyConcerns/c100SafetyConcerns";
+import {
+  FinancialAbuseContent
+} from "../../../../../fixtures/citizen/createCase/C100/safetyConcerns/financialAbuseContent";
+
+interface FinancialAbusePageOptions {
+  page: Page;
+  accessibilityTest: boolean;
+  c100FinancialAbuseYesNoToAll: boolean;
+}
+
+interface FillInFieldsOptions {
+  page: Page;
+  c100FinancialAbuseYesNoToAll: boolean;
+}
+
+interface CheckPageLoadsOptions {
+  page: Page;
+  accessibilityTest: boolean;
+}
+
+export class FinancialAbusePage {
+  public static async financialAbusePage({
+    page,
+    accessibilityTest,
+    c100FinancialAbuseYesNoToAll,
+  }: FinancialAbusePageOptions): Promise<void> {
+    await this.checkPageLoads({
+      page,
+      accessibilityTest,
+    });
+    await this.fillInFields({
+      page,
+      c100FinancialAbuseYesNoToAll,
+    });
+  }
+
+  private static async checkPageLoads({
+   page,
+   accessibilityTest,
+  }: CheckPageLoadsOptions): Promise<void> {
+    await page.waitForSelector(
+      `${Selectors.GovukHeadingXL}:text-is("${FinancialAbuseContent.pageTitle}")`,
+    );
+    await ReportAbuseHelpers.checkStaticText(page);
+    if (accessibilityTest) {
+      await AccessibilityTestHelper.run(page);
+    }
+  }
+
+  private static async fillInFields({
+    page,
+    c100FinancialAbuseYesNoToAll,
+  }: FillInFieldsOptions): Promise<void> {
+    for (let checkbox of Object.values(reportAbuseCheckboxIDs)) {
+      await page.check(checkbox);
+    }
+    const textToFill: [string, string] = [
+      "behaviourDetails",
+      "behaviourStartDate",
+    ];
+    for (let key of textToFill) {
+      let inputKey = key as keyof typeof reportAbuseInputIDs;
+      let contentKey = key as keyof typeof FinancialAbuseContent;
+      await page.fill(reportAbuseInputIDs[inputKey], FinancialAbuseContent[contentKey]);
+    }
+    await ReportAbuseHelpers.ongoingBehaviourFields({
+      page: page,
+      c100ReportAbuseYesNoToAll: c100FinancialAbuseYesNoToAll,
+      inputIDs: reportAbuseInputIDs,
+    });
+    await ReportAbuseHelpers.seekHelpFields({
+      page: page,
+      c100ReportAbuseYesNoToAll: c100FinancialAbuseYesNoToAll,
+      inputIDs: reportAbuseInputIDs,
+      abuseContent: FinancialAbuseContent,
+    });
+    await page.click(
+      `${Selectors.button}:text-is("${CommonStaticText.continue}")`,
+    );
+  }
+}
