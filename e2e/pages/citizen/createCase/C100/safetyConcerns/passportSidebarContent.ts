dd21db592d0878fddaa0c8dedbf33c3870b3938a
OLT2000 <olliethurston1@gmail.com>
2024-10-23 11:54:31 +0100

    PRL-6405: Added common sidebar content

---
+++ e2e/pages/citizen/createCase/C100/safetyConcerns/passportSidebarContent.ts
@@ -0,0 +1,5 @@
+export enum PassportSidebarContent {
+  sidebarLink1 = 'Guidance on parental child abduction',
+  sidebarLink2 = 'Get help to return a child from abroad or arrange contact',
+  sidebarLink3 = 'Stop a child from getting a passport',
+}
