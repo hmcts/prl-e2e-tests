cf0c20517231a80195405bad88c2095912841916
OLT2000 <olliethurston1@gmail.com>
2024-10-23 10:59:27 +0100

    PRL-6404: journey

--- e2e/journeys/citizen/createCase/C100SafetyConcerns/c100SafetyConcerns.ts
+++ e2e/journeys/citizen/createCase/C100SafetyConcerns/c100SafetyConcerns.ts
@@ -2,12 +2,32 @@ import { Page } from "@playwright/test";
 import { ConcernGuidancePage } from "../../../../pages/citizen/createCase/C100/safetyConcerns/concernGuidancePage";
 import { ConcernsForSafetyPage } from "../../../../pages/citizen/createCase/C100/safetyConcerns/concernsForSafetyPage";
 import { ConcernAboutPage } from "../../../../pages/citizen/createCase/C100/safetyConcerns/concernAboutPage";
+import { EmotionalAbusePage } from "../../../../pages/citizen/createCase/C100/safetyConcerns/emotionalAbusePage";
+import { SexualAbusePage } from "../../../../pages/citizen/createCase/C100/safetyConcerns/sexualAbusePage";
+import { FinancialAbusePage } from "../../../../pages/citizen/createCase/C100/safetyConcerns/financialAbusePage";
+
+export enum reportAbuseCheckboxIDs {
+  child1 = "#childrenConcernedAbout",
+}
+
+export enum reportAbuseInputIDs {
+  ongoingBehaviorYes = "#isOngoingBehaviour",
+  ongoingBehaviorNo = "#isOngoingBehaviour-2",
+  seekHelpYes = "#seekHelpFromPersonOrAgency",
+  seekHelpNo = "#seekHelpFromPersonOrAgency-2",
+  behaviourDetails = "#behaviourDetails",
+  behaviourStartDate = "#behaviourStartDate",
+  seekHelpDetails = "#seekHelpDetails",
+}
 
 interface C100SafetyConcernsOptions {
   page: Page;
   accessibilityTest: boolean;
   errorMessaging: boolean;
   c100ChildrenSafetyConcerns: boolean;
+  c100EmotionalAbuseYesNoToAll: boolean;
+  c100SexualAbuseYesNoToAll: boolean;
+  c100FinancialAbuseYesNoToAll: boolean
 }
 
 export class C100SafetyConcerns {
@@ -16,6 +36,9 @@ export class C100SafetyConcerns {
     accessibilityTest,
     errorMessaging,
     c100ChildrenSafetyConcerns,
+    c100EmotionalAbuseYesNoToAll,
+    c100SexualAbuseYesNoToAll,
+    c100FinancialAbuseYesNoToAll
   }: C100SafetyConcernsOptions): Promise<void> {
     await ConcernGuidancePage.concernGuidancePage({
       page: page,
@@ -32,6 +55,22 @@ export class C100SafetyConcerns {
         page: page,
         accessibilityTest: accessibilityTest,
         errorMessaging: errorMessaging,
+      });
+      // PRL-6401
+      await EmotionalAbusePage.emotionalAbusePage({
+        page: page,
+        accessibilityTest: accessibilityTest,
+        c100EmotionalAbuseYesNoToAll: c100EmotionalAbuseYesNoToAll
+      });
+      await SexualAbusePage.sexualAbusePage({
+        page: page,
+        accessibilityTest: accessibilityTest,
+        c100SexualAbuseYesNoToAll: c100SexualAbuseYesNoToAll
+      });
+      await FinancialAbusePage.financialAbusePage({
+        page: page,
+        accessibilityTest: accessibilityTest,
+        c100FinancialAbuseYesNoToAll: c100FinancialAbuseYesNoToAll
       });
     }
   }
