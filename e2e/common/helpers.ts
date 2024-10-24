5f51fb8cfa547a14fbcabde97b40a4d827d86bdc
scralfie <kellyalfie6@gmail.com>
2024-10-24 15:23:41 +0100

    Added functionality to look for the current date

--- e2e/common/helpers.ts
+++ e2e/common/helpers.ts
@@ -114,8 +114,8 @@ export class Helpers {
 
   public static getCurrentDateFormatted(): string {
     const today = new Date();
-    const day = String(today.getDate()).padStart(2, '0');
-    const month = String(today.getMonth() + 1).padStart(2, '0');
+    const day = String(today.getDate()).padStart(2, "0");
+    const month = String(today.getMonth() + 1).padStart(2, "0");
     const year = today.getFullYear();
     return `${day}${month}${year}`;
   }
