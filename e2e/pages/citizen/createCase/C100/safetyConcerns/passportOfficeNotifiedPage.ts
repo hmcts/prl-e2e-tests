dd2c582276fdcf28cf2f5b65502fdd25042bd1f9
OLT2000 <olliethurston1@gmail.com>
2024-10-25 10:20:18 +0100

    PRL-6406: passport office notified

---
+++ e2e/pages/citizen/createCase/C100/safetyConcerns/passportOfficeNotifiedPage.ts
@@ -0,0 +1,118 @@
+import { Page } from "@playwright/test";
+import AccessibilityTestHelper from "../../../../../common/accessibilityTestHelper";
+import { Selectors } from "../../../../../common/selectors";
+import { PassportOfficeNotifiedContent } from "../../../../../fixtures/citizen/createCase/C100/safetyConcerns/passportOfficeNotifiedContent";
+import { Helpers } from "../../../../../common/helpers";
+import { CommonStaticText } from "../../../../../common/commonStaticText";
+import { SafetyConcernHelpers } from "./safetyConcernHelpers";
+
+enum radioIDs {
+  radioYes = "#c1A_abductionPassportOfficeNotifiedNotified",
+  radioNo = "#c1A_abductionPassportOfficeNotifiedNotified-2",
+}
+
+interface PassportOfficeNotifiedPageOptions {
+  page: Page;
+  accessibilityTest: boolean;
+  errorMessaging: boolean;
+  c100PassportOfficeNotified: boolean;
+}
+
+interface FillInFieldsOptions {
+  page: Page;
+  c100PassportOfficeNotified: boolean;
+}
+
+interface CheckPageLoadsOptions {
+  page: Page;
+  accessibilityTest: boolean;
+}
+
+export class PassportOfficeNotifiedPage {
+  public static async passportOfficeNotifiedPage({
+                                           page,
+                                           accessibilityTest,
+                                           errorMessaging,
+                                           c100PassportOfficeNotified,
+                                         }: PassportOfficeNotifiedPageOptions): Promise<void> {
+    await this.checkPageLoads({
+      page: page,
+      accessibilityTest: accessibilityTest,
+    });
+    if (errorMessaging) {
+      await this.checkErrorMessaging(page);
+    }
+    await this.fillInFields({
+      page: page,
+      c100PassportOfficeNotified: c100PassportOfficeNotified,
+    });
+  }
+
+  private static async checkPageLoads({
+                                        page,
+                                        accessibilityTest,
+                                      }: CheckPageLoadsOptions): Promise<void> {
+    await page.waitForSelector(
+      `${Selectors.GovukHeadingXL}:text-is("${PassportOfficeNotifiedContent.pageTitle}")`,
+    );
+    await Promise.all([
+      Helpers.checkVisibleAndPresent(
+        page,
+        `${Selectors.GovukCaptionXL}:text-is("${PassportOfficeNotifiedContent.caption}")`,
+        1,
+      ),
+      Helpers.checkVisibleAndPresent(
+        page,
+        `${Selectors.GovukLabel}:text-is("${CommonStaticText.yes}")`,
+        1,
+      ),
+      Helpers.checkVisibleAndPresent(
+        page,
+        `${Selectors.GovukLabel}:text-is("${CommonStaticText.no}")`,
+        1,
+      ),
+    ]);
+    await SafetyConcernHelpers.checkPassportSidebar(page);
+    await SafetyConcernHelpers.checkContactDetailsText(page);
+    if (accessibilityTest) {
+      await AccessibilityTestHelper.run(page);
+    }
+  }
+
+  private static async checkErrorMessaging(page: Page): Promise<void> {
+    await page.click(
+      `${Selectors.button}:text-is("${CommonStaticText.continue}")`,
+    );
+    await Promise.all([
+      Helpers.checkVisibleAndPresent(
+        page,
+        `${Selectors.GovukErrorSummaryTitle}:text-is("${CommonStaticText.errorSummaryTitle}")`,
+        1,
+      ),
+      Helpers.checkVisibleAndPresent(
+        page,
+        `${Selectors.GovukErrorList} ${Selectors.a}:text-is("${PassportOfficeNotifiedContent.errorSummaryList}")`,
+        1,
+      ),
+      Helpers.checkVisibleAndPresent(
+        page,
+        `${Selectors.ErrorMessage}:text-is("${PassportOfficeNotifiedContent.errorMessage}")`,
+        1,
+      ),
+    ]);
+  }
+
+  private static async fillInFields({
+                                      page,
+                                      c100PassportOfficeNotified,
+                                    }: FillInFieldsOptions): Promise<void> {
+    if (c100PassportOfficeNotified) {
+      await page.click(radioIDs.radioYes);
+    } else {
+      await page.click(radioIDs.radioNo);
+    }
+    await page.click(
+      `${Selectors.button}:text-is("${CommonStaticText.continue}")`,
+    );
+  }
+}
