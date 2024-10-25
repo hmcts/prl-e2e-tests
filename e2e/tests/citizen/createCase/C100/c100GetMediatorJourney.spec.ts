44868160c5183ea5bb383289f27656e9ca69fd6b
OLT2000 <olliethurston1@gmail.com>
2024-10-25 11:35:34 +0100

    PRL-6416: Added missing journey

--- e2e/tests/citizen/createCase/C100/c100GetMediatorJourney.spec.ts
+++ e2e/tests/citizen/createCase/C100/c100GetMediatorJourney.spec.ts
@@ -103,25 +103,17 @@ test.describe("Create Citizen Application but you must get a mediator. @citizenF
     });
   });
 
-  test(`MIAM testing  with no previously attended MIAM, Redirects to Get Mediator
+  test(`MIAM testing  with no previously attended MIAM, redirects to Get Mediator
   With a valid reason for attending
   With all general exemptions
   But no valid reasons
   But accessing a mediator with no valid reason
-  Error message testing,
+  error message testing,
   Not accessibility Testing`, async ({ page }): Promise<void> => {
-    await C100.c100({
+    await C100.getMediatorJourney({
       page: page,
       accessibilityTest: false,
       errorMessaging: true,
-      c100ScreeningWrittenAgreementReview: false,
-      c100LegalRepresentation: false,
-      c100CourtPermissionNeeded: true,
-      c100OthersKnowApplicantsContact: "yes",
-      c100PrivateDetails: false,
-      c100ChildrenSafetyConcerns: false,
-      miamChildrenInvolvedOtherProceedings: false,
-      urgencyAndWithoutNoticeAllOptionsYes: true,
       miamAlreadyAttended: false,
       documentSignedByMediator: false,
       miamValidReasonNoAttendance: true,
@@ -133,6 +125,29 @@ test.describe("Create Citizen Application but you must get a mediator. @citizenF
       miamAttendanceType: "None of these",
       miamPreviousAttendanceMediatorSignedDocument: false,
       miamOtherReasonForNotAttending: "Cannot access mediator",
+      miamReasonForNoAccessToMediator: "None of these",
+    });
+  });
+
+  test(`MIAM testing  with no previously attended MIAM. Redirect to Get Mediator
+  With no valid reason for attending
+  error message testing,
+  Not accessibility Testing`, async ({ page }): Promise<void> => {
+    await C100.getMediatorJourney({
+      page: page,
+      accessibilityTest: false,
+      errorMessaging: true,
+      miamAlreadyAttended: false,
+      documentSignedByMediator: false,
+      miamValidReasonNoAttendance: false,
+      miamGeneralExemptions: false,
+      miamDomesticAbuse: false,
+      miamDomesticAbuseProvidingEvidence: false,
+      miamChildProtectionConcernsType: "None of the above",
+      miamUrgencyType: "None of these",
+      miamAttendanceType: "None of these",
+      miamPreviousAttendanceMediatorSignedDocument: false,
+      miamOtherReasonForNotAttending: "None of the above",
       miamReasonForNoAccessToMediator: "None of these",
     });
   });
