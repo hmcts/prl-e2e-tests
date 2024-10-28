1ad2efaf9893171ce98c8def0e80caaf87cdb362
scralfie <kellyalfie6@gmail.com>
2024-10-28 11:18:27 +0000

    Page and contents skeleton code complete

--- e2e/fixtures/citizen/createCase/C100/reasonableAdjustments/currentBranch/reasonableAdjustmentsCommunicationHelpContent.ts
+++ e2e/fixtures/citizen/createCase/C100/reasonableAdjustments/currentBranch/reasonableAdjustmentsCommunicationHelpContent.ts
@@ -1,3 +1,1 @@
-export enum ReasonableAdjustmentsCommunicationHelpContent {
-
-}
+export enum ReasonableAdjustmentsCommunicationHelpContent {}
--- e2e/fixtures/citizen/createCase/C100/reasonableAdjustments/currentBranch/reasonableAdjustmentsDocumentsSupportContent.ts
+++ e2e/fixtures/citizen/createCase/C100/reasonableAdjustments/currentBranch/reasonableAdjustmentsDocumentsSupportContent.ts
@@ -1,3 +1,1 @@
-export enum ReasonableAdjustmentsDocumentsSupportContent {
-
-}
+export enum ReasonableAdjustmentsDocumentsSupportContent {}
