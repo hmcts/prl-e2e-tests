5080b8065432dd36a79c0cf75cb54ea38fec2499
OLT2000 <olliethurston1@gmail.com>
2024-10-28 10:45:54 +0000

    PRL-6408: sexual abuse

---
+++ e2e/pages/citizen/createCase/C100/safetyConcerns/yourselfConcerns/sexualAbuseYourselfPage.ts
@@ -0,0 +1,93 @@
+import AccessibilityTestHelper from "../../../../../../common/accessibilityTestHelper";
+import { Page } from "@playwright/test";
+import { Selectors } from "../../../../../../common/selectors";
+import { SexualAbuseContent } from "../../../../../../fixtures/citizen/createCase/C100/safetyConcerns/yourselfConcerns/sexualAbuseContent";
+import { CommonStaticText } from "../../../../../../common/commonStaticText";
+import { SafetyConcernHelpers } from "../safetyConcernHelpers";
+import {
+  reportAbuseCheckboxIDs,
+  reportAbuseInputIDs,
+} from "../../../../../../common/commonUniqueSelectors";
+
+interface SexualAbusePageOptions {
+  page: Page;
+  accessibilityTest: boolean;
+  c100SexualAbuseYesNoToAll: boolean;
+}
+
+interface FillInFieldsOptions {
+  page: Page;
+  c100SexualAbuseYesNoToAll: boolean;
+}
+
+interface CheckPageLoadsOptions {
+  page: Page;
+  accessibilityTest: boolean;
+}
+
+export class SexualAbuseYourselfPage {
+  public static async sexualAbusePage({
+                                          page,
+                                          accessibilityTest,
+                                          c100SexualAbuseYesNoToAll,
+                                        }: SexualAbusePageOptions): Promise<void> {
+    await this.checkPageLoads({
+      page,
+      accessibilityTest,
+    });
+    await this.fillInFields({
+      page,
+      c100SexualAbuseYesNoToAll,
+    });
+  }
+
+  private static async checkPageLoads({
+                                        page,
+                                        accessibilityTest,
+                                      }: CheckPageLoadsOptions): Promise<void> {
+    await page.waitForSelector(
+      `${Selectors.GovukHeadingXL}:text-is("${SexualAbuseContent.pageTitle}")`,
+    );
+    await SafetyConcernHelpers.checkStaticTextYourself(page);
+    await SafetyConcernHelpers.checkSidebarYourself(page);
+    await SafetyConcernHelpers.checkContactDetailsText(page);
+    if (accessibilityTest) {
+      await AccessibilityTestHelper.run(page);
+    }
+  }
+
+  private static async fillInFields({
+                                      page,
+                                      c100SexualAbuseYesNoToAll,
+                                    }: FillInFieldsOptions): Promise<void> {
+    for (let checkbox of Object.values(reportAbuseCheckboxIDs)) {
+      await page.check(checkbox);
+    }
+    const textToFill: [string, string] = [
+      "behaviourDetails",
+      "behaviourStartDate",
+    ];
+    for (let key of textToFill) {
+      let inputKey = key as keyof typeof reportAbuseInputIDs;
+      let contentKey = key as keyof typeof SexualAbuseContent;
+      await page.fill(
+        reportAbuseInputIDs[inputKey],
+        SexualAbuseContent[contentKey],
+      );
+    }
+    await SafetyConcernHelpers.ongoingBehaviourFieldsYourself({
+      page: page,
+      c100ReportAbuseYesNoToAll: c100SexualAbuseYesNoToAll,
+      inputIDs: reportAbuseInputIDs,
+    });
+    await SafetyConcernHelpers.seekHelpFieldsYourself({
+      page: page,
+      c100ReportAbuseYesNoToAll: c100SexualAbuseYesNoToAll,
+      inputIDs: reportAbuseInputIDs,
+      abuseContent: SexualAbuseContent,
+    });
+    await page.click(
+      `${Selectors.button}:text-is("${CommonStaticText.continue}")`,
+    );
+  }
+}
