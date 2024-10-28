aecb6190d69872b169ce1e3763924d5cd34e5d06
OLT2000 <olliethurston1@gmail.com>
2024-10-28 13:38:13 +0000

    PRL-6409: other issues

---
+++ e2e/pages/citizen/createCase/C100/safetyConcerns/otherIssuesPage.ts
@@ -0,0 +1,169 @@
+import { Page } from "@playwright/test";
+import { Selectors } from "../../../../../common/selectors";
+import {
+  OtherIssuesContent
+} from "../../../../../fixtures/citizen/createCase/C100/safetyConcerns/otherIssuesContent";
+import { Helpers } from "../../../../../common/helpers";
+import { CommonStaticText } from "../../../../../common/commonStaticText";
+import AccessibilityTestHelper from "../../../../../common/accessibilityTestHelper";
+import { SafetyConcernHelpers } from "./safetyConcernHelpers";
+
+enum inputIDs {
+  radioYes = "#c1A_childSafetyConcerns",
+  radioNo = "#c1A_childSafetyConcerns-2",
+  otherDetails = "#c1A_childSafetyConcernsDetails"
+}
+
+interface OtherIssuesPageOptions {
+  page: Page;
+  accessibilityTest: boolean;
+  errorMessaging: boolean;
+  c100YesNoOtherIssues: boolean
+}
+
+interface CheckPageLoadsOptions {
+  page: Page;
+  accessibilityTest: boolean;
+}
+
+interface FillInFieldsOptions {
+  page: Page;
+  c100YesNoOtherIssues: boolean
+}
+
+export class OtherIssuesPage {
+  public static async otherIssuesPage({
+                                         page,
+                                         accessibilityTest,
+                                         errorMessaging,
+                                         c100YesNoOtherIssues
+                                       }: OtherIssuesPageOptions): Promise<void> {
+    await this.checkPageLoads({
+      page: page,
+      accessibilityTest: accessibilityTest
+    })
+    if (errorMessaging) {
+      await this.checkErrorMessaging(page)
+    }
+    await this.fillInFields({
+      page: page,
+      c100YesNoOtherIssues: c100YesNoOtherIssues
+    });
+  }
+
+  private static async checkPageLoads({
+                                        page,
+                                        accessibilityTest
+                                      }: CheckPageLoadsOptions): Promise<void> {
+    await page.waitForSelector(
+      `${Selectors.GovukHeadingXL}:text-is("${OtherIssuesContent.pageTitle}")`
+    );
+    await Promise.all([
+      Helpers.checkVisibleAndPresent(
+        page,
+        `${Selectors.GovukHint}:text-is("${OtherIssuesContent.formHint}")`,
+        1
+      ),
+      Helpers.checkVisibleAndPresent(
+        page,
+        `${Selectors.GovukCaptionXL}:text-is("${OtherIssuesContent.caption}")`,
+        1
+      ),
+      Helpers.checkVisibleAndPresent(
+        page,
+        `${Selectors.GovukLabel}:text-is("${CommonStaticText.yes}")`,
+        1
+      ),
+      Helpers.checkVisibleAndPresent(
+        page,
+        `${Selectors.GovukLabel}:text-is("${CommonStaticText.no}")`,
+        1
+      ),
+    ]);
+    await SafetyConcernHelpers.checkContactDetailsText(page);
+    if (accessibilityTest) {
+      await AccessibilityTestHelper.run(page)
+    }
+  }
+
+  private static async checkErrorMessaging(
+    page: Page
+  ): Promise<void> {
+    await page.click(
+      `${Selectors.button}:text-is("${CommonStaticText.continue}")`
+    );
+    await Promise.all([
+      Helpers.checkVisibleAndPresent(
+        page,
+        `${Selectors.GovukErrorSummaryTitle}:text-is("${CommonStaticText.errorSummaryTitle}")`,
+        1
+      ),
+      Helpers.checkVisibleAndPresent(
+        page,
+        `${Selectors.GovukErrorList} ${Selectors.a}:text-is("${OtherIssuesContent.errorSummaryList}")`,
+        1
+      ),
+      Helpers.checkVisibleAndPresent(
+        page,
+        `${Selectors.GovukErrorMessageCitizen}:text-is("${OtherIssuesContent.errorMessage}")`,
+        1
+      ),
+    ]);
+    await this.checkNestedErrorMessaging(page)
+  }
+
+  private static async checkNestedErrorMessaging(
+    page: Page
+  ): Promise<void> {
+    await page.click(
+      inputIDs.radioYes
+    );
+    await page.click(
+      `${Selectors.button}:text-is("${CommonStaticText.continue}")`
+    );
+    await Promise.all([
+      Helpers.checkVisibleAndPresent(
+        page,
+        `${Selectors.GovukErrorSummaryTitle}:text-is("${CommonStaticText.errorSummaryTitle}")`,
+        1
+      ),
+      Helpers.checkVisibleAndPresent(
+        page,
+        `${Selectors.GovukErrorList} ${Selectors.a}:text-is("${OtherIssuesContent.descriptionErrorSummaryList}")`,
+        1
+      ),
+      Helpers.checkVisibleAndPresent(
+        page,
+        `${Selectors.GovukErrorMessageCitizen}:text-is("${OtherIssuesContent.descriptionErrorMessage}")`,
+        1
+      ),
+    ])
+  }
+
+  private static async fillInFields({
+                                      page,
+                                      c100YesNoOtherIssues
+                                    }: FillInFieldsOptions): Promise<void> {
+    if (c100YesNoOtherIssues) {
+      await page.click(
+        inputIDs.radioYes
+      );
+      await Helpers.checkVisibleAndPresent(
+        page,
+        `${Selectors.GovukLabel}:text-is("${OtherIssuesContent.descriptionLabel}")`,
+        1
+      );
+      await page.fill(
+        inputIDs.otherDetails,
+        OtherIssuesContent.otherDetails
+      )
+    } else {
+      await page.click(
+        inputIDs.radioNo
+      )
+    }
+    await page.click(
+      `${Selectors.button}:text-is("${CommonStaticText.continue}")`
+    );
+  }
+}
