edd14a765769ced78176326a64b226af41e06e4b
JakeCG <jakecgowler@gmail.com>
2024-10-28 09:46:38 +0000

    PRL-6411 (#105)
    prl-6411

---
+++ e2e/journeys/citizen/createCase/C100/subJourneys/c100ReasonableAdjustments.ts
@@ -0,0 +1,45 @@
+import { Page } from "@playwright/test";
+import { ReasonableAdjustmentsAttendingCourtPage } from "../../../../../pages/citizen/createCase/C100/reasonableAdjustments/reasonableAdjustmentsAttendingCourtPage";
+import { ReasonableAdjustmentsLanguageRequirementsPage } from "../../../../../pages/citizen/createCase/C100/reasonableAdjustments/reasonableAdjustmentsLanguageRequirementsPage";
+import { ReasonableAdjustmentsSpecialArrangementsPage } from "../../../../../pages/citizen/createCase/C100/reasonableAdjustments/reasonableAdjustmentsSpecialArrangementsPage";
+
+interface C100ReasonableAdjustmentsOptions {
+  page: Page;
+  accessibilityTest: boolean;
+  errorMessaging: boolean;
+  yesNoReasonableAdjustments: boolean;
+}
+
+export class C100ReasonableAdjustments {
+  public static async c100ReasonableAdjustments({
+    page: page,
+    accessibilityTest: accessibilityTest,
+    errorMessaging: errorMessaging,
+    yesNoReasonableAdjustments: yesNoReasonableAdjustments,
+  }: C100ReasonableAdjustmentsOptions): Promise<void> {
+    await ReasonableAdjustmentsAttendingCourtPage.reasonableAdjustmentsAttendingCourtPage(
+      {
+        page: page,
+        accessibilityTest: accessibilityTest,
+        errorMessaging: errorMessaging,
+        yesNoReasonableAdjustments: yesNoReasonableAdjustments,
+      },
+    );
+    await ReasonableAdjustmentsLanguageRequirementsPage.reasonableAdjustmentsAttendingCourtPage(
+      {
+        page: page,
+        accessibilityTest: accessibilityTest,
+        errorMessaging: errorMessaging,
+        yesNoReasonableAdjustments: yesNoReasonableAdjustments,
+      },
+    );
+    await ReasonableAdjustmentsSpecialArrangementsPage.reasonableAdjustmentsSpecialArrangementsPage(
+      {
+        page: page,
+        accessibilityTest: accessibilityTest,
+        errorMessaging: errorMessaging,
+        yesNoReasonableAdjustments: yesNoReasonableAdjustments,
+      },
+    );
+  }
+}
