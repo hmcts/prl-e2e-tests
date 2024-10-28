5c116d3ac8e698423b55088ae9c9c0429383946d
OLT2000 <olliethurston1@gmail.com>
2024-10-28 16:40:38 +0000

    PRL-6415: help w fees

---
+++ e2e/fixtures/citizen/createCase/C100/helpWithFees/needHelpWithFeesContent.ts
@@ -0,0 +1,11 @@
+export enum NeedHelpWithFeesContent {
+  pageTitle = "Do you need help with paying the fee for this application?",
+  bodyM = "This application costs £255.00. You may be able to get help with paying the fee if you have little or no savings, and either:",
+  li1 = "get certain benefits",
+  li2 = "are on a low income",
+  link = "See if you are eligible for Help with Fees.",
+  formLabel1 = "Yes, I need help with paying the fee",
+  formLabel2 = "No, I do not need help",
+  errorSummaryList = "Select yes if you already applied for help with your application fee",
+  errorMessage = "Select yes if you already applied for help with your application fee",
+}
---
+++ e2e/pages/citizen/createCase/C100/helpWithFees/needHelpWithFeesPage.ts
@@ -0,0 +1,134 @@
+import { Page } from "@playwright/test";
+import { Selectors } from "../../../../../common/selectors";
+import {
+  NeedHelpWithFeesContent
+} from "../../../../../fixtures/citizen/createCase/C100/helpWithFees/needHelpWithFeesContent";
+import { Helpers } from "../../../../../common/helpers";
+import { SafetyConcernHelpers } from "../safetyConcerns/safetyConcernHelpers";
+import AccessibilityTestHelper from "../../../../../common/accessibilityTestHelper";
+import { CommonStaticText } from "../../../../../common/commonStaticText";
+
+enum inputIDs {
+  radioYes = "#hwf_needHelpWithFees",
+  radioNo = '#hwf_needHelpWithFees-2'
+}
+
+interface NeedHelpWithFeesPageOptions {
+  page: Page;
+  accessibilityTest: boolean;
+  errorMessaging: boolean;
+  c100YesNoNeedHelpWithFees: boolean;
+}
+
+interface FillInFieldsOptions {
+  page: Page;
+  c100YesNoNeedHelpWithFees: boolean;
+}
+
+interface CheckPageLoadsOptions {
+  page: Page;
+  accessibilityTest: boolean;
+}
+
+export class NeedHelpWithFeesPage {
+  public static async needHelpWithFeesPage({
+    page,
+    accessibilityTest,
+    errorMessaging,
+    c100YesNoNeedHelpWithFees
+  }: NeedHelpWithFeesPageOptions): Promise<void> {
+    await this.checkPageLoads({
+      page: page,
+      accessibilityTest: accessibilityTest
+    });
+    if (errorMessaging) {
+      await this.checkErrorMessaging(page)
+    }
+    await this.fillInFields({
+      page: page,
+      c100YesNoNeedHelpWithFees: c100YesNoNeedHelpWithFees
+    })
+  }
+
+  private static async checkPageLoads({
+    page,
+    accessibilityTest
+  }: CheckPageLoadsOptions): Promise<void> {
+    await page.waitForSelector(
+      `${Selectors.GovukHeadingXL}:text-is("${NeedHelpWithFeesContent.pageTitle}")`
+    );
+    await Promise.all([
+      Helpers.checkVisibleAndPresent(
+        page,
+        `${Selectors.GovukBodyM}:text-is("${NeedHelpWithFeesContent.bodyM}")`,
+        1
+      ),
+      Helpers.checkVisibleAndPresent(
+        page,
+        `${Selectors.a}:text-is("${NeedHelpWithFeesContent.link}")`,
+        1
+      ),
+      Helpers.checkGroup(
+        page,
+        2,
+        NeedHelpWithFeesContent,
+        'li',
+        Selectors.li
+      ),
+      Helpers.checkGroup(
+        page,
+        2,
+        NeedHelpWithFeesContent,
+        'formLabel',
+        Selectors.GovukLabel
+      )
+    ])
+    await SafetyConcernHelpers.checkContactDetailsText(page)
+    if (accessibilityTest) {
+      await AccessibilityTestHelper.run(page)
+    }
+  }
+
+  private static async checkErrorMessaging(
+    page: Page
+  ): Promise<void> {
+    await page.click(
+      `${Selectors.button}:text-is("${CommonStaticText.continue}")`
+    );
+    await Promise.all([
+      Helpers.checkVisibleAndPresent(
+        page,
+        `${Selectors.GovukErrorSummaryTitle}:text-is("${CommonStaticText.errorSummaryTitle}")`,
+        1
+      ),
+      Helpers.checkVisibleAndPresent(
+        page,
+        `${Selectors.GovukErrorList} ${Selectors.a}:text-is("${NeedHelpWithFeesContent.errorSummaryList}")`,
+        1
+      ),
+      Helpers.checkVisibleAndPresent(
+        page,
+        `${Selectors.GovukErrorMessageCitizen}:text-is("${NeedHelpWithFeesContent.errorMessage}")`,
+        1
+      ),
+    ])
+  }
+
+  private static async fillInFields({
+    page,
+    c100YesNoNeedHelpWithFees
+  }: FillInFieldsOptions): Promise<void> {
+    if (c100YesNoNeedHelpWithFees) {
+      await page.click(
+        inputIDs.radioYes
+      )
+    } else {
+      await page.click(
+        inputIDs.radioNo
+      )
+    }
+    await page.click(
+      `${Selectors.button}:text-is("${CommonStaticText.continue}}")`
+    );
+  }
+}
