0ab0e281b91129e061d266d85d3a13fa52fc349b
OLT2000 <olliethurston1@gmail.com>
2024-10-28 10:23:06 +0000

    PRL-6409: Added new content files

---
+++ e2e/pages/citizen/createCase/C100/safetyConcerns/courtActionContent.ts
@@ -0,0 +1,13 @@
+export enum CourtActionContent {
+  caption = "Safety concerns",
+  pageTitle = "What do you want the court to do to keep you and the children safe?",
+  detailsSummaryText = "Actions the court can take",
+  detailsStrong1 = "Non-molestation order:",
+  detailsStrong2 = "Prohibited Steps:",
+  detailsStrong3 = "Specific issue:",
+  detailsP1 = " The court may decide to make a non-molestation order. This requires the person served with the order not to be violent, or threaten violence. They must not harass or pester the person who applied for the order in any way. This includes harassing them over messaging apps or social media (Facebook, Twitter and so on).",
+  detailsP2 = " this order prevents a person from taking certain actions without the permission of the court.",
+  detailsP3 = " this order sets out a decision on specific issues, which could include medical treatment, education or a foreign holiday.",
+  errorSummaryList = "Describe what do you want the court to do to keep you and the children safe",
+  errorMessage = "Describe what do you want the court to do to keep you and the children safe"
+}
---
+++ e2e/pages/citizen/createCase/C100/safetyConcerns/drugConcernsContent.ts
@@ -0,0 +1,11 @@
+export enum DrugConcernsContent {
+  pageTitle = "Have the children been impacted by drug, alcohol or substance abuse?",
+  caption = 'Safety concerns',
+  body1 = 'This could be abuse that is taking place now, or abuse that occurred in the past.',
+  body2 = 'For example, you think the children are impacted by living with someone who has a substance abuse problem.',
+  descriptionLabel = "Describe in a few sentences the nature of the behaviour that you want the court to be aware of. Explain who is involved, and if the behaviour is ongoing.",
+  errorSummaryList1 = 'Select yes if the children have been impacted by drug, alcohol or substance abuse',
+  errorMessage = 'Select yes if the children have been impacted by drug, alcohol or substance abuse',
+  descriptionErrorList = "Describe how the children have been impacted by drug, alcohol or substance abuse",
+  descriptionErrorMessage = "Describe how the children have been impacted by drug, alcohol or substance abuse"
+}
