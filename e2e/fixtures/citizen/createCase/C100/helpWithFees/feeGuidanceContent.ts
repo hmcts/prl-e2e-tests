062931fdfc1ef45a0790442008a42a3fbd714d7c
OLT2000 <olliethurston1@gmail.com>
2024-10-28 16:40:26 +0000

    PRL-6415: fee guidance

---
+++ e2e/fixtures/citizen/createCase/C100/helpWithFees/feeGuidanceContent.ts
@@ -0,0 +1,13 @@
+export enum FeeGuidanceContent {
+  pageTitle = "You need to apply for help with your child arrangements application fee",
+  bodyM1 = "You need to ",
+  bodyM2 = " before you continue with this child arrangements application.",
+  bodyM3 = "After you have applied for help with fees, you will receive a reference number. Add this reference number below to proceed with the child arrangements application.",
+  link = "apply for help with fees (opens in a new tab)",
+  insetText = "Enter 'C100' when you are asked for your court or tribunal form number.",
+  formLabel = "Enter your help with fees reference number",
+  formHint = "For example, HWF-A1B-23C",
+  errorSummaryList = "Enter the help with fees reference number you received when you applied for help with fees",
+  errorMessage = "Enter the help with fees reference number you received when you applied for help with fees",
+  referenceNumber = "ABC-D0E-00F"
+}
