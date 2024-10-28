fc8aa9b63caa2e9435d24a65073aa7d7e97abf83
OLT2000 <olliethurston1@gmail.com>
2024-10-28 13:37:59 +0000

    PRL-6409: court action

--- e2e/fixtures/citizen/createCase/C100/safetyConcerns/courtActionContent.ts
+++ e2e/fixtures/citizen/createCase/C100/safetyConcerns/courtActionContent.ts
@@ -1,6 +1,7 @@
 export enum CourtActionContent {
   caption = "Safety concerns",
   pageTitle = "What do you want the court to do to keep you and the children safe?",
+  bodyL = "Describe what you want the court to do to keep you and the children safe. The court may be able to make a protective order.",
   detailsSummaryText = "Actions the court can take",
   detailsStrong1 = "Non-molestation order:",
   detailsStrong2 = "Prohibited Steps:",
@@ -9,5 +10,6 @@ export enum CourtActionContent {
   detailsP2 = " this order prevents a person from taking certain actions without the permission of the court.",
   detailsP3 = " this order sets out a decision on specific issues, which could include medical treatment, education or a foreign holiday.",
   errorSummaryList = "Describe what do you want the court to do to keep you and the children safe",
-  errorMessage = "Describe what do you want the court to do to keep you and the children safe"
+  errorMessage = "Describe what do you want the court to do to keep you and the children safe",
+  courtActionStatement = "Automated Safety Statement"
 }
