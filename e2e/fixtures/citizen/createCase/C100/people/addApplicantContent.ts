1494d6272c2af83def5a7b666150cd68a8b1923f
louisware <louis.ware@solirius.com>
2024-10-29 16:18:39 +0000

    Implement addApplicantPage

--- e2e/fixtures/citizen/createCase/C100/people/addApplicantContent.ts
+++ e2e/fixtures/citizen/createCase/C100/people/addApplicantContent.ts
@@ -1,1 +1,12 @@
-export enum AddApplicantContent {}
+export enum AddApplicantContent {
+    pageTitle = "Enter your name",
+    insetText1 = "You and anyone else making this application are known as the applicants.",
+    insetText2 = "The other people who will receive this application are known as the respondents. We will ask for their details later.",
+    heading = "Enter name",
+    hint = "Include all middle names here",
+    label1 = "First name(s)",
+    label2 = "Last name(s)",
+    errorLink1 = "Enter the first name",
+    errorLink2 = "Enter the last name",
+    exampleText = "exampleText",
+}
--- e2e/pages/citizen/createCase/C100/people/addApplicantPage.ts
+++ e2e/pages/citizen/createCase/C100/people/addApplicantPage.ts
@@ -20,6 +20,11 @@ interface fillInFieldsOptions {
   page: Page;
 }
 
+enum inputIds {
+  firstName = "#applicantFirstName",
+  lastName = "#applicantLastName",
+}
+
 export class AddApplicantPage {
   public static async addApplicantPage({
     page: page,
@@ -42,14 +47,75 @@ export class AddApplicantPage {
     page: page,
     accessibilityTest: accessibilityTest,
   }: checkPageLoadsOptions): Promise<void> {
+    await page.waitForSelector(
+        `${Selectors.GovukHeadingXL}:text-is("${AddApplicantContent.pageTitle}")`
+    );
+    await Promise.all([
+        Helpers.checkVisibleAndPresent(
+            page,
+            `${Selectors.GovukHeadingM}:text-is("${AddApplicantContent.heading}")`,
+            1,
+        ),
+      Helpers.checkVisibleAndPresent(
+          page,
+          `${Selectors.GovukHint}:text-is("${AddApplicantContent.hint}")`,
+          1,
+      ),
+        Helpers.checkGroup(
+            page,
+            2,
+            AddApplicantContent,
+            "insetText",
+            `${Selectors.GovukInsetText}`,
+        ),
+      Helpers.checkGroup(
+          page,
+          2,
+          AddApplicantContent,
+          "label",
+          `${Selectors.GovukLabel}`,
+      ),
+    ]);
     if (accessibilityTest) {
       await AccessibilityTestHelper.run(page);
     }
   }
 
-  private static async triggerErrorMessages(page: Page): Promise<void> {}
+  private static async triggerErrorMessages(page: Page): Promise<void> {
+    await page.click(
+        `${Selectors.GovukButton}:text-is("${CommonStaticText.continue}")`,
+    );
+    await Promise.all([
+      Helpers.checkVisibleAndPresent(
+          page,
+          `${Selectors.GovukErrorSummaryTitle}:text-is("${CommonStaticText.errorSummaryTitle}")`,
+          1,
+      ),
+      Helpers.checkGroup(
+          page,
+          2,
+          AddApplicantContent,
+          "errorLink",
+          `${Selectors.a}`,
+      ),
+      Helpers.checkGroup(
+          page,
+          2,
+          AddApplicantContent,
+          "errorLink",
+          `${Selectors.GovukErrorMessageCitizen}`,
+      ),
+    ]);
+  }
 
   private static async fillInFields({
     page: page,
-  }: fillInFieldsOptions): Promise<void> {}
+  }: fillInFieldsOptions): Promise<void> {
+    for (const selector of Object.values(inputIds)) {
+      await page.fill(`${selector}`, AddApplicantContent.exampleText);
+    }
+    await page.click(
+        `${Selectors.GovukButton}:text-is("${CommonStaticText.continue}")`,
+    );
+  }
 }
