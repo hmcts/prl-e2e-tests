8f250d133d345336fc8ad40b63abb05ab34c63a8
OLT2000 <olliethurston1@gmail.com>
2024-10-29 15:23:04 +0000

    PRL-6382: relationship

---
+++ e2e/fixtures/citizen/createCase/C100/casePartyDetails/otherPeople/otherPersonRelationshipContent.ts
@@ -0,0 +1,15 @@
+export enum OtherPersonRelationshipContent {
+  pageTitle1 = "What is",
+  pageTitle2 = "relationship to", // Dynamic
+  formLabel1 = "Mother",
+  formLabel2 = "Father",
+  formLabel3 = "Guardian",
+  formLabel4 = "Special Guardian",
+  formLabel5 = "Grandparent",
+  formLabel6 = "Other",
+  formHint1 = "Someone who represents the rights of a child, appointed by the court",
+  formHint2 = "Someone who represents the rights of a child, may be appointed by a parent, special guardian or the court",
+  otherFormLabel = "Please specify",
+  errorSummaryList = "Enter the relationship",
+  errorMessage = "Enter the relationship"
+}
---
+++ e2e/pages/citizen/createCase/C100/casePartyDetails/otherPeople/otherPersonRelationshipPage.ts
@@ -0,0 +1,150 @@
+import AccessibilityTestHelper from "../../../../../../common/accessibilityTestHelper";
+import { Page } from "@playwright/test";
+import { Selectors } from "../../../../../../common/selectors";
+import { Helpers } from "../../../../../../common/helpers";
+import { CommonStaticText } from "../../../../../../common/commonStaticText";
+import { Relationship } from "../../../../../../common/types";
+import {
+  OtherPersonRelationshipContent
+} from "../../../../../../fixtures/citizen/createCase/C100/casePartyDetails/otherPeople/otherPersonRelationshipContent";
+
+interface applicantPageOptions {
+  page: Page;
+  accessibilityTest: boolean;
+  errorMessaging: boolean;
+  relationship: Relationship;
+}
+
+interface checkPageLoadsOptions {
+  page: Page;
+  accessibilityTest: boolean;
+}
+
+interface fillInFieldsOptions {
+  page: Page;
+  relationship: Relationship;
+}
+
+enum inputIds {
+  mother = "#relationshipType",
+  father = "#relationshipType-2",
+  guardian = "#relationshipType-3",
+  specialGuardian = "#relationshipType-4",
+  grandparent = "#relationshipType-5",
+  other = "#relationshipType-6",
+}
+
+export class OtherPersonRelationshipPage {
+  public static async otherPersonRelationshipPage({
+                                                         page,
+                                                         accessibilityTest,
+                                                         errorMessaging,
+                                                         relationship,
+                                                       }: applicantPageOptions): Promise<void> {
+    await this.checkPageLoads({ page, accessibilityTest });
+    if (errorMessaging) {
+      await this.triggerErrorMessages(page);
+    }
+    await this.fillInFields({
+      page,
+      relationship,
+    });
+  }
+
+  private static async checkPageLoads({
+                                        page,
+                                        accessibilityTest,
+                                      }: checkPageLoadsOptions): Promise<void> {
+    await Promise.all([
+      //wait for two parts of the title to load, split by dynamic content
+      page.waitForSelector(
+        `${Selectors.GovukHeadingXL}:has-text("${OtherPersonRelationshipContent.pageTitle1}")`,
+      ),
+      page.waitForSelector(
+        `${Selectors.GovukHeadingXL}:has-text("${OtherPersonRelationshipContent.pageTitle2}")`,
+      ),
+    ]);
+
+    await Promise.all([
+      Helpers.checkGroup(
+        page,
+        6,
+        OtherPersonRelationshipContent,
+        "formLabel",
+        Selectors.GovukLabel,
+      ),
+      Helpers.checkGroup(
+        page,
+        2,
+        OtherPersonRelationshipContent,
+        "formHint",
+        Selectors.GovukHint,
+      ),
+    ]);
+    if (accessibilityTest) {
+      await AccessibilityTestHelper.run(page);
+    }
+  }
+
+  private static async triggerErrorMessages(page: Page): Promise<void> {
+    await page.click(
+      `${Selectors.GovukButton}:text-is("${CommonStaticText.continue}")`,
+    );
+    await Promise.all([
+      Helpers.checkVisibleAndPresent(
+        page,
+        `${Selectors.GovukErrorSummaryTitle}:text-is("${CommonStaticText.errorSummaryTitle}")`,
+        1,
+      ),
+      Helpers.checkVisibleAndPresent(
+        page,
+        `${Selectors.GovukErrorList} ${Selectors.a}:text-is("${OtherPersonRelationshipContent.errorMessage}")`,
+        1,
+      ),
+      Helpers.checkVisibleAndPresent(
+        page,
+        `${Selectors.GovukErrorMessage}:text-is("${OtherPersonRelationshipContent.errorMessage}")`,
+        1,
+      ),
+    ]);
+    await page.click(
+      inputIds.other
+    );
+    await page.click(
+      `${Selectors.GovukButton}:text-is("${CommonStaticText.continue}")`,
+    );
+    await Promise.all([
+      Helpers.checkVisibleAndPresent(
+        page,
+        `${Selectors.GovukErrorSummaryTitle}:text-is("${CommonStaticText.errorSummaryTitle}")`,
+        1,
+      ),
+      Helpers.checkVisibleAndPresent(
+        page,
+        `${Selectors.GovukErrorList} ${Selectors.a}:text-is("${OtherPersonRelationshipContent.errorMessage}")`,
+        1,
+      ),
+      Helpers.checkVisibleAndPresent(
+        page,
+        `${Selectors.GovukErrorMessage}:text-is("${OtherPersonRelationshipContent.errorMessage}")`,
+        1,
+      ),
+    ]);
+  }
+
+  private static async fillInFields({
+                                      page,
+                                      relationship,
+                                    }: fillInFieldsOptions): Promise<void> {
+    if (!(relationship in Object.keys(inputIds))) {
+      throw new Error(
+        `The value 'relationship' must be one of 'mother', 'father', 'guardian', 'specialGuardian', 'grandparent', 'other'. You used ${relationship}.`,
+      );
+    }
+    let inputKey = relationship as keyof typeof inputIds;
+    await page.click(inputIds[inputKey]);
+    await page.click(
+      `${Selectors.GovukButton}:text-is("${CommonStaticText.continue}")`,
+    );
+  }
+}
