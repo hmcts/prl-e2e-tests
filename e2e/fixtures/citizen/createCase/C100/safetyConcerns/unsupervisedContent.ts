e89310436a9aed42fc75eef9dc06fa96a40068d0
Ollie Thurston <65600338+OLT2000@users.noreply.github.com>
2024-10-29 09:56:53 +0000

    Prl 6409 (#113)
    * PRL-6409: court action

---
+++ e2e/fixtures/citizen/createCase/C100/safetyConcerns/unsupervisedContent.ts
@@ -0,0 +1,14 @@
+export enum UnsupervisedContent {
+  caption = "Safety concerns",
+  pageTitle = "Contact between the children and the other people in this application",
+  h1Label = "The court will presume it is good for the children's welfare to have both of their parents involved in their lives, unless there is evidence showing that this would cause harm to the children.",
+  legendM1 = "Do you agree to the children spending time with the other people in this application?",
+  legendM2 = "Do you agree to the other people in this application being in touch with the children in other ways?",
+  formLabel1 = "Yes, but I prefer that it is supervised",
+  formLabel2 = "No, I would prefer the other people do not spend time with the children",
+  formHint = "For example, by phone, text or email",
+  errorSummaryList1 = "Select whether you agree to the children spending time with the other people in this application",
+  errorSummaryList2 = "Select yes if you agree to the other people in this application being in touch with the children in other ways",
+  errorMessage1 = "Select whether you agree to the children spending time with the other people in this application",
+  errorMessage2 = "Select yes if you agree to the other people in this application being in touch with the children in other ways",
+}
