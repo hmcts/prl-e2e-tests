706febe260d2d3ee87e750704e2a7b95965f0bc2
Ollie Thurston <65600338+OLT2000@users.noreply.github.com>
2024-10-30 12:56:16 +0000

    Prl 6382 (#124)
    * PRL-6382: address lookup

---
+++ e2e/fixtures/citizen/createCase/C100/casePartyDetails/otherPeople/otherPersonAddressLookupContent.ts
@@ -0,0 +1,9 @@
+export enum OtherPersonAddressLookupContent {
+  pageTitle = "Address of", //name of applicant
+  label = "Current Postcode",
+  link = "I dont know their postcode or they live outside the UK",
+  postcodeText = "CF10 1NS",
+  errorMessage = "Enter the postcode",
+  invalidPostcodeError = "Enter a valid postcode",
+  insetText = "Documents relating to this application may be sent here",
+}
---
+++ e2e/pages/citizen/createCase/C100/casePartyDetails/otherPeople/otherPersonAddressLookupPage.ts
@@ -0,0 +1,115 @@
+import AccessibilityTestHelper from "../../../../../../common/accessibilityTestHelper";
+import { Page } from "@playwright/test";
+import { Selectors } from "../../../../../../common/selectors";
+import { OtherPersonAddressLookupContent } from "../../../../../../fixtures/citizen/createCase/C100/casePartyDetails/otherPeople/otherPersonAddressLookupContent";
+import { Helpers } from "../../../../../../common/helpers";
+import { CommonStaticText } from "../../../../../../common/commonStaticText";
+
+interface applicantAddressLookupOptions {
+  page: Page;
+  accessibilityTest: boolean;
+  errorMessaging: boolean;
+}
+
+interface checkPageLoadsOptions {
+  page: Page;
+  accessibilityTest: boolean;
+}
+
+const postcodeLookup: string = "#PostCode";
+
+const numericalPostcode: string = "0";
+
+export class OtherPersonAddressLookupPage {
+  public static async otherPersonAddressLookupPage({
+    page,
+    accessibilityTest,
+    errorMessaging,
+  }: applicantAddressLookupOptions): Promise<void> {
+    await this.checkPageLoads({ page, accessibilityTest });
+    if (errorMessaging) {
+      await this.triggerErrorMessages(page);
+    }
+    await this.fillInFields(page);
+  }
+
+  private static async checkPageLoads({
+    page,
+    accessibilityTest,
+  }: checkPageLoadsOptions): Promise<void> {
+    await page.waitForSelector(
+      `${Selectors.GovukHeadingL}:has-text("${OtherPersonAddressLookupContent.pageTitle}")`,
+    );
+    await Promise.all([
+      Helpers.checkVisibleAndPresent(
+        page,
+        `${Selectors.GovukLabel}:text-is("${OtherPersonAddressLookupContent.label}")`,
+        1,
+      ),
+      Helpers.checkVisibleAndPresent(
+        page,
+        `${Selectors.GovukLink}:text-is("${OtherPersonAddressLookupContent.link}")`,
+        1,
+      ),
+      Helpers.checkVisibleAndPresent(
+        page,
+        `${Selectors.GovukInsetText}:text-is("${OtherPersonAddressLookupContent.insetText}")`,
+        1,
+      ),
+    ]);
+    if (accessibilityTest) {
+      await AccessibilityTestHelper.run(page);
+    }
+  }
+
+  private static async triggerErrorMessages(page: Page): Promise<void> {
+    await page.click(
+      `${Selectors.GovukButton}:text-is("${CommonStaticText.continue}")`,
+    );
+    await Promise.all([
+      Helpers.checkVisibleAndPresent(
+        page,
+        `${Selectors.GovukErrorSummaryTitle}:text-is("${CommonStaticText.errorSummaryTitle}")`,
+        1,
+      ),
+      Helpers.checkVisibleAndPresent(
+        page,
+        `${Selectors.GovukErrorList} ${Selectors.a}:text-is("${OtherPersonAddressLookupContent.errorMessage}")`,
+        1,
+      ),
+      Helpers.checkVisibleAndPresent(
+        page,
+        `${Selectors.GovukErrorMessageCitizen}:text-is("${OtherPersonAddressLookupContent.errorMessage}")`,
+        1,
+      ),
+    ]);
+    await page.fill(postcodeLookup, numericalPostcode);
+    await Promise.all([
+      Helpers.checkVisibleAndPresent(
+        page,
+        `${Selectors.GovukErrorSummaryTitle}:text-is("${CommonStaticText.errorSummaryTitle}")`,
+        1,
+      ),
+      Helpers.checkVisibleAndPresent(
+        page,
+        `${Selectors.GovukErrorList} ${Selectors.a}:text-is("${OtherPersonAddressLookupContent.invalidPostcodeError}")`,
+        1,
+      ),
+      Helpers.checkVisibleAndPresent(
+        page,
+        `${Selectors.GovukErrorMessageCitizen}:text-is("${OtherPersonAddressLookupContent.invalidPostcodeError}")`,
+        1,
+      ),
+    ]);
+  }
+
+  private static async fillInFields(page: Page): Promise<void> {
+    await page.fill(
+      postcodeLookup,
+      OtherPersonAddressLookupContent.postcodeText,
+    );
+    await page.click(
+      `${Selectors.GovukButton}:text-is("${CommonStaticText.continue}")`,
+    );
+  }
+}
