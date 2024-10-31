69b4aabb388518ef75f68f7273b27a9deb059765
louisware <louis.ware@solirius.com>
2024-10-31 17:28:11 +0000

    Linting

--- e2e/fixtures/citizen/createCase/C100/casePartyDetails/otherPeople/livingArrangementsContent.ts
+++ e2e/fixtures/citizen/createCase/C100/casePartyDetails/otherPeople/livingArrangementsContent.ts
@@ -1,6 +1,6 @@
 export enum LivingArrangementsContent {
-    pageTitle = "'s living arrangements",
-    body = "We need this information so that the court has a complete understanding of the child’s living arrangements.",
-    legend = "Select all of the people that the child lives with",
-    firstNameLastName = "exampleText exampleText",
+  pageTitle = "'s living arrangements",
+  body = "We need this information so that the court has a complete understanding of the child’s living arrangements.",
+  legend = "Select all of the people that the child lives with",
+  firstNameLastName = "exampleText exampleText",
 }
