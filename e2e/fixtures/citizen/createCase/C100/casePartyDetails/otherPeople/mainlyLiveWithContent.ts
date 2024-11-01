d2a55e8bbfd6faad934ac65a5fc58d934e86635d
Louis-Solirius <148347068+Louis-Solirius@users.noreply.github.com>
2024-11-01 15:22:50 +0000

    PRL-6383 (#133)

---
+++ e2e/fixtures/citizen/createCase/C100/casePartyDetails/otherPeople/mainlyLiveWithContent.ts
@@ -0,0 +1,10 @@
+export enum MainlyLiveWithContent {
+  pageTitlePart1 = "Who does",
+  pageTitlePart2 = "mainly live with?",
+  firstNameLastName = "Other Person First Name Other Person Last Name",
+  hint1 = "Select the person that the child lives with most of the time.",
+  hint2 = "Address: PRINCIPALITY STADIUM, WESTGATE STREET, CARDIFF, CF10 1NS, CARDIFF, United Kingdom",
+  body = "If any of the addresses listed here are not correct, you must",
+  link = "update the address details.",
+  errorMessage = "Select the person the child lives with most of the time",
+}
---
+++ e2e/pages/citizen/createCase/C100/casePartyDetails/otherPeople/otherPersonManualPage.ts
@@ -0,0 +1,69 @@
+import AccessibilityTestHelper from "../../../../../../common/accessibilityTestHelper";
+import { Page } from "@playwright/test";
+import { Selectors } from "../../../../../../common/selectors";
+import { OtherPersonManualContent } from "../../../../../../fixtures/citizen/createCase/C100/casePartyDetails/otherPeople/otherPersonManualContent";
+import { Helpers } from "../../../../../../common/helpers";
+import { CommonStaticText } from "../../../../../../common/commonStaticText";
+
+interface otherPersonManualOptions {
+  page: Page;
+  accessibilityTest: boolean;
+  errorMessaging: boolean;
+}
+
+interface checkPageLoadsOptions {
+  page: Page;
+  accessibilityTest: boolean;
+}
+
+interface fillInFieldsOptions {
+  page: Page;
+  errorMessaging: boolean;
+}
+
+export class OtherPersonManualPage {
+  public static async otherPersonManualPage({
+    page,
+    accessibilityTest,
+    errorMessaging,
+  }: otherPersonManualOptions): Promise<void> {
+    await this.checkPageLoads({ page, accessibilityTest });
+    await this.fillInFields({
+      page,
+      errorMessaging,
+    });
+  }
+  private static async checkPageLoads({
+    page,
+    accessibilityTest,
+  }: checkPageLoadsOptions): Promise<void> {
+    await page.waitForSelector(
+      `${Selectors.GovukHeadingL}:text-is("${OtherPersonManualContent.pageTitle} ${OtherPersonManualContent.firstNameLastName}")`,
+    );
+    await Promise.all([
+      Helpers.checkGroup(
+        page,
+        6,
+        OtherPersonManualContent,
+        "govukLabel",
+        `${Selectors.GovukLabel}`,
+      ),
+      Helpers.checkVisibleAndPresent(
+        page,
+        `${Selectors.GovukHint}:text-is("${OtherPersonManualContent.govukHint}")`,
+        1,
+      ),
+    ]);
+    if (accessibilityTest) {
+      await AccessibilityTestHelper.run(page);
+    }
+  }
+
+  private static async fillInFields({
+    page,
+  }: fillInFieldsOptions): Promise<void> {
+    await page.click(
+      `${Selectors.GovukButton}:text-is("${CommonStaticText.continue}")`,
+    );
+  }
+}
