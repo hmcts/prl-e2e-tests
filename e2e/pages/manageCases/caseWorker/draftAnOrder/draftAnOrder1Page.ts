247fb33fea4bb6548debb55d7a673586de2cbbdd
RobL681 <robert.lacey@justice.gov.uk>
2024-11-06 15:20:31 +0000

    Prl 6421 (#138)
    * Initial draft an order changes for pages 1 to 4
    
    * Complete page and content up to page 4 for draft an order
    
    * formatting
    
    * Add draft an order journey pages 5, 16 and 20 for non-molestation order type
    
    * Linting
    
    * Fix error messaging
    
    * Ensure PDF checking is correct
    
    * Add submit page and reorganise directories
    
    * Formatting
    
    * Check order is submitted and add spec file
    
    * Add unit tests for helper method
    
    ---------
    
    Co-authored-by: Abigail Smith <148337562+abigailsmith97@users.noreply.github.com>

---
+++ e2e/pages/manageCases/caseWorker/draftAnOrder/draftAnOrder1Page.ts
@@ -0,0 +1,86 @@
+import { Page } from "@playwright/test";
+import { Selectors } from "../../../../common/selectors";
+import { Helpers } from "../../../../common/helpers";
+import AccessibilityTestHelper from "../../../../common/accessibilityTestHelper";
+import { DraftAnOrder1Content } from "../../../../fixtures/manageCases/caseWorker/draftAnOrder/draftAnOrder1Content";
+
+enum UniqueSelectors {
+  orderTypeRadio = "#draftOrderOptions-draftAnOrder",
+}
+
+export class DraftAnOrder1Page {
+  public static async draftAnOrder1Page(
+    page: Page,
+    errorMessaging: boolean,
+    accessibilityTest: boolean,
+  ): Promise<void> {
+    await this.checkPageLoads(page, accessibilityTest);
+    if (errorMessaging) {
+      await this.checkErrorMessaging(page);
+    }
+    await this.fillInFields(page);
+    await this.continue(page);
+  }
+
+  private static async checkPageLoads(
+    page: Page,
+    accessibilityTest: boolean,
+  ): Promise<void> {
+    await page.waitForSelector(
+      `${Selectors.GovukHeadingL}:text-is("${DraftAnOrder1Content.pageTitle}")`,
+    );
+    await Promise.all([
+      Helpers.checkGroup(
+        page,
+        1,
+        DraftAnOrder1Content,
+        `formLabel`,
+        `${Selectors.GovukFormLabel}`,
+      ),
+      Helpers.checkVisibleAndPresent(
+        page,
+        `${Selectors.button}:text-is("${DraftAnOrder1Content.previous}")`,
+        1,
+      ),
+      Helpers.checkVisibleAndPresent(
+        page,
+        `${Selectors.button}:text-is("${DraftAnOrder1Content.continue}")`,
+        1,
+      ),
+    ]);
+    if (accessibilityTest) {
+      await AccessibilityTestHelper.run(page);
+    }
+  }
+
+  private static async checkErrorMessaging(page: Page): Promise<void> {
+    await this.continue(page);
+    await Promise.all([
+      Helpers.checkVisibleAndPresent(
+        page,
+        `${Selectors.GovukErrorSummaryTitle}:text-is("${DraftAnOrder1Content.errorBanner}")`,
+        1,
+      ),
+      Helpers.checkVisibleAndPresent(
+        page,
+        `${Selectors.GovukErrorValidation}:has-text("${DraftAnOrder1Content.errorMessageWhatDoYouWantToDo}")`,
+        1,
+      ),
+      Helpers.checkVisibleAndPresent(
+        page,
+        `${Selectors.GovukErrorMessage}:has-text("${DraftAnOrder1Content.errorMessageWhatDoYouWantToDo}")`,
+        1,
+      ),
+    ]);
+  }
+
+  private static async fillInFields(page: Page): Promise<void> {
+    await page.check(UniqueSelectors.orderTypeRadio);
+  }
+
+  private static async continue(page: Page): Promise<void> {
+    await page.click(
+      `${Selectors.button}:text-is("${DraftAnOrder1Content.continue}")`,
+    );
+  }
+}
