39f197ddca2910bbadedad482ffbe8cd894aa7ab
abigailflorence97 <abigail.smith@hmcts.net>
2024-11-13 10:43:37 +0000

    globel-setup update

--- e2e/tests/global.setup.ts
+++ e2e/tests/global.setup.ts
@@ -1,21 +1,49 @@
-import { APIRequestContext, request, test as setup } from "@playwright/test";
+import {
+  APIRequestContext,
+  Page,
+  request,
+  test as setup,
+} from "@playwright/test";
 import IdamLoginHelper from "../common/idamLoginHelper";
 import config from "../config";
 import { getAccessToken } from "../common/getAccessTokenHelper";
+import { existsSync } from "fs";
 
-setup("Setup solicitor user", async ({ page }) => {
+async function setupSolicitorUser(page: Page) {
   await IdamLoginHelper.signInSolicitorUser(
     page,
     "solicitor",
     config.manageCasesBaseURL,
   );
-});
+}
 
-setup("Retrieve bearer token for citizen user creation", async () => {
+async function setupCitizenUserToken() {
   const apiContext: APIRequestContext = await request.newContext();
   const token = await getAccessToken("citizenCreateUser", apiContext);
   if (!token) {
     throw new Error("Setup failed: Unable to get bearer token.");
   }
   process.env.CITIZEN_CREATE_USER_BEARER_TOKEN = token;
-});
+}
+
+const isEnvFilePresent = existsSync(".env");
+const runningTest = process.env.RUNNING_TEST;
+
+if (!isEnvFilePresent) {
+  if (runningTest === "manageCases") {
+    setup("Setup solicitor user", async ({ page }) => {
+      await setupSolicitorUser(page);
+    });
+  } else if (runningTest === "citizenFrontend") {
+    setup("Retrieve bearer token for citizen user creation", async () => {
+      await setupCitizenUserToken();
+    });
+  }
+} else {
+  setup("Setup solicitor user", async ({ page }) => {
+    await setupSolicitorUser(page);
+  });
+  setup("Retrieve bearer token for citizen user creation", async () => {
+    await setupCitizenUserToken();
+  });
+}
