68a509daa5a84e2d91e8e5f64186455a14aa4233
RobL681 <robert.lacey@justice.gov.uk>
2024-11-25 12:32:21 +0000

    Welsh lang requirement tests

---
+++ e2e/tests/manageCases/caseProgression/welshLanguageRequirements/welshLanguageRequirements.spec.ts
@@ -0,0 +1,107 @@
+import { test } from "@playwright/test";
+import Config from "../../../../config";
+import createDaCitizenCourtNavCase from "../../../../common/createCaseHelper";
+import { Helpers } from "../../../../common/helpers";
+import config from "../../../../config";
+import { WelshLanguageRequirements } from "../../../../journeys/manageCases/caseProgression/welshLanguageRequirements/welshLanguageRequirements";
+
+test.use({ storageState: Config.sessionStoragePath + "caseWorker.json" });
+
+test.describe("Welsh Language Requirements task for DA Citizen case tests. @manageCases", () => {
+  test.beforeEach(async ({ page }) => {
+    const ccdRef: string = await createDaCitizenCourtNavCase(false);
+    await Helpers.goToCase(page, config.manageCasesBaseURL, ccdRef, "tasks");
+  });
+
+  test(`Complete Welsh Language Requirements with following options: 
+  Does any person in this case need orders or documents in Welsh: No
+  Accessibility testing: No. @crossbrowserManageCases`, async ({
+    page,
+  }): Promise<void> => {
+    await WelshLanguageRequirements.welshLanguageRequirements({
+      page: page,
+      needDocumentsInWelsh: false,
+      languageToCompleteApplication: "English",
+      doesApplicationNeedTranslating: false,
+      accessibilityTest: false,
+    });
+  });
+
+  test(`Complete Welsh Language Requirements with following options: 
+  Does any person in this case need orders or documents in Welsh: Yes
+  Which language are you using to complete this application: English
+  Does this application need to be translated: Yes
+  Accessibility testing: No. @crossbrowserManageCases`, async ({
+    page,
+  }): Promise<void> => {
+    await WelshLanguageRequirements.welshLanguageRequirements({
+      page: page,
+      needDocumentsInWelsh: true,
+      languageToCompleteApplication: "English",
+      doesApplicationNeedTranslating: true,
+      accessibilityTest: false,
+    });
+  });
+
+  test(`Complete Welsh Language Requirements with following options: 
+  Does any person in this case need orders or documents in Welsh: Yes
+  Which language are you using to complete this application: English
+  Does this application need to be translated: No
+  Accessibility testing: No. @crossbrowserManageCases`, async ({
+    page,
+  }): Promise<void> => {
+    await WelshLanguageRequirements.welshLanguageRequirements({
+      page: page,
+      needDocumentsInWelsh: true,
+      languageToCompleteApplication: "English",
+      doesApplicationNeedTranslating: false,
+      accessibilityTest: false,
+    });
+  });
+
+  test(`Complete Welsh Language Requirements with following options: 
+  Does any person in this case need orders or documents in Welsh: Yes
+  Which language are you using to complete this application: Welsh
+  Does this application need to be translated: Yes
+  Accessibility testing: No. @crossbrowserManageCases`, async ({
+    page,
+  }): Promise<void> => {
+    await WelshLanguageRequirements.welshLanguageRequirements({
+      page: page,
+      needDocumentsInWelsh: true,
+      languageToCompleteApplication: "Welsh",
+      doesApplicationNeedTranslating: true,
+      accessibilityTest: false,
+    });
+  });
+
+  test(`Complete Welsh Language Requirements with following options: 
+  Does any person in this case need orders or documents in Welsh: Yes
+  Which language are you using to complete this application: Welsh
+  Does this application need to be translated: No
+  Accessibility testing: No. @crossbrowserManageCases`, async ({
+    page,
+  }): Promise<void> => {
+    await WelshLanguageRequirements.welshLanguageRequirements({
+      page: page,
+      needDocumentsInWelsh: true,
+      languageToCompleteApplication: "Welsh",
+      doesApplicationNeedTranslating: false,
+      accessibilityTest: false,
+    });
+  });
+
+  test(`Complete Welsh Language Requirements with following options: 
+  Does any person in this case need orders or documents in Welsh: No
+  Accessibility testing: Yes. @accessibilityManageCases`, async ({
+    page,
+  }): Promise<void> => {
+    await WelshLanguageRequirements.welshLanguageRequirements({
+      page: page,
+      needDocumentsInWelsh: false,
+      languageToCompleteApplication: "English",
+      doesApplicationNeedTranslating: false,
+      accessibilityTest: true,
+    });
+  });
+});
