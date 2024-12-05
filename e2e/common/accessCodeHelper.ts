0433fd779370a66d11519daa64b2f2921ef09839
RobL681 <robert.lacey@justice.gov.uk>
2024-12-05 13:06:45 +0000

    Add code to retrieve the access code for an applicant or recipient for a given case

---
+++ e2e/common/accessCodeHelper.ts
@@ -0,0 +1,65 @@
+import { APIRequestContext, request } from "@playwright/test";
+import { getAccessToken, getS2SToken } from "./getAccessTokenHelper";
+
+interface CaseInvite {
+  id: string;
+  value: CaseInviteDetails;
+}
+
+interface CaseInviteDetails {
+  accessCode: string;
+  isApplicant: string;
+}
+
+export class AccessCodeHelper {
+  public static async getApplicantAccessCode(ccdRef: string): Promise<string> {
+    const caseInvites: CaseInvite[] = await this.getAccessCode(ccdRef);
+    return (
+      caseInvites.find((invitee) => {
+        return invitee.value.isApplicant === "Yes";
+      })?.value.accessCode || ""
+    );
+  }
+
+  public static async getRespondentAccessCode(ccdRef: string): Promise<string> {
+    const caseInvites: CaseInvite[] = await this.getAccessCode(ccdRef);
+    return (
+      caseInvites.find((invitee) => {
+        return invitee.value.isApplicant === "No";
+      })?.value.accessCode || ""
+    );
+  }
+
+  private static async getAccessCode(ccdRef: string): Promise<CaseInvite[]> {
+    let caseInvites: CaseInvite[] = [];
+    const apiContextSystemUser: APIRequestContext = await request.newContext();
+    const apiContextS2SToken: APIRequestContext = await request.newContext();
+    const tokenSystemUserCreateCase = await getAccessToken(
+      "accessCode",
+      apiContextSystemUser,
+    );
+    const s2sToken = await getS2SToken(apiContextS2SToken, "prl_cos_api");
+    const url = `${process.env.CCD_DATA_STORE_URL as string}${ccdRef}`;
+    const response = await apiContextSystemUser.get(url, {
+      headers: {
+        Authorization: `Bearer ${tokenSystemUserCreateCase}`,
+        ServiceAuthorization: `${s2sToken}`,
+        experimental: "test",
+      },
+    });
+    let responseBody;
+    if (response.ok()) {
+      responseBody = await response.json();
+      if (responseBody.data) {
+        caseInvites = responseBody.data.caseInvites;
+      } else {
+        throw new Error("Failed to find case data");
+      }
+    } else {
+      throw new Error(
+        `Failed to find case: ${response.status()} - ${response.statusText()}`,
+      );
+    }
+    return caseInvites;
+  }
+}
