83437969de2f8076aa9b039a3c88bda17b1cf0a9
scralfie <kellyalfie6@gmail.com>
2024-12-09 15:27:46 +0000

    Pages and Journey in progress

---
+++ e2e/pages/manageCases/caseProgression/createBundle/createBundle1Page.ts
@@ -0,0 +1,60 @@
+import { Page } from "@playwright/test";
+import { Selectors } from "../../../../common/selectors";
+import {
+  CreateBundle1Content
+} from "../../../../fixtures/manageCases/caseProgression/createBundle/createBundle1Content";
+import { Helpers } from "../../../../common/helpers";
+import AccessibilityTestHelper from "../../../../common/accessibilityTestHelper";
+import { CommonStaticText } from "../../../../common/commonStaticText";
+
+
+interface CreateBundle1PageOptions {
+  page: Page;
+  accessibilityTest: boolean;
+}
+
+export class CreateBundle1Page {
+  public static async createBundle1Page({
+                                          page,
+                                          accessibilityTest,
+                                        }: CreateBundle1PageOptions): Promise<void> {
+    await this.checkPageLoads(page, accessibilityTest);
+    await this.fillInFields(page);
+  }
+
+  private static async checkPageLoads(
+    page: Page,
+    accessibilityTest: boolean,
+  ): Promise<void> {
+    const pageTitle = page.locator(
+      `${Selectors.GovukHeadingL}:text-is("${CreateBundle1Content.pageTitle}")`,
+    );
+    await pageTitle.waitFor();
+    await Promise.all([
+      Helpers.checkVisibleAndPresent(
+        page,
+        `${Selectors.h1}:text-is("${CreateBundle1Content.h1}")`,
+        1,
+      ),
+      Helpers.checkVisibleAndPresent(
+        page,
+        `${Selectors.h2}:text-is("${CreateBundle1Content.h2}")`,
+        1,
+      ),
+      Helpers.checkVisibleAndPresent(
+        page,
+        `${Selectors.p}:text-is("${CreateBundle1Content.p}")`,
+        1,
+      ),
+    ]);
+    if (accessibilityTest) {
+      await AccessibilityTestHelper.run(page);
+    }
+  }
+
+  private static async fillInFields(page: Page): Promise<void> {
+    await page.click(
+      `${Selectors.button}:text-is("${CommonStaticText.submit}")`,
+    );
+  }
+}
