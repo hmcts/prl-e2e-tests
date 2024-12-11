4e0dfe7574011934bac8c509ed6676fa12bc8699
nixonthapa <nixon.thapa2@hmcts.net>
2024-12-11 13:09:39 +0000

    updated adminEditAndApproveAnOrder23Page

--- e2e/pages/manageCases/caseProgression/completeTheOrder/adminEditAndApproveAnOrder23Page.ts
+++ e2e/pages/manageCases/caseProgression/completeTheOrder/adminEditAndApproveAnOrder23Page.ts
@@ -81,8 +81,8 @@ export class AdminEditAndApproveAnOrder23Page {
       await page.check(`${UniqueSelectors.respondentsOptionsCourtBailiff}`);
     } else {
       await page.check(`${UniqueSelectors.respondentOptionNo}`);
-      await page.check((`${Selectors.GovukFormLabel}:has-text("(Applicant)")`));
-      await page.check((`${Selectors.GovukFormLabel}:has-text("(Respondent)")`));
+      await page.check(`${Selectors.GovukFormLabel}:has-text("(Applicant)")`);
+      await page.check(`${Selectors.GovukFormLabel}:has-text("(Respondent)")`);
       await Helpers.checkVisibleAndPresent(
         page,
         `${Selectors.GovukFormLabel}:text-is("${AdminEditAndApproveAnOrder23Content.formLabel4}"):visible`,
