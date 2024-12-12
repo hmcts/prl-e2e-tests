37b4c26cc9deaff3662ff08e9b6210219028400e
Jay Ashton <jay.ashton@justice.gov.uk>
2024-12-12 15:14:26 +0000

    Optimisations

--- scripts/get_secrets.sh
+++ scripts/get_secrets.sh
@@ -5,35 +5,35 @@
 # Requires: Azure CLI & JQ (brew install azure-cli jq)
 
 KEY_VAULT_NAME="prl-aat"
-env_example_file=".env.example"
-env_file=".env"
-temp_env_file=$(mktemp)
-secrets=$(az keyvault secret list --vault-name $KEY_VAULT_NAME --query "[].id" -o tsv)
+EXAMPLE_ENV_FILE=".env.example"
+ENV_FILE=".env"
+TEMP_ENV_FILE=$(mktemp)
 
-for secret in $secrets; do
-    secret_name=$(basename $secret)
-    tags=$(az keyvault secret show --vault-name $KEY_VAULT_NAME --name $secret_name --query "tags" -o json)
+secrets=$(az keyvault secret list --vault-name $KEY_VAULT_NAME --query "[].{id:id, tags:tags}" -o json)
+echo "$secrets" | jq -c '.[]' | while read -r secret; do
+    secret_id=$(echo $secret | jq -r '.id')
+    tags=$(echo $secret | jq -r '.tags')
     if echo $tags | jq -e 'has("e2e")' > /dev/null; then
         env_var=$(echo $tags | jq -r '.e2e')
-        secret_value=$(az keyvault secret show --vault-name $KEY_VAULT_NAME --name $secret_name --query "value" -o tsv)
-        echo "Reading $secret_name from $KEY_VAULT_NAME vault"
-        echo "$env_var=$secret_value" >> $temp_env_file
+        secret_value=$(az keyvault secret show --id $secret_id --query "value" -o tsv)
+        echo "Reading $(basename $secret_id) from $KEY_VAULT_NAME vault"
+        echo "$env_var=$secret_value" >> $TEMP_ENV_FILE
     fi
 done
 
-if [ -f $env_example_file ]; then
-    cp $env_example_file $env_file
-    while IFS= read -r line; do
+if [ -f $EXAMPLE_ENV_FILE ]; then
+    cp $EXAMPLE_ENV_FILE $ENV_FILE
+    while IFS= read -r line || [ -n "$line" ]; do
         key=$(echo $line | cut -d '=' -f 1)
-        value=$(grep "^$key=" $temp_env_file | cut -d '=' -f 2-)
+        value=$(grep "^$key=" $TEMP_ENV_FILE | cut -d '=' -f 2-)
         if [ -n "$value" ]; then
             echo "Setting $key"
-            sed -i '' "s|^$key=.*|$key=$value|" $env_file
+            sed -i '' "s|^$key=.*|$key=$value|" $ENV_FILE
         fi
-    done < $env_example_file
+    done < $EXAMPLE_ENV_FILE
     echo ".env file created successfully."
 else
     echo ".env.example file not found." && exit 1
 fi
 
-rm $temp_env_file
+rm $TEMP_ENV_FILE
