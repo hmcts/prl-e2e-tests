78246c9046be5470359415efc30e6a6f338e2449
Abigail Smith <148337562+abigailsmith97@users.noreply.github.com>
2024-12-12 11:53:58 +0000

    PRL-6673: Service of Documents (#210)
    * PRL-6673 create content, pages, journeys, and test

---
+++ e2e/fixtures/manageCases/caseProgression/serviceOfDocuments/serviceOfDocuments1Content.ts
@@ -0,0 +1,11 @@
+export enum ServiceOfDocuments1Content {
+  pageTitle = "Service of documents",
+  h21 = "Upload additional documents (Optional)",
+  h22 = "Documents",
+  formHint1 = "Please upload cover letter as first document if any, that is to be sent to both the applicant and the respondent.",
+  formHint2 = "Select a document to reference",
+  formHint3 = "Select the document",
+  formLabel1 = "Select the document (Optional)",
+  h3 = "Documents",
+  hiddenh3 = "Upload additional documents",
+}
---
+++ e2e/pages/manageCases/caseProgression/serviceOfDocuments/serviceOfDocumentsSubmitPage.ts
@@ -0,0 +1,210 @@
+import { expect, Page } from "@playwright/test";
+import { Selectors } from "../../../../common/selectors";
+import { Helpers } from "../../../../common/helpers";
+import { CommonStaticText } from "../../../../common/commonStaticText";
+import AccessibilityTestHelper from "../../../../common/accessibilityTestHelper";
+import { yesNoNA } from "../../../../common/types";
+import { ServiceOfDocumentsSubmitContent } from "../../../../fixtures/manageCases/caseProgression/serviceOfDocuments/serviceOfDocumentsSubmitContent";
+
+interface serviceOfDocumentsSubmitOptions {
+  page: Page;
+  accessibilityTest: boolean;
+  withCaseDoc: boolean;
+  additionalDoc: boolean;
+  additionalRecipient: boolean;
+  personallyServed: yesNoNA;
+  servedByPost: boolean;
+  checkDocuments: boolean;
+}
+
+export class ServiceOfDocumentsSubmitPage {
+  public static async serviceOfDocumentsSubmitPage({
+    page,
+    accessibilityTest,
+    withCaseDoc,
+    additionalDoc,
+    additionalRecipient,
+    personallyServed,
+    servedByPost,
+    checkDocuments,
+  }: serviceOfDocumentsSubmitOptions): Promise<void> {
+    await this.checkPageLoads({
+      page,
+      accessibilityTest,
+      withCaseDoc,
+      additionalDoc,
+      additionalRecipient,
+      personallyServed,
+      servedByPost,
+      checkDocuments,
+    });
+    await this.saveAndContinue(page);
+  }
+
+  private static async checkPageLoads({
+    page,
+    accessibilityTest,
+    withCaseDoc,
+    additionalDoc,
+    additionalRecipient,
+    personallyServed,
+    servedByPost,
+    checkDocuments,
+  }: Partial<serviceOfDocumentsSubmitOptions>) {
+    if (!page) {
+      throw new Error("No page found");
+    }
+    const pageTitle = page.locator(
+      `${Selectors.GovukHeadingL}:text-is("${ServiceOfDocumentsSubmitContent.pageTitle}")`,
+    );
+    await pageTitle.waitFor();
+    await this.checkStaticFields(page);
+    await this.checkCaseDocument(page, withCaseDoc);
+    await this.checkAdditionalDocument(page, additionalDoc);
+    await this.checkAdditionalRecipient(
+      page,
+      additionalRecipient,
+      servedByPost,
+    );
+    await this.checkPersonalService(page, personallyServed);
+    await this.checkDocumentVerification(page, checkDocuments);
+    if (accessibilityTest) {
+      await AccessibilityTestHelper.run(page);
+    }
+  }
+
+  private static async checkStaticFields(page: Page): Promise<void> {
+    await expect(
+      page.getByText(ServiceOfDocumentsSubmitContent.textFielda),
+    ).toBeVisible();
+    //checking group is causing failures, so using singular checks
+    await Helpers.checkGroup(
+      page,
+      5,
+      ServiceOfDocumentsSubmitContent,
+      `textField`,
+      `${Selectors.GovukText16}`,
+    );
+  }
+
+  private static async checkCaseDocument(
+    page: Page,
+    withCaseDoc?: boolean,
+  ): Promise<void> {
+    if (withCaseDoc) {
+      await Helpers.checkGroup(
+        page,
+        2,
+        ServiceOfDocumentsSubmitContent,
+        `textFieldCaseDoc`,
+        `${Selectors.GovukText16}`,
+      );
+    }
+  }
+
+  private static async checkPersonalService(
+    page: Page,
+    personallyServed?: yesNoNA,
+  ): Promise<void> {
+    switch (personallyServed) {
+      case "Yes":
+        await Helpers.checkGroup(
+          page,
+          3,
+          ServiceOfDocumentsSubmitContent,
+          `textPersonallyServed`,
+          `${Selectors.GovukText16}`,
+        );
+        break;
+      case "No":
+        await Promise.all([
+          Helpers.checkGroup(
+            page,
+            2,
+            ServiceOfDocumentsSubmitContent,
+            `textNotPersonallyServed`,
+            `${Selectors.GovukText16}`,
+          ),
+          Helpers.checkGroup(
+            page,
+            2,
+            ServiceOfDocumentsSubmitContent,
+            `pNotPersonallyServed`,
+            `${Selectors.p}`,
+          ),
+        ]);
+        break;
+      case "Not applicable":
+        await Helpers.checkVisibleAndPresent(
+          page,
+          `${Selectors.GovukText16}:text-is("${ServiceOfDocumentsSubmitContent.textNAPersonallyServed}")`,
+          1,
+        );
+        break;
+      default:
+        throw new Error("Invalid value for personallyServed");
+    }
+  }
+
+  private static async checkDocumentVerification(
+    page: Page,
+    checkDocuments?: boolean,
+  ): Promise<void> {
+    const verificationText = checkDocuments
+      ? ServiceOfDocumentsSubmitContent.textAnsCheckDoc
+      : ServiceOfDocumentsSubmitContent.textAnsNoCheckDoc;
+
+    await Helpers.checkVisibleAndPresent(
+      page,
+      `${Selectors.GovukText16}:text-is("${verificationText}")`,
+      1,
+    );
+  }
+
+  private static async checkAdditionalRecipient(
+    page: Page,
+    additionalRecipient?: boolean,
+    servedByPost?: boolean,
+  ): Promise<void> {
+    if (additionalRecipient) {
+      await Promise.all([
+        await Helpers.checkGroup(
+          page,
+          3,
+          ServiceOfDocumentsSubmitContent,
+          `textFieldAdditionalRecipient`,
+          `${Selectors.GovukText16}`,
+        ),
+      ]);
+
+      const recipientMethodText = servedByPost
+        ? ServiceOfDocumentsSubmitContent.textFieldAdditionalRecipientPost
+        : ServiceOfDocumentsSubmitContent.textFieldAdditionalRecipientEmail;
+
+      await Helpers.checkVisibleAndPresent(
+        page,
+        `${Selectors.GovukText16}:text-is("${recipientMethodText}")`,
+        1,
+      );
+    }
+  }
+
+  private static async checkAdditionalDocument(
+    page: Page,
+    additionalDoc?: boolean,
+  ): Promise<void> {
+    if (additionalDoc) {
+      await Helpers.checkVisibleAndPresent(
+        page,
+        `${Selectors.GovukText16}:text-is("${ServiceOfDocumentsSubmitContent.textFieldAdditionDoc1}")`,
+        1,
+      );
+    }
+  }
+
+  private static async saveAndContinue(page: Page): Promise<void> {
+    await page.click(
+      `${Selectors.button}:text-is("${CommonStaticText.saveAndContinue}")`,
+    );
+  }
+}
