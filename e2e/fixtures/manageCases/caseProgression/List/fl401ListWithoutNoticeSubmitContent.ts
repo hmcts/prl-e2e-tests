2f782559e9e494dd305610ad0cbc50cb789c5b05
elizereyes123 <107991381+elizereyes123@users.noreply.github.com>
2024-12-12 10:29:05 +0000

    PRL 6719 (#212)
    * Add content, pages and tests for list without notice
    
    * add list without notice pages
    
    * fix issues from merge
    
    ---------
    
    Co-authored-by: Jay Ashton <jay.ashton@justice.gov.uk>
    Co-authored-by: Abigail Smith <148337562+abigailsmith97@users.noreply.github.com>

---
+++ e2e/fixtures/manageCases/caseProgression/List/fl401ListWithoutNoticeSubmitContent.ts
@@ -0,0 +1,7 @@
+export enum Fl401ListWithoutNoticeSubmitContent {
+  govHeadingL = "List without notice",
+  headingH2 = "Check your answers",
+  text161 = "Check the information below carefully.",
+  text162 = "Give admin hearing instructions",
+  span1 = "Test admin hearing instructions",
+}
