8f21c1f146b30b2afbad53010b64b669ceef2218
scralfie <kellyalfie6@gmail.com>
2024-12-16 10:33:43 +0000

    Refactoring file names to temporary names

--- e2e/tests/manageCases/caseProgression/completeOrderServiceOfApplication/completeOrderServiceOfApplicationStatementOfService.spec.ts
+++ e2e/tests/manageCases/caseProgression/completeOrderServiceOfApplication/completeOrderServiceOfApplicationStatementOfService.spec.ts
@@ -19,47 +19,51 @@ test.describe("Complete the Order task for DA Citizen case tests.", () => {
     page,
     browser,
   }): Promise<void> => {
-    await CompleteOrderServiceOfApplicationStatementOfService.completeOrderServiceOfApplication({
-      page: page,
-      accessibilityTest: false,
-      yesNoSendToGateKeeper: true,
-      ccdRef: ccdRef,
-      c100CaseWorkerActions: "Manage orders",
-      manageOrdersOptions: "create order",
-      createOrderFL401Options: "power of arrest",
-      yesNoManageOrders: false,
-      judgeTitles: "Her Honour Judge",
-      withOrWithoutNotice: true,
-      createOrderManageOrders19Options: "dateToBeFixed", // "dateConfirmed" will not pass because page 19 does not give a hearing you are allowed to select
-      howLongWillOrderBeInForce: "untilNextHearing", // Should not matter unless non-molestation order is selected.
-      browser: browser,
-      personallyServed: true,
-      yesNoServiceOfApplication4: true,
-      responsibleForServing: "courtBailiff",
-    });
+    await CompleteOrderServiceOfApplicationStatementOfService.completeOrderServiceOfApplication(
+      {
+        page: page,
+        accessibilityTest: false,
+        yesNoSendToGateKeeper: true,
+        ccdRef: ccdRef,
+        c100CaseWorkerActions: "Manage orders",
+        manageOrdersOptions: "create order",
+        createOrderFL401Options: "power of arrest",
+        yesNoManageOrders: false,
+        judgeTitles: "Her Honour Judge",
+        withOrWithoutNotice: true,
+        createOrderManageOrders19Options: "dateToBeFixed", // "dateConfirmed" will not pass because page 19 does not give a hearing you are allowed to select
+        howLongWillOrderBeInForce: "untilNextHearing", // Should not matter unless non-molestation order is selected.
+        browser: browser,
+        personallyServed: true,
+        yesNoServiceOfApplication4: true,
+        responsibleForServing: "courtBailiff",
+      },
+    );
   });
 
   test("Complete Task - Complete the Order & statement of service - Amended, discharged or varied order (FL404B)  with accessibility test. @regression @accessibility", async ({
     page,
     browser,
   }): Promise<void> => {
-    await CompleteOrderServiceOfApplicationStatementOfService.completeOrderServiceOfApplication({
-      page: page,
-      accessibilityTest: false,
-      yesNoSendToGateKeeper: true,
-      ccdRef: ccdRef,
-      c100CaseWorkerActions: "Manage orders",
-      manageOrdersOptions: "create order",
-      createOrderFL401Options: "amend discharge varied order",
-      yesNoManageOrders: false,
-      judgeTitles: "Deputy Circuit Judge",
-      withOrWithoutNotice: false,
-      createOrderManageOrders19Options: "dateToBeFixed", // "dateConfirmed" will not pass because page 19 does not give a hearing you are allowed to select
-      howLongWillOrderBeInForce: "untilNextHearing", // Should not matter unless non-molestation order is selected.
-      browser: browser,
-      personallyServed: false,
-      yesNoServiceOfApplication4: true,
-      responsibleForServing: "courtBailiff",
-    });
+    await CompleteOrderServiceOfApplicationStatementOfService.completeOrderServiceOfApplication(
+      {
+        page: page,
+        accessibilityTest: false,
+        yesNoSendToGateKeeper: true,
+        ccdRef: ccdRef,
+        c100CaseWorkerActions: "Manage orders",
+        manageOrdersOptions: "create order",
+        createOrderFL401Options: "amend discharge varied order",
+        yesNoManageOrders: false,
+        judgeTitles: "Deputy Circuit Judge",
+        withOrWithoutNotice: false,
+        createOrderManageOrders19Options: "dateToBeFixed", // "dateConfirmed" will not pass because page 19 does not give a hearing you are allowed to select
+        howLongWillOrderBeInForce: "untilNextHearing", // Should not matter unless non-molestation order is selected.
+        browser: browser,
+        personallyServed: false,
+        yesNoServiceOfApplication4: true,
+        responsibleForServing: "courtBailiff",
+      },
+    );
   });
 });
