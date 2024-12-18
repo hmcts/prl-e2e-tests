f4d7d052f7d56e64f5c5cdca85cdd84662acc18f
nixonthapa <nixon.thapa2@hmcts.net>
2024-12-18 16:34:25 +0000

    add content, pages, journey and test file for create hearing request

---
+++ e2e/pages/manageCases/caseProgression/createHearingRequest/hearingStagePage.ts
@@ -0,0 +1,59 @@
+import { Page } from "@playwright/test";
+import AccessibilityTestHelper from "../../../../common/accessibilityTestHelper";
+import { Selectors } from "../../../../common/selectors";
+import { HearingStageContent } from "../../../../fixtures/manageCases/caseProgression/createHearingRequest/hearingStageContent";
+import { Helpers } from "../../../../common/helpers";
+import { CommonStaticText } from "../../../../common/commonStaticText";
+
+enum UniqueSelectors {
+  allocation = "#ABA5-ALL",
+}
+
+export class HearingStagePage {
+  public static async hearingStagePage(
+    page: Page,
+    accessibilityTest: boolean,
+  ): Promise<void> {
+    await this.checkPageLoads(page, accessibilityTest);
+    await this.fillInFields(page);
+    await this.continue(page);
+  }
+
+  private static async checkPageLoads(
+    page: Page,
+    accessibilityTest: boolean,
+  ): Promise<void> {
+    await page
+      .locator(`${Selectors.GovukHeadingL}`, {
+        hasText: `${HearingStageContent.govUkHeadingL}`,
+      })
+      .waitFor();
+    await Promise.all([
+      Helpers.checkGroup(
+        page,
+        26,
+        HearingStageContent,
+        "GovukLabel",
+        Selectors.GovukLabel,
+      ),
+      Helpers.checkVisibleAndPresent(
+        page,
+        `${Selectors.GovukButton}:text-is("${CommonStaticText.continue}")`,
+        1,
+      ),
+    ]);
+    if (accessibilityTest) {
+      await AccessibilityTestHelper.run(page);
+    }
+  }
+
+  private static async fillInFields(page: Page): Promise<void> {
+    await page.check(`${UniqueSelectors.allocation}`);
+  }
+
+  private static async continue(page: Page): Promise<void> {
+    await page.click(
+      `${Selectors.GovukButton}:text-is("${CommonStaticText.continue}")`,
+    );
+  }
+}
