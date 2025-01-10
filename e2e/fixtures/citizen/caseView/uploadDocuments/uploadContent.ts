efc8533ea4aa893a0d8640f4c18aca699037c80b
nixonthapa <nixon.thapa2@hmcts.net>
2025-01-10 15:57:49 +0000

    removed white spaces

--- e2e/fixtures/citizen/caseView/uploadDocuments/uploadContent.ts
+++ e2e/fixtures/citizen/caseView/uploadDocuments/uploadContent.ts
@@ -2,12 +2,10 @@ export enum UploadContent {
   GovukCaptionL = "Upload documents",
   GovukHeadingXL = "Select the type of document",
   GovukBody = "The court will tell you in a letter or email which documents or material you need to submit.",
-
   GovukHeadingAppTask1 = "Witness statements and evidence",
   GovukHeadingAppTask2 = "Applications",
   GovukHeadingAppTask3 = "Expert reports",
   GovukHeadingAppTask4 = "Other documents",
-
   GovukLink1 = "Your position statement",
   GovukLink2 = "Your witness statements",
   GovukLink3 = "Other people's witness statements",
@@ -15,15 +13,12 @@ export enum UploadContent {
   GovukLink5 = "Medical records",
   GovukLink6 = "Letters from school",
   GovukLink7 = "Tenancy and mortgage agreements",
-
   GovukLink8 = "Submit an application",
   GovukLink9 = "Previous orders submitted with application",
-
   GovukLink10 = "Medical reports",
   GovukLink11 = "Paternity test reports",
   GovukLink12 = "Drug and alcohol tests (toxicology)",
   GovukLink13 = "Police reports",
-
   GovukLink14 = "Statement of position on non-court dispute resolution (NCDR) (form FM5)",
   GovukLink15 = "Other documents",
 }
