8dd001771eac9f8ecd78bebdd4d9e63f801f96c8
scralfie <kellyalfie6@gmail.com>
2025-01-10 14:53:38 +0000

    Created Journey and tests. Now passing

---
+++ e2e/journeys/citizen/caseView/checkTheApplication/checkTheApplication.ts
@@ -0,0 +1,40 @@
+import { ActivateCase, CaseUser } from "../../activateCase/activateCase.ts";
+import { Browser, Page } from "@playwright/test";
+
+interface checkTheApplicationParams {
+  page: Page;
+  browser: Browser;
+  caseRef: string;
+  accessibilityTest: boolean;
+  isApplicant: boolean;
+}
+
+enum UniqueSelectors {
+  yourApplicationApplicant = "#yourApplicationPDF",
+  checkTheApplicationRespondent = "#checkTheApplication",
+}
+
+export class CheckTheApplication {
+  public static async checkTheApplication({
+    page,
+    browser,
+    caseRef,
+    accessibilityTest,
+    isApplicant,
+  }: checkTheApplicationParams): Promise<void> {
+    const caseUser: CaseUser = isApplicant ? "applicant" : "respondent";
+    page = await ActivateCase.activateCase({
+      page,
+      browser,
+      caseRef,
+      caseUser,
+      accessibilityTest,
+    });
+    if (isApplicant) {
+      await page.click(UniqueSelectors.yourApplicationApplicant);
+    } else {
+      await page.click(UniqueSelectors.checkTheApplicationRespondent);
+    }
+    await page.waitForTimeout(2000);
+  }
+}
