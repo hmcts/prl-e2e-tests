89c799df083c98ff3fac272693c218a4437473bd
scralfie <kellyalfie6@gmail.com>
2025-01-10 11:10:34 +0000

    journey and test refactoring complete

---
+++ e2e/journeys/citizen/caseView/viewAllDocuments/viewAllDocuments.ts
@@ -0,0 +1,47 @@
+import { ActivateCase, CaseUser } from "../../activateCase/activateCase.ts";
+import { Browser, Page } from "@playwright/test";
+import { AllCategoriesPage } from "../../../../pages/citizen/caseView/viewAllDocuments/allCatergoriesPage.ts";
+import {
+  ApplicationPackDocumentsPage
+} from "../../../../pages/citizen/caseView/viewAllDocuments/applicationPackDocumentsPage.ts";
+
+interface reasonableAdjustmentsParams {
+  page: Page;
+  browser: Browser;
+  caseRef: string;
+  isApplicant: boolean;
+  accessibilityTest: boolean;
+}
+
+enum UniqueSelectors {
+  viewAllDocuments = "#viewAllDocuments",
+}
+
+export class ViewAllDocuments {
+  public static async viewAllDocuments({
+                                              page,
+                                              browser,
+                                              caseRef,
+                                              isApplicant,
+                                              accessibilityTest,
+                                            }: reasonableAdjustmentsParams): Promise<void> {
+    const caseUser: CaseUser = isApplicant ? "applicant" : "respondent";
+    page = await ActivateCase.activateCase({
+      page: page,
+      browser: browser,
+      caseRef: caseRef,
+      caseUser: caseUser,
+      accessibilityTest: accessibilityTest,
+    });
+
+    await page.click(UniqueSelectors.viewAllDocuments);
+    await AllCategoriesPage.allCategoriesPage({
+      page,
+      accessibilityTest,
+    });
+    await ApplicationPackDocumentsPage.applicationPackDocumentsPage({
+      page,
+      accessibilityTest,
+    })
+  }
+}
