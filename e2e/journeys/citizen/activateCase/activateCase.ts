595f0a1114242ff57456468a901b35fd765b5dae
RobL681 <robert.lacey@justice.gov.uk>
2025-01-20 15:55:22 +0000

    Fix journey for time being

--- e2e/journeys/citizen/activateCase/activateCase.ts
+++ e2e/journeys/citizen/activateCase/activateCase.ts
@@ -7,6 +7,7 @@ import { CaseActivatedPage } from "../../../pages/citizen/activateCase/caseActiv
 import { ApplicantDashboardPage } from "../../../pages/citizen/activateCase/applicantDashboardPage.ts";
 import { RespondentDashboardPage } from "../../../pages/citizen/activateCase/respondentDashboardPage.ts";
 import { E2eFlowUpToServiceOfApplication } from "../../manageCases/caseProgression/e2eFlowUpToServiceOfApplication/e2eFlowUpToServiceOfApplication.ts";
+import { jsonDatas } from "../../../common/solicitorCaseCreatorHelper.ts";
 
 interface ActiveCaseParams {
   page: Page;
@@ -44,6 +45,7 @@ export class ActivateCase {
       personallyServed: true,
       yesNoServiceOfApplication4: false,
       responsibleForServing: "courtBailiff", // this isn't used when yesNoServiceOfApplication4 is false
+      manageOrderData: jsonDatas.defaultData,
     });
     switch (caseUser) {
       case "applicant":
