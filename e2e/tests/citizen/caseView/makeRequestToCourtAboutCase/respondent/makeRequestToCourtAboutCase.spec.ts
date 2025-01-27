cfe22b9f50b32bd6ec375ee4fd884e8fbadbced5
nixonthapa <nixon.thapa2@hmcts.net>
2025-01-27 14:14:53 +0000

    add tests for applicant and respondent Make request to court about case

---
+++ e2e/tests/citizen/caseView/makeRequestToCourtAboutCase/respondent/makeRequestToCourtAboutCase.spec.ts
@@ -0,0 +1,32 @@
+import { test } from "@playwright/test";
+import Config from "../../../../../config.ts";
+import config from "../../../../../config.ts";
+import createDaCitizenCourtNavCase from "../../../../../common/createCaseHelper.ts";
+import { Helpers } from "../../../../../common/helpers.ts";
+import { MakeRequestToCourtAboutCase } from "../../../../../journeys/citizen/caseView/makeRequestToCourtAboutCase/respondent/makeRequestToCourtAboutCase.ts";
+
+test.use({ storageState: Config.sessionStoragePath + "caseWorker.json" });
+
+test.describe("Respondent Make a request to the court about your case tests", (): void => {
+  test.slow();
+  let ccdRef: string;
+
+  test.beforeEach(async ({ page }) => {
+    ccdRef = await createDaCitizenCourtNavCase(true, false);
+    await Helpers.goToCase(page, config.manageCasesBaseURL, ccdRef, "tasks");
+  });
+
+  test("Respondent Make a request to the court about your case page. @regression @accessibility @nightly", async ({
+   page,
+   browser,
+ }): Promise<void> => {
+    await MakeRequestToCourtAboutCase.makeRequestToCourtAboutCase({
+      page: page,
+      browser: browser,
+      caseRef: ccdRef,
+      accessibilityTest: false,
+      isApplicant: false,
+      applicationSubmittedBy: "Citizen",
+    });
+  });
+});
