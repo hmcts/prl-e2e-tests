7a15e17cdb01c5ccdfa376d5bb0c6735eef9a395
nixonthapa <nixon.thapa2@hmcts.net>
2025-01-29 13:58:31 +0000

    PRL-6907-Request more time both applicant and respondent (#296)
    * add tests for Request more time both applicant and respondent
    
    * updated journey files for requestMoreTime added isManualSOA param
    
    * removed continue param from content files and used continue param from CommonStaticText file
    
    * fixed selecting form link with same text by using indexing

---
+++ e2e/fixtures/citizen/caseView/requestMoreTime/agreementForRequestContent.ts
@@ -0,0 +1,7 @@
+export enum AgreementForRequestContent {
+  GovukCaptionL = "Request more time to do what is required by a court order",
+  GovukHeadingL = "Get agreement for your request",
+  GovukBodyM1 = "If you are able to, getting agreement from the other person in the case will reduce the fee you may have to pay.",
+  GovukBodyM2 = "You will need to provide proof of their agreement.",
+  GovukFieldsetLegend = "Does the other person in the case agree with this request?",
+}
---
+++ e2e/pages/citizen/caseView/requestMoreTime/guidanceApplicantPage.ts
@@ -0,0 +1,63 @@
+import { Page } from "@playwright/test";
+import { Selectors } from "../../../../common/selectors.ts";
+import AccessibilityTestHelper from "../../../../common/accessibilityTestHelper.ts";
+import { GuidanceContent } from "../../../../fixtures/citizen/caseView/requestMoreTime/guidanceContent.ts";
+import { Helpers } from "../../../../common/helpers.ts";
+import { CommonStaticText } from "../../../../common/commonStaticText.ts";
+
+export class GuidanceApplicantPage {
+  public static async guidanceApplicantPage(
+    page: Page,
+    accessibilityTest: boolean,
+  ): Promise<void> {
+    // this part of the case is complete when the case is created through courtnav so only need to check the page
+    await this.checkPageLoads(page, accessibilityTest);
+    await this.startNow(page);
+  }
+
+  private static async checkPageLoads(
+    page: Page,
+    accessibilityTest: boolean,
+  ): Promise<void> {
+    await page
+      .locator(Selectors.GovukCaptionL, {
+        hasText: GuidanceContent.GovukCaptionL,
+      })
+      .waitFor();
+    await Promise.all([
+      Helpers.checkVisibleAndPresent(
+        page,
+        `${Selectors.GovukHeadingXL}:text-is("${GuidanceContent.GovukHeadingXL}")`,
+        1,
+      ),
+      Helpers.checkVisibleAndPresent(
+        page,
+        `${Selectors.GovukHeadingM}:text-is("${GuidanceContent.GovukHeadingM}")`,
+        1,
+      ),
+      Helpers.checkVisibleAndPresent(
+        page,
+        `${Selectors.GovukLink}:text-is("${GuidanceContent.GovukLink}")`,
+        1,
+      ),
+      Helpers.checkVisibleAndPresent(
+        page,
+        `${Selectors.GovukBody}:text-is("${GuidanceContent.GovukBodyApplicant}")`,
+        1,
+      ),
+      Helpers.checkGroup(
+        page,
+        3,
+        GuidanceContent,
+        `GovukBody`,
+        `${Selectors.GovukBody}`,
+      ),
+    ]);
+    if (accessibilityTest) {
+      await AccessibilityTestHelper.run(page);
+    }
+  }
+  private static async startNow(page: Page): Promise<void> {
+    await page.click(`${Selectors.GovukButton}:text-is("${CommonStaticText.startNow}")`,);
+  }
+}
