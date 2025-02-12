c7f1c70ffb441db16715398ebb385e252bcc09aa
RobL681 <robert.lacey@justice.gov.uk>
2025-02-12 11:06:40 +0000

    Move create blank case into helpers for CA and DA

--- e2e/common/solicitorCACaseCreator.ts
+++ e2e/common/solicitorCACaseCreator.ts
@@ -1,9 +1,8 @@
 import { Page } from "@playwright/test";
 import {
-  getData,
+  createBlankCase,
   JsonData,
   jsonDatas,
-  postData,
   submitEvent,
 } from "./solicitorCaseCreatorHelper.ts";
 import { solicitorCACaseAPIEvent } from "./types.ts";
@@ -32,56 +31,12 @@ export class SolicitorCACaseCreator {
     page: Page,
     jsonData: JsonData = jsonDatas.solicitorCACaseData,
   ): Promise<string> {
-    const caseRef: string = await this.createBlankCase(page);
+    const caseRef: string = await createBlankCase(page, jsonData);
     for (const event of solicitorCaseEvents) {
       console.log(`Starting event: ${event}`);
       await submitEvent(page, caseRef, event, jsonData);
       console.log(`Finished event: ${event}`);
     }
     return caseRef;
-  }
-
-  private static async createBlankCase(
-    page: Page,
-    jsonData: JsonData = jsonDatas.solicitorCACaseData,
-  ): Promise<string> {
-    const startCaseCreationUrl = `/data/internal/case-types/PRLAPPS/event-triggers/solicitorCreate?ignore-warning=false`;
-
-    const startCaseCreationHeaders = {
-      Accept:
-        "application/vnd.uk.gov.hmcts.ccd-data-store-api.ui-start-case-trigger.v2+json;charset=UTF-8",
-      Experimental: "true",
-      "Content-type": "application/json; charset=UTF-8",
-    };
-    const eventToken: string = await getData(
-      page,
-      startCaseCreationUrl,
-      startCaseCreationHeaders,
-    );
-
-    const submitCaseUrl = `/data/case-types/PRLAPPS/cases?ignore-warning=false`;
-    const data = {
-      data: jsonData.solicitorCreate.data,
-      draft_id: null,
-      event: {
-        id: "solicitorCreate",
-        summary: "",
-        description: "",
-      },
-      event_token: eventToken,
-      ignore_warning: false,
-    };
-    const submitEventHeaders = {
-      Accept:
-        "application/vnd.uk.gov.hmcts.ccd-data-store-api.create-case.v2+json;charset=UTF-8",
-      Experimental: "true",
-      "Content-type": "application/json; charset=UTF-8",
-    };
-    return await postData(
-      page,
-      submitCaseUrl,
-      submitEventHeaders,
-      JSON.stringify(data),
-    );
   }
 }
--- e2e/common/solicitorCaseCreatorHelper.ts
+++ e2e/common/solicitorCaseCreatorHelper.ts
@@ -146,3 +146,47 @@ export async function submitEvent(
     console.log(`Completed event: ${eventId}`);
   }
 }
+
+export async function createBlankCase(
+  page: Page,
+  jsonData: JsonData,
+): Promise<string> {
+  const startCaseCreationUrl = `/data/internal/case-types/PRLAPPS/event-triggers/solicitorCreate?ignore-warning=false`;
+
+  const startCaseCreationHeaders = {
+    Accept:
+      "application/vnd.uk.gov.hmcts.ccd-data-store-api.ui-start-case-trigger.v2+json;charset=UTF-8",
+    Experimental: "true",
+    "Content-type": "application/json; charset=UTF-8",
+  };
+  const eventToken: string = await getData(
+    page,
+    startCaseCreationUrl,
+    startCaseCreationHeaders,
+  );
+
+  const submitCaseUrl = `/data/case-types/PRLAPPS/cases?ignore-warning=false`;
+  const data = {
+    data: jsonData.solicitorCreate.data,
+    draft_id: null,
+    event: {
+      id: "solicitorCreate",
+      summary: "",
+      description: "",
+    },
+    event_token: eventToken,
+    ignore_warning: false,
+  };
+  const submitEventHeaders = {
+    Accept:
+      "application/vnd.uk.gov.hmcts.ccd-data-store-api.create-case.v2+json;charset=UTF-8",
+    Experimental: "true",
+    "Content-type": "application/json; charset=UTF-8",
+  };
+  return await postData(
+    page,
+    submitCaseUrl,
+    submitEventHeaders,
+    JSON.stringify(data),
+  );
+}
