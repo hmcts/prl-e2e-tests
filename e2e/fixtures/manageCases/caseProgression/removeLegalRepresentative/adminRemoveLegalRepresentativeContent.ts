763dfe1efa496142820d7e9746d61dd81ecf81a6
nixonthapa <nixon.thapa2@hmcts.net>
2025-02-17 13:17:15 +0000

    PRL-6966  Remove Legal Representative (#319)
    * add tests for Remove Legal Representative
    
    * added tag @nightly and turned on accessibilityTest

---
+++ e2e/fixtures/manageCases/caseProgression/removeLegalRepresentative/adminRemoveLegalRepresentativeContent.ts
@@ -0,0 +1,6 @@
+export enum AdminRemoveLegalRepresentativeContent {
+  pageTitle = "Remove legal representative",
+  p1 = "Legal Solicitor (John Doe)",
+  p2 = "Legal Solicitor Jr (Jeremy Anderson)",
+  p3 = "Sr Legal Solicitor (Martina Graham)",
+}
---
+++ e2e/tests/manageCases/caseProgression/removeLegalRepresentative/removeLegalRepresentative.spec.ts
@@ -0,0 +1,18 @@
+import { test } from "@playwright/test";
+import Config from "../../../../config.ts";
+import { RemoveLegalRepresentative } from "../../../../journeys/manageCases/caseProgression/removeLegalRepresentative/removeLegalRepresentative.ts";
+
+test.use({ storageState: Config.sessionStoragePath + "solicitor.json" });
+
+test.describe("Complete Remove legal representative event as a court admin", () => {
+  test(`Remove legal representation from applicants and check on Parties tab @regression @nightly`, async ({
+    page,
+    browser,
+  }): Promise<void> => {
+    await RemoveLegalRepresentative.removeLegalRepresentative({
+      page: page,
+      browser: browser,
+      accessibilityTest: true,
+    });
+  });
+});
