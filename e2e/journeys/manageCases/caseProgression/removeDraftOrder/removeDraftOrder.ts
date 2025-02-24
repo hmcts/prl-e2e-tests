a9636aea1bcc23f17bcd0b150526e39106ba94d4
abigailflorence97 <abigail.smith@hmcts.net>
2025-02-24 12:31:23 +0000

    fix

--- e2e/journeys/manageCases/caseProgression/removeDraftOrder/removeDraftOrder.ts
+++ e2e/journeys/manageCases/caseProgression/removeDraftOrder/removeDraftOrder.ts
@@ -1,15 +1,17 @@
-import { Page } from "@playwright/test";
+import { Page, Browser } from "@playwright/test";
 import { Helpers } from "../../../../common/helpers.ts";
 import { RemoveDraftOrder1Page } from "../../../../pages/manageCases/caseProgression/removeDraftOrder/removeDraftOrder1Page.ts";
 import { RemoveDraftOrder2Page } from "../../../../pages/manageCases/caseProgression/removeDraftOrder/removeDraftOrder2Page.ts";
 import { RemoveDraftOrderSubmitPage } from "../../../../pages/manageCases/caseProgression/removeDraftOrder/removeDraftOrderSubmitPage.ts";
 import { submitEvent } from "../../../../common/solicitorCaseCreatorHelper.ts";
 import config from "../../../../config.ts";
+import { DraftAnOrder } from "../../caseWorker/draftAnOrder/draftAnOrder.ts";
 
 interface RemoveDraftOrderParams {
   page: Page;
   accessibilityTest: boolean;
   caseRef: string;
+  browser: Browser;
 }
 
 export class RemoveDraftOrder {
@@ -17,7 +19,31 @@ export class RemoveDraftOrder {
     page,
     accessibilityTest,
     caseRef,
+    browser,
   }: RemoveDraftOrderParams): Promise<void> {
+    await DraftAnOrder.draftAnOrder({
+      page: page,
+      errorMessaging: false,
+      accessibilityTest: false,
+      caseType: "FL401",
+      orderType: "nonMolestation",
+      yesNoToAll: true,
+      howLongWillOrderBeInForce: "noEndDate",
+      willAllPartiesAttendHearing: false,
+      caseRef: caseRef,
+      checkPdf: false,
+      browser: browser,
+    });
+    const caseWorkerPage: Page = await Helpers.openNewBrowserWindow(
+      browser,
+      "caseWorker",
+    );
+    await Helpers.goToCase(
+      caseWorkerPage,
+      config.manageCasesBaseURL,
+      caseRef,
+      "tasks",
+    );
     await submitEvent(page, caseRef, "fl401AddCaseNumber");
     await Helpers.goToCase(page, config.manageCasesBaseURL, caseRef, "tasks");
     await Helpers.chooseEventFromDropdown(page, "Remove draft order");
--- e2e/tests/manageCases/caseProgression/removeDraftOrder/removeDraftOrder.spec.ts
+++ e2e/tests/manageCases/caseProgression/removeDraftOrder/removeDraftOrder.spec.ts
@@ -1,54 +1,36 @@
-import { test, Page } from "@playwright/test";
+import { test } from "@playwright/test";
 import Config from "../../../../config";
-import { DraftAnOrder } from "../../../../journeys/manageCases/caseWorker/draftAnOrder/draftAnOrder";
-import { Helpers } from "../../../../common/helpers.ts";
-import config from "../../../../config.ts";
-import { RemoveDraftOrder } from "../../../../journeys/manageCases/caseProgression/removeDraftOrder/removeDraftOrder.ts";
-import { SolicitorDACaseCreator } from "../../../../common/solicitorDACaseCreator.ts";
+import { Helpers } from "../../../../common/helpers";
+import { RemoveDraftOrder } from "../../../../journeys/manageCases/caseProgression/removeDraftOrder/removeDraftOrder";
+import { SolicitorDACaseCreator } from "../../../../common/solicitorDACaseCreator";
 
 test.use({ storageState: Config.sessionStoragePath + "solicitor.json" });
 
-test.describe("Remove draft order as a court admin for solicitor created CA case.", (): void => {
+test.describe("Remove draft order as a court admin for solicitor-created CA case", (): void => {
   let caseRef: string;
   test.slow();
+
   test.beforeEach(async ({ page }) => {
-    await page.goto(Config.manageCasesBaseURL);
+    await page.goto(Config.manageCasesBaseURLCase);
     caseRef =
       await SolicitorDACaseCreator.createCaseStatementOfTruthAndSubmit(page);
-    await Helpers.goToCase(page, config.manageCasesBaseURL, caseRef, "tasks");
-  });
-
-  test(`Draft an non-Molestation order as a solicitor and then remove draft as a court admin for a DA case created by solicitor. @regression`, async ({
-    page,
-    browser,
-  }): Promise<void> => {
-    await DraftAnOrder.draftAnOrder({
-      page: page,
-      errorMessaging: false,
-      accessibilityTest: false,
-      caseType: "FL401",
-      orderType: "nonMolestation",
-      yesNoToAll: true,
-      howLongWillOrderBeInForce: "noEndDate",
-      willAllPartiesAttendHearing: false,
-      browser: browser,
-      caseRef: caseRef,
-      checkPdf: false,
-    });
-    const caseWorkerPage: Page = await Helpers.openNewBrowserWindow(
-      browser,
-      "caseWorker",
-    );
     await Helpers.goToCase(
-      caseWorkerPage,
-      config.manageCasesBaseURL,
+      page,
+      Config.manageCasesBaseURLCase,
       caseRef,
       "tasks",
     );
+  });
+
+  test("Draft a non-molestation order as a solicitor and remove draft as a court admin for a DA case created by solicitor. @regression", async ({
+    page,
+    browser,
+  }): Promise<void> => {
     await RemoveDraftOrder.removeDraftOrder({
-      page: caseWorkerPage,
+      page,
       accessibilityTest: true,
-      caseRef: caseRef,
+      caseRef,
+      browser: browser,
     });
   });
 });
