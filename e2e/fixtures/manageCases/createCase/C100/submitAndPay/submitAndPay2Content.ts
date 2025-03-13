122473b687e04d0f5fbb79f0657a2cd669e8a58a
RobL681 <robert.lacey@justice.gov.uk>
2025-03-13 09:14:31 +0000

    Update json data to work with demo (#344)
    * Update json data to work with demo
    
    * Fix tests to run with demo env
    
    * Hardcode jenkins file to use demo env - needs to be fixed later
    
    * Add comments to tests to trigger them in the pipeline
    
    * Changes to jenkins file
    
    * Remove references to specific aat user
    
    * Get env for pipeline from the given url param
    
    * debug matching environment
    
    * debug matching environment
    
    * Change which json data is used based on the environment (aat or demo)
    
    * Remove comments to trigger tests and fix a test
    
    * Change jenkins default manage cases url back to aat
    
    * Fix manage docs test
    
    * fix tests

--- e2e/fixtures/manageCases/createCase/C100/submitAndPay/submitAndPay2Content.ts
+++ e2e/fixtures/manageCases/createCase/C100/submitAndPay/submitAndPay2Content.ts
@@ -1,7 +1,8 @@
 export enum SubmitAndPay2Content {
   h3 = "Declaration",
   p1 = "I understand that proceedings for contempt of court may be brought against anyone who makes, or causes to be made, a false statement in a document verified by a statement of truth without an honest belief in its truth.",
-  p2 = "The applicant believes that the facts stated in this form and any continuation sheets are true. AAT Solicitor is authorised by the applicant to sign this statement.",
+  p21 = "The applicant believes that the facts stated in this form and any continuation sheets are true.",
+  p22 = "is authorised by the applicant to sign this statement.",
   formHint = "This option should only be selected if the legal representative is signing the form on behalf of the applicant.",
   formLabelAgreeWithStatement = "I agree with this statement",
   h3Hidden1 = "Next step - Submit the application",
--- e2e/pages/manageCases/caseTabs/FL401/fl401ConfidentialDetailsTabPage.ts
+++ e2e/pages/manageCases/caseTabs/FL401/fl401ConfidentialDetailsTabPage.ts
@@ -1,4 +1,4 @@
-import { Page, expect } from "@playwright/test";
+import { expect, Page } from "@playwright/test";
 import { Selectors } from "../../../../common/selectors";
 import { Helpers } from "../../../../common/helpers";
 import AccessibilityTestHelper from "../../../../common/accessibilityTestHelper";
@@ -183,11 +183,6 @@ export class FL401ConfidentialDetailsTabPage {
             .locator("span"),
         ).toBeVisible(),
         expect(
-          restrictedDocsSection
-            .getByRole("cell", { name: "PRL AAT AM Swansea HCA", exact: true })
-            .locator("span"),
-        ).toBeVisible(),
-        expect(
           restrictedDocsSection.getByRole("columnheader", {
             name: FL401ConfidentialDetailsTabContent.text16_reasonRestrict,
           }),
@@ -226,19 +221,16 @@ export class FL401ConfidentialDetailsTabPage {
         expect(
           page
             .getByRole("cell", {
-              name: `${FL401ConfidentialDetailsTabContent.refugeSectionDocument} ${FL401ConfidentialDetailsTabContent.testPdf} Uploaded by PRL AAT AM Swansea HCA`,
-              exact: true,
+              name: `${FL401ConfidentialDetailsTabContent.refugeSectionDocument} ${FL401ConfidentialDetailsTabContent.testPdf} Uploaded by`,
             })
-            .getByRole("link"),
+            .getByRole("link")
+            .filter({ hasText: FL401ConfidentialDetailsTabContent.testPdf }),
         ).toBeVisible(),
         expect(
           confidentialDocsSection.getByText(
             FL401ConfidentialDetailsTabContent.text16_uploadedBy,
             { exact: true },
           ),
-        ).toBeVisible(),
-        expect(
-          confidentialDocsSection.getByText("PRL AAT AM Swansea HCA"),
         ).toBeVisible(),
         expect(
           confidentialDocsSection.getByText(documentCategory),
