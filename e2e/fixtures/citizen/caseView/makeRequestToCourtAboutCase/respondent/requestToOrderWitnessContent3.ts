a9e336d9db6680069054e0679bf22658a509e9c4
elizereyes123 <107991381+elizereyes123@users.noreply.github.com>
2025-03-31 14:05:48 +0100

    Make a request to order a witness to attend court FP25 (#370)
    * Respondent making a request to order witness to court FP25
    
    * Applicant making a request to order witness to court FP25

---
+++ e2e/fixtures/citizen/caseView/makeRequestToCourtAboutCase/respondent/requestToOrderWitnessContent3.ts
@@ -0,0 +1,11 @@
+export enum RequestToOrderWitnessContent3 {
+  GovukCaptionL = "Make a request to order a witness to attend court",
+  GovukHeadingL = "Help with fees",
+  GovukBodyM = "The cost of this application is £58.00. You can check the help with fees guidance on",
+  GovukLink = "GOV.UK (opens in a new tab)",
+  GovukBodyM2 = "to find out if you are eligible for support.",
+  GovukFieldsetLegend = "Will you be using help with fees to pay for this application?",
+  GovukLabel = "Yes",
+  GovukLabel1 = "No",
+  continue = "Continue",
+}
