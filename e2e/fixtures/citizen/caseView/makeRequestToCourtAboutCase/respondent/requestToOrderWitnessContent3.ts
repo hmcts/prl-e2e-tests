b401b91e3207f95c6af42c75bb6b0048f9a27e80
abigailflorence97 <abigail.smith@hmcts.net>
2025-04-01 15:04:53 +0100

    update fees

--- e2e/fixtures/citizen/caseView/makeRequestToCourtAboutCase/respondent/requestToOrderWitnessContent3.ts
+++ e2e/fixtures/citizen/caseView/makeRequestToCourtAboutCase/respondent/requestToOrderWitnessContent3.ts
@@ -1,7 +1,7 @@
 export enum RequestToOrderWitnessContent3 {
   GovukCaptionL = "Make a request to order a witness to attend court",
   GovukHeadingL = "Help with fees",
-  GovukBodyM = "The cost of this application is £58.00. You can check the help with fees guidance on",
+  GovukBodyM = "The cost of this application is £60.00. You can check the help with fees guidance on",
   GovukLink = "GOV.UK (opens in a new tab)",
   GovukBodyM2 = "to find out if you are eligible for support.",
   GovukFieldsetLegend = "Will you be using help with fees to pay for this application?",
--- e2e/fixtures/citizen/caseView/requestMoreTime/helpWithFeesContent.ts
+++ e2e/fixtures/citizen/caseView/requestMoreTime/helpWithFeesContent.ts
@@ -1,7 +1,7 @@
 export enum HelpWithFeesContent {
   GovukCaptionL = "Request more time to do what is required by a court order",
   GovukHeadingL = "Help with fees",
-  GovukBodyM1 = "The cost of this application is £58.00. You can check the help with fees guidance on",
+  GovukBodyM1 = "The cost of this application is £60.00. You can check the help with fees guidance on",
   GovukLink = "GOV.UK (opens in a new tab)",
   GovukBodyM2 = "to find out if you are eligible for support.",
   GovukFieldsetLegend = "Will you be using help with fees to pay for this application?",
