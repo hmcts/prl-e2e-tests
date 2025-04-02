95aa739bba291d61779c3b02dc0e89c14f153289
abigailflorence97 <abigail.smith@hmcts.net>
2025-04-02 15:11:18 +0100

    remove comment to run tests

--- e2e/tests/manageCases/createCase/C100/c100ApplicantDetails.spec.ts
+++ e2e/tests/manageCases/createCase/C100/c100ApplicantDetails.spec.ts
@@ -3,7 +3,7 @@ import Config from "../../../../config";
 import { C100ApplicantDetails } from "../../../../journeys/manageCases/createCase/C100ApplicantDetails/c100ApplicantDetails";
 
 test.use({ storageState: Config.sessionStoragePath + "solicitor.json" });
-//comment to runs tests
+
 test.describe("C100 Create case - Applicant Details tests", (): void => {
   test(`Complete the C100 applicant details event as a solicitor with the following options:
   Not Accessibility testing,
--- e2e/tests/manageCases/createCase/FL401/fl401ApplicantDetails.spec.ts
+++ e2e/tests/manageCases/createCase/FL401/fl401ApplicantDetails.spec.ts
@@ -3,7 +3,7 @@ import { FL401ApplicantDetails } from "../../../../journeys/manageCases/createCa
 import Config from "../../../../config";
 
 test.use({ storageState: Config.sessionStoragePath + "solicitor.json" });
-//comment to run tests
+
 test.describe("FL401 Create case applicant details tests", (): void => {
   test(`Complete the FL401 applicant details event as a solicitor with the following options:
   Not Accessibility testing,
