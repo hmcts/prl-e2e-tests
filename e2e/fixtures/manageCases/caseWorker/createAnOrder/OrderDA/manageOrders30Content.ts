eb97092c12df77539221f50575be41ed2b829be5
Abigail Smith <148337562+abigailsmith97@users.noreply.github.com>
2025-04-04 16:04:41 +0100

    Fix tests failing in pipeline: (manage docs /remove draft order /create an order/ upload an order tests) (#384)

--- e2e/fixtures/manageCases/caseWorker/createAnOrder/OrderDA/manageOrders30Content.ts
+++ e2e/fixtures/manageCases/caseWorker/createAnOrder/OrderDA/manageOrders30Content.ts
@@ -1,6 +1,6 @@
 export enum ManageOrders30Content {
   formLabel1 = "Directions to admin: (Optional)",
   formLabel2 = "Once this order is complete, can the application be served?",
-  formHint = "Give any further directions, for example if special measures are needed",
+  formHint = "Give any further directions, for example if there are listing requirements or special measures needed.",
   loremIpsum = "Lorem Ipsum",
 }
