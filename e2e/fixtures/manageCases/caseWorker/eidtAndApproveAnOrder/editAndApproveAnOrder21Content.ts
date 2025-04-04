2171aae2d301341e39698ee62888c785a58266b0
Abigail Smith <148337562+abigailsmith97@users.noreply.github.com>
2025-04-04 16:25:30 +0100

    fix edit and approve a CA order tests (#385)

--- e2e/fixtures/manageCases/caseWorker/eidtAndApproveAnOrder/editAndApproveAnOrder21Content.ts
+++ e2e/fixtures/manageCases/caseWorker/eidtAndApproveAnOrder/editAndApproveAnOrder21Content.ts
@@ -1,6 +1,6 @@
 export enum EditAndApproveAnOrder21Content {
   govUkHeadingL = "Edit and approve a draft order",
   formLabel = "Directions to admin: (Optional)",
-  formHint = "Give any further directions, for example if special measures are needed",
+  formHint = "Give any further directions, for example if there are listing requirements or special measures needed.",
   directionsText = "Test directions to admin",
 }
