3bf7026dbec5b810d01135d27c369de0d25ba763
Luciano <luciano.marsilio@hmcts.net>
2025-04-24 17:13:43 +0100

    adding missing fields Judges Title and name

---
+++ e2e/journeys/manageCases/caseWorker/uploadAnOrder/c100ManageOrdersUploadJourney.ts
@@ -0,0 +1,52 @@
+import { Page } from "@playwright/test";
+import { Helpers } from "../../../../common/helpers";
+import {
+  uploadOrderC100Options,
+} from "../../../../common/types.ts";
+import { ManageOrders1PageUpload } from "../../../../pages/manageCases/caseWorker/uploadAnOrder/manageOrders1PageUpload";
+import { ManageOrders3PageCA } from "../../../../pages/manageCases/caseWorker/uploadAnOrder/manageOrders3PageCA";
+import {
+  ManageOrders5PageCA
+} from "../../../../pages/manageCases/caseWorker/uploadAnOrder/manageOrders5PageCA";
+import { ManageOrders24PageCA } from "../../../../pages/manageCases/caseWorker/uploadAnOrder/manageOrders24PageCA";
+import { SubmitPage } from "../../../../pages/manageCases/caseWorker/createAnOrder/orderCA/submitPage";
+
+interface C100ManageOrdersOptions {
+  page: Page;
+  accessibilityTest: boolean;
+  yesNoManageOrders: boolean;
+  uploadOrderC100Options: uploadOrderC100Options;
+}
+
+export class C100ManageOrdersUploadJourney {
+  public static async c100ManageOrdersUploadJourney({
+                                         page,
+                                         accessibilityTest,
+                                                      yesNoManageOrders,
+                                                      uploadOrderC100Options,
+                                       }: C100ManageOrdersOptions): Promise<void> {
+    await Helpers.chooseEventFromDropdown(page, `Manage orders`);
+    await ManageOrders1PageUpload.manageOrders1PageUpload({
+      page,
+      accessibilityTest,
+    });
+    await ManageOrders3PageCA.manageOrders3PageCA({
+      page,
+      accessibilityTest,
+      yesNoManageOrders,
+      uploadOrderC100Options,
+    });
+    await ManageOrders5PageCA.manageOrders5PageCA({
+      page,
+      accessibilityTest,
+    });
+    await ManageOrders24PageCA.manageOrders24PageCA({
+      page,
+      accessibilityTest,
+    });
+    await SubmitPage.submitPage({
+      page,
+      accessibilityTest,
+    });
+  }
+}
---
+++ e2e/tests/manageCases/caseWorker/uploadAnOrder/uploadAnOrderCaseworker.spec.ts
@@ -0,0 +1,31 @@
+import { test } from "@playwright/test";
+import Config from "../../../../config";
+import { C100UploadAnOrder } from "../../../../journeys/manageCases/caseWorker/uploadAnOrder/UploadC100Order.ts";
+
+test.use({ storageState: Config.sessionStoragePath + "caseWorker.json" });
+
+test.describe("Upload an order tests", (): void => {
+  test(`Complete Uploading an order as a Caseworker with the following options:
+  Case: C100,
+  Not accessibility testing. 
+  @regression`, async ({ page }): Promise<void> => {
+    await C100UploadAnOrder.c100UploadAnOrder({
+      page: page,
+      accessibilityTest: false,
+      solicitorCaseCreateType: "C100",
+      yesNoManageOrders: false,
+      uploadOrderC100Options: "Child arrangements, specific issue or prohibited steps order (C43)",
+    });
+  });
+});
+//   test(`Complete Creating an order as a Caseworker with the following options:
+//   Case: C100,
+//   Accessibility testing: yes.
+//   @nightly @accessibility`, async ({ page }): Promise<void> => {
+//     await C100UploadAnOrder.c100UploadAnOrder({
+//       page: page,
+//       accessibilityTest: true,
+//       solicitorCaseCreateType: "C100",
+//     });
+//   });
+// });
