5a268ee0fda47fd934306c4b02068a41ef239ce6
lucianotmarsilio <107107435+lucianotmarsilio@users.noreply.github.com>
2025-07-11 14:59:11 +0100

    Fpvtl 560 upload order by Solicitor (#434)
    * initial draft WIP
    
    * adding upload order by cw test- happy paht
    
    * adding missing fields Judges Title and name
    
    * adding remaining pages
    
    * remove comments
    
    * adding upload order scenario as caseworker with the new judge title/name fields
    
    * latest review fixes
    
    * tests passing locally - last bits
    
    * fixing impact on existing judge upload order test
    
    * addressing the review comments
    
    * removing unnecessary comment
    
    * addressing new review comments
    
    * initial draft adding upload CA order by Solicitor
    
    * fixed tests passing locally
    
    * addressing yarn lint issues and redundancies
    
    * lint potential fixes
    
    * fix 2 linting
    
    * fixing wrong condition
    
    * adding extra timeout before new browser window
    
    * fixing conflicts local
    
    * fixes Mon 7 upload order solicitor
    
    * addressing review comment
    
    * removing comment
    
    ---------
    
    Co-authored-by: Abigail Smith <148337562+abigailsmith97@users.noreply.github.com>

--- e2e/fixtures/manageCases/caseTabs/C100/c100DraftOrdersTabContent.ts
+++ e2e/fixtures/manageCases/caseTabs/C100/c100DraftOrdersTabContent.ts
@@ -2,4 +2,6 @@ export enum C100DraftOrdersTabContent {
   tabTitle = "Draft orders",
   subTitle1 = "Draft orders 1",
   typeOfOrderField = "Type of order",
+  otherdetails1= "Order made by",
+  otherdetails2 = "Status",
 }
