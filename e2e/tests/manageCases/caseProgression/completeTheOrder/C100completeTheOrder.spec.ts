ac94af0e2df2cb7db7d43a49cb9a381dcd4544e4
abhik275 <abhinav.kumar@justice.gov.uk>
2025-07-22 15:14:31 +0100

    added regression tag and removed test without accessibility

--- e2e/tests/manageCases/caseProgression/completeTheOrder/C100completeTheOrder.spec.ts
+++ e2e/tests/manageCases/caseProgression/completeTheOrder/C100completeTheOrder.spec.ts
@@ -35,25 +35,10 @@ test.describe("Complete the Order task for C100 case tests.", () => {
   test(`Complete Creating and serving C100 order as a Caseworker with the following options:
   Case: C100,
   Accessibility testing: yes. 
-  @nightly @accessibility`, async ({ page }): Promise<void> => {
+  @nightly @accessibility @regression`, async ({ page }): Promise<void> => {
     await C100CompleteTheOrder.C100completeTheOrder({
       page: page,
       accessibilityTest: true,
-      solicitorCaseCreateType: "C100",
-      isUploadOrder: false,
-      checkOption: "noCheck", //options passed could be either noCheck or judgeOrLegalAdvisorCheck or managerCheck
-      serveOrderNow: true, //select to serve order instantly
-      personallyServed: true,
-    });
-  });
-
-  test(`Complete Creating and serving C100 order as a Caseworker with the following options:
-  Case: C100,
-  Not accessibility testing. 
-  @regression`, async ({ page }): Promise<void> => {
-    await C100CompleteTheOrder.C100completeTheOrder({
-      page: page,
-      accessibilityTest: false,
       solicitorCaseCreateType: "C100",
       isUploadOrder: false,
       checkOption: "noCheck", //options passed could be either noCheck or judgeOrLegalAdvisorCheck or managerCheck
