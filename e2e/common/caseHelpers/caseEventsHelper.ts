c88ad2a812dc2c098cfb5f84654bb5f2afd76184
RobL681 <robert.lacey@justice.gov.uk>
2025-07-31 13:35:04 +0100

    Remove import

--- e2e/common/caseHelpers/caseEventsHelper.ts
+++ e2e/common/caseHelpers/caseEventsHelper.ts
@@ -6,7 +6,6 @@ import { CompleteTheOrder } from "../../journeys/manageCases/caseProgression/com
 import { applicationSubmittedBy, createOrderFL401Options } from "../types.ts";
 import { ConfidentialityCheck } from "../../journeys/manageCases/caseProgression/confidentilityCheck/confidentialityCheck.js";
 import { CaseEventUtils } from "../../utils/caseEvent.utils.js";
-import { ServiceOfApplication } from "../../journeys/manageCases/caseProgression/serviceOfApplication/serviceOfApplication.js";
 
 // Note: These methods assume the current page context is court admin
 export async function completeCheckApplicationAndSendToGatekeeper(
