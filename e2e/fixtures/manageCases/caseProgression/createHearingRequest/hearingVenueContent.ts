442221fba8213628342dbdd8d304cdc3e52cc2a3
RobL681 <robert.lacey@justice.gov.uk>
2025-08-11 09:16:06 +0100

    Submit fl401StatementOfTruthAndSubmit event via API to send case to Swansea court and revert expected hearing location back to Swansea court

--- e2e/fixtures/manageCases/caseProgression/createHearingRequest/hearingVenueContent.ts
+++ e2e/fixtures/manageCases/caseProgression/createHearingRequest/hearingVenueContent.ts
@@ -2,6 +2,6 @@ export enum HearingVenueContent {
   govUkHeadingL = "What are the hearing venue details?",
   GovukInsetText = "You can check the venue has the required facilities or reasonable adjustments using",
   GovukHint = "If this is a fully remote hearing you must still select the court or tribunal which will be managing the case.",
-  location = "Aberystwyth Justice Centre",
+  location = "Swansea Civil And Family Justice Centre",
   continue = "Continue",
 }
