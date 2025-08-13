8204eb0f96eadee56897207babb0afefee7d1627
RobL681 <robert.lacey@justice.gov.uk>
2025-08-13 12:54:05 +0100

    Update spec file to use new page objects

--- e2e/tests/manageCases/caseProgression/checkApplication/checkApplication.spec.ts
+++ e2e/tests/manageCases/caseProgression/checkApplication/checkApplication.spec.ts
@@ -1,38 +1,43 @@
 import { test } from "../../../fixtures.ts";
 import config from "../../../../utils/config.utils.ts";
 import { Helpers } from "../../../../common/helpers.ts";
-import { CheckApplicationJourney } from "../../../../journeys/manageCases/caseProgression/checkApplication/checkApplicationJourney.ts";
 
 test.use({ storageState: config.sessionStoragePath + "caseWorker.json" });
 
 test.describe("Check Application task for DA Solicitor case tests.", () => {
-  let ccdRef: string = "";
+  let caseNumber: string;
 
-  test.beforeEach(async ({ page, browser, caseEventUtils }) => {
-    ccdRef = await caseEventUtils.createDACase(browser);
-    await Helpers.goToCase(
-      page,
-      config.manageCasesBaseURLCase,
-      ccdRef,
-      "tasks",
+  test.beforeEach(
+    async ({ page, browser, caseEventUtils, caseAccessViewPage }) => {
+      caseNumber = await caseEventUtils.createDACase(browser);
+      await Helpers.goToCase(
+        page,
+        config.manageCasesBaseURLCase,
+        caseNumber,
+        "tasks",
+      );
+      await caseAccessViewPage.exuiHeader.checkIsVisible();
+    },
+  );
+
+  test("Complete Task - Check Application with accessibility test. @nightly @accessibility @regression", async ({
+    caseAccessViewPage,
+    fl401AddCaseNumber1Page,
+    fl401AddCaseNumberSubmitPage,
+  }): Promise<void> => {
+    await caseAccessViewPage.exuiTasksContainer.assignTaskToMeAndTriggerNextSteps(
+      "Check Application",
+      "Add Case Number",
     );
-  });
-
-  test("Complete Task - Check Application without accessibility test. @nightly @regression", async ({
-    page,
-  }): Promise<void> => {
-    await CheckApplicationJourney.checkApplication({
-      page,
-      accessibilityTest: false,
-    });
-  });
-
-  test("Complete Task - Check Application with accessibility test. @regression @accessibility", async ({
-    page,
-  }): Promise<void> => {
-    await CheckApplicationJourney.checkApplication({
-      page,
-      accessibilityTest: true,
-    });
+    await fl401AddCaseNumber1Page.checkPageContent();
+    await fl401AddCaseNumber1Page.fillInFields();
+    await fl401AddCaseNumber1Page.continueButton.click();
+    await fl401AddCaseNumberSubmitPage.checkPageContents();
+    await fl401AddCaseNumberSubmitPage.saveAndContinueButton.click();
+    await caseAccessViewPage.alertBanner.assertEventAlert(
+      caseNumber,
+      "Add case number",
+    );
+    await caseAccessViewPage.caseHeader.checkFamilyManNumber("1234");
   });
 });
