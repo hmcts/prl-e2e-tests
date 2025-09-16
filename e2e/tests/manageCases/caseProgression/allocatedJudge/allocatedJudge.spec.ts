de197e09bd51e3fb39cd9280daa7072cf8eae180
RobL681 <robert.lacey@justice.gov.uk>
2025-09-16 16:14:37 +0100

    Replace allocatedJudge.spec.ts test to use new page objects

--- e2e/tests/manageCases/caseProgression/allocatedJudge/allocatedJudge.spec.ts
+++ e2e/tests/manageCases/caseProgression/allocatedJudge/allocatedJudge.spec.ts
@@ -1,7 +1,6 @@
 import { test } from "../../../fixtures.ts";
 import config from "../../../../utils/config.utils.ts";
 import { Helpers } from "../../../../common/helpers.ts";
-import { AllocatedJudge } from "../../../../journeys/manageCases/caseProgression/allocatedJudge/allocatedJudge.ts";
 
 test.use({ storageState: config.sessionStoragePath + "caseWorker.json" });
 
@@ -18,12 +17,24 @@ test.describe("Allocate a judge to the case", () => {
     );
   });
 
-  test("Allocate a Judge to a DA case @nightly @regression", async ({
-    page,
-  }) => {
-    await AllocatedJudge.allocatedJudge({
-      page: page,
-      accessibilityTest: true,
+  [
+    { isSpecificJudgeOrLegalAdviser: true },
+    { isSpecificJudgeOrLegalAdviser: false },
+  ].forEach(({ isSpecificJudgeOrLegalAdviser }) => {
+    test(`Allocate a Judge to a DA case isSpecificJudgeOrLegalAdviser: ${isSpecificJudgeOrLegalAdviser} @nightly @regression - POM`, async ({
+      summaryPage,
+      allocatedJudge1Page,
+      allocatedJudgeSubmitPage,
+    }) => {
+      await summaryPage.chooseEventFromDropdown("Allocated judge");
+      await allocatedJudge1Page.checkPageContents();
+      await allocatedJudge1Page.fillInFields(isSpecificJudgeOrLegalAdviser);
+      await allocatedJudge1Page.clickContinue();
+      await allocatedJudgeSubmitPage.checkPageContents(
+        isSpecificJudgeOrLegalAdviser,
+      );
+      await allocatedJudgeSubmitPage.clickSubmit();
+      // TODO: Check roles and access tab to make sure judge has been assigned correctly
     });
   });
 });
