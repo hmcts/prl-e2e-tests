e88dae81d015a9d440ec1606465a9eca559de06b
RobL681 <robert.lacey@justice.gov.uk>
2025-09-17 11:22:32 +0100

    Assert judge section of caes summary

--- e2e/pageObjects/pages/exui/caseView/summary.po.ts
+++ e2e/pageObjects/pages/exui/caseView/summary.po.ts
@@ -1,18 +1,13 @@
 import { CaseAccessViewPage } from "./caseAccessView.po.js";
 import { expect, Locator, Page } from "@playwright/test";
-import {
-  c100SolicitorEvents,
-  fl401SolicitorEvents,
-  fl401SubmittedSolicitorEvents,
-  fl401JudiciaryEvents,
-  WACaseWorkerActions,
-  fl401CaseWorkerActions,
-  courtAdminEvents,
-  amendEvents,
-} from "../../../../common/types.ts";
 
 export class SummaryPage extends CaseAccessViewPage {
-  readonly goButton: Locator = this.page.getByRole("button", { name: "Go" });
+  readonly allocatedJudgeLabel: Locator = this.page.locator(
+    "#case-viewer-field-read--allocatedJudgeLabel",
+  );
+  readonly allocatedJudgeDetails: Locator = this.page.locator(
+    "#case-viewer-field-read--allocatedJudgeDetails",
+  );
 
   constructor(page: Page) {
     super(page);
@@ -22,26 +17,54 @@ export class SummaryPage extends CaseAccessViewPage {
     await this.page.getByRole("tab", { name: "Summary" }).click();
   }
 
-    await this.page.selectOption("#next-step", chosenEvent);
-
-    await expect(this.goButton).toBeEnabled();
-
-    await expect
-      .poll(
-        async () => {
-          const goButtonStillVisible = await this.goButton.isVisible();
-          if (goButtonStillVisible) {
-            await this.goButton.click();
-          }
-          return goButtonStillVisible;
-        },
-        {
-          // Retry every 15 seconds
-          intervals: [15_000],
-          // Timeout after 5 minutes
-          timeout: 300_000,
-        },
-      )
-      .toBeFalsy();
+  async assertAllocatedJudgeSection(
+    isSpecificJudgeOrLegalAdviser: boolean,
+    judgeTier: string,
+    court: string,
+    judgeLastName?: string,
+    judgeEmailAddress?: string,
+  ): Promise<void> {
+    await expect(this.allocatedJudgeLabel).toBeVisible();
+    await expect(
+      this.allocatedJudgeDetails.getByText("Allocated judge", { exact: true }),
+    ).toBeVisible();
+    if (isSpecificJudgeOrLegalAdviser) {
+      await expect(
+        this.allocatedJudgeDetails.getByText("Tier of judge", { exact: true }),
+      ).toBeVisible();
+    } else {
+      await expect(
+        this.allocatedJudgeDetails.getByText("Tier of judiciary", {
+          exact: true,
+        }),
+      ).toBeVisible();
+    }
+    await expect(
+      this.allocatedJudgeDetails.getByText(judgeTier, { exact: true }),
+    ).toBeVisible();
+    await expect(
+      this.allocatedJudgeDetails.getByText("Last name", { exact: true }),
+    ).toBeVisible();
+    if (judgeLastName) {
+      await expect(
+        this.allocatedJudgeDetails.getByText(judgeLastName, { exact: true }),
+      ).toBeVisible();
+    }
+    await expect(
+      this.allocatedJudgeDetails.getByText("Email address", { exact: true }),
+    ).toBeVisible();
+    if (judgeEmailAddress) {
+      await expect(
+        this.allocatedJudgeDetails.getByText(judgeEmailAddress, {
+          exact: true,
+        }),
+      ).toBeVisible();
+    }
+    await expect(
+      this.allocatedJudgeDetails.getByText("Court name", { exact: true }),
+    ).toBeVisible();
+    await expect(
+      this.allocatedJudgeDetails.getByText(court, { exact: true }),
+    ).toBeVisible();
   }
 }
