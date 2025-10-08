dac3ba395b214f68a0cce7a8bd0359170a758e44
RobL681 <robert.lacey@justice.gov.uk>
2025-10-08 09:27:45 +0100

    Add test comments to trigger tests on PR

--- e2e/tests/manageCases/caseProgression/removeDraftOrder/removeDraftOrder.spec.ts
+++ e2e/tests/manageCases/caseProgression/removeDraftOrder/removeDraftOrder.spec.ts
@@ -4,7 +4,7 @@ import { RemoveDraftOrder } from "../../../../journeys/manageCases/caseProgressi
 import { test } from "../../../fixtures.ts";
 
 test.use({ storageState: Config.sessionStoragePath + "solicitor.json" });
-
+// TEST COMMENT
 test.describe("Remove draft order as a court admin for solicitor-created DA case", (): void => {
   let caseRef: string;
   test.slow();
