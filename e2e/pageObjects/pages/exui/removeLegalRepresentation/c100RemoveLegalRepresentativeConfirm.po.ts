b09a7ddf44385d24ebfa76f67c1fc5694fd5fdb6
Abigail Smith <148337562+abigailsmith97@users.noreply.github.com>
2025-11-12 09:25:40 +0000

    Move common actions to base page and event po (#602)
    * move common actions to base page and event po
    
    * update po

--- e2e/pageObjects/pages/exui/removeLegalRepresentation/c100RemoveLegalRepresentativeConfirm.po.ts
+++ e2e/pageObjects/pages/exui/removeLegalRepresentation/c100RemoveLegalRepresentativeConfirm.po.ts
@@ -1,7 +1,5 @@
 import { EventPage } from "../eventPage.po.js";
 import { expect, Locator, Page } from "@playwright/test";
-import { Selectors } from "../../../../common/selectors.js";
-import { CommonStaticText } from "../../../../common/commonStaticText.js";
 
 export class C100RemoveLegalRepresentativeConfirmPage extends EventPage {
   private readonly confirmationHeader: Locator = this.page.locator(
@@ -15,10 +13,6 @@ export class C100RemoveLegalRepresentativeConfirmPage extends EventPage {
         "Sr Legal Solicitor is no longer representing Martina Graham in this case. Legal Solicitor is no longer representing John Doe in this case. Legal Solicitor Jr is no longer representing Jeremy Anderson in this case. All other parties have been notified about this change",
     },
   );
-  private readonly closeAndReturnToCaseDetailsButton: Locator =
-    this.page.locator(Selectors.button, {
-      hasText: CommonStaticText.closeAndReturnToCaseDetails,
-    });
 
   constructor(page: Page) {
     super(page, "Remove legal representative");
@@ -29,9 +23,5 @@ export class C100RemoveLegalRepresentativeConfirmPage extends EventPage {
     await expect(this.confirmationHeader).toBeVisible();
     await expect(this.confirmationBody).toBeVisible();
     await expect(this.closeAndReturnToCaseDetailsButton).toBeVisible();
-  }
-
-  async clickCloseAndReturnToCaseDetails(): Promise<void> {
-    await this.closeAndReturnToCaseDetailsButton.click();
   }
 }
