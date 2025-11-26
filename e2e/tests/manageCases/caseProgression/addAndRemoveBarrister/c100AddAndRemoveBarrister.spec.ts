aa569576838e0f614bab21a283aeb7635cfc7c88
Luciano <luciano.marsilio@hmcts.net>
2025-11-26 09:28:34 +0000

    fix for snapshot path

--- e2e/tests/manageCases/caseProgression/addAndRemoveBarrister/c100AddAndRemoveBarrister.spec.ts
+++ e2e/tests/manageCases/caseProgression/addAndRemoveBarrister/c100AddAndRemoveBarrister.spec.ts
@@ -120,7 +120,7 @@ test.describe("Add/Remove Barrister for CA case", () => {
         );
         await c100AdminAddBarrister1Page.clickContinue();
         await c100AdminAddBarristerSubmit.assertPageContents(
-          ["caseProgression", "Add barrister"],
+          ["caseProgression", "addBarrister"],
           addBarristerSnapshotName,
         );
         // await axeUtils.audit(); Note: to remove this comment once FPVTL-1357 fix is deployed
@@ -146,7 +146,7 @@ test.describe("Add/Remove Barrister for CA case", () => {
         );
         await c100AdminRemoveBarrister1Page.clickContinue();
         await c100AdminRemoveBarristerSubmit.assertPageContents(
-          ["caseProgression", "Remove barrister"],
+          ["caseProgression", "removeBarrister"],
           removeBarristerSnapshotName,
         );
         await axeUtils.audit();
