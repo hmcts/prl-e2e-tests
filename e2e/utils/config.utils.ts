d94ca6a9a6659a0479dccf09d7c0872c7d153760
Jahnavi <jahnavi1satya@gmail.com>
2025-12-01 17:45:31 +0000

    Update config.utils.ts

--- e2e/utils/config.utils.ts
+++ e2e/utils/config.utils.ts
@@ -45,7 +45,7 @@ export class Config {
   public static readonly citizenFrontendBaseURL: string =
     Config.ensureTrailingSlash(
       process.env.CITIZEN_FRONTEND_BASE_URL ||
-        "https://prl-citizen-frontend-pr-1942.preview.platform.hmcts.net/",
+        "https://privatelaw.aat.platform.hmcts.net/",
     );
 
   public static readonly manageCasesBaseURLCase: string =
