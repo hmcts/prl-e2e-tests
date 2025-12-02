b6ee7c45795c3869e02fd12dc37f4a6ac159fab3
RobL681 <robert.lacey@justice.gov.uk>
2025-12-02 14:20:31 +0000

    Add debug statements

--- e2e/pageObjects/components/exui/checkYourAnswersTable.component.ts
+++ e2e/pageObjects/components/exui/checkYourAnswersTable.component.ts
@@ -14,11 +14,14 @@ export class CheckYourAnswersTableComponent {
     const table: Locator = this.page.locator(
       tableSelector ? tableSelector : defaultCyaTableSelector,
     );
+    console.log("Sceenshot path: " + screenShotPath);
     const providedScreenshotName: string =
       screenShotPath[screenShotPath.length - 1];
+    console.log("Provided screenshot name: " + providedScreenshotName);
     const screenshotPathCopy: string[] = Array.from(screenShotPath);
     screenshotPathCopy[screenShotPath.length - 1] =
       `${providedScreenshotName}-cya.png`;
+    console.log("Sceenshot path copy: " + screenshotPathCopy);
     await expect(table).toHaveScreenshot(screenshotPathCopy, {
       maxDiffPixelRatio: 0.02,
     });
