7d11dac8360ed2b74506a327cbc0ef5eb0afad15
Luciano-Marsilio <107107435+lucianotmarsilio@users.noreply.github.com>
2025-12-15 11:00:42 +0000

    Fpvtl 1607 case name FTs (#642)
    * initial commit
    
    * test passing for the Appls surname
    
    * linting
    
    * fixing expects + linting - passing test
    
    * removing case list check + renaming + adding DA all passing
    
    * Refactor test to use role based pages
    
    * addressing review comments + new rolebased fixtures
    
    * updating the timeout
    
    * removing unnecessary comments and steps as per review
    
    ---------
    
    Co-authored-by: RobL681 <robert.lacey@justice.gov.uk>

--- e2e/pageObjects/pages/exui/amendApplicantDetails/amendApplicantDetails1.po.ts
+++ e2e/pageObjects/pages/exui/amendApplicantDetails/amendApplicantDetails1.po.ts
@@ -1,9 +1,40 @@
-import { Page } from "@playwright/test";
+import { Page, Locator } from "@playwright/test";
 import { EventPage } from "../eventPage.po.ts";
 
 // more details and page asserts to be added as needed in the future
+
 export class AmendApplicantDetails1 extends EventPage {
+  private readonly c100applicantFirstNameField: Locator = this.page
+    .locator("#applicants_0_firstName")
+    .first();
+
+  private readonly c100applicantLastNameField: Locator = this.page
+    .locator("#applicants_0_lastName")
+    .first();
+  private readonly fl401applicantFirstNameField: Locator = this.page
+    .locator("#applicantsFL401_firstName")
+    .first();
+  private readonly fl401applicantLastNameField: Locator = this.page
+    .locator("#applicantsFL401_lastName")
+    .first();
+
   constructor(page: Page) {
     super(page, "Amend applicant details");
+  }
+
+  async c100updateApplicantsName(
+    firstname: string,
+    surname: string,
+  ): Promise<void> {
+    await this.c100applicantFirstNameField.fill(firstname);
+    await this.c100applicantLastNameField.fill(surname);
+  }
+
+  async fl401updateApplicantsName(
+    firstname: string,
+    surname: string,
+  ): Promise<void> {
+    await this.fl401applicantFirstNameField.fill(firstname);
+    await this.fl401applicantLastNameField.fill(surname);
   }
 }
--- e2e/pageObjects/pages/exui/amendRespondentDetails/amendRespondentDetailsSubmit.po.ts
+++ e2e/pageObjects/pages/exui/amendRespondentDetails/amendRespondentDetailsSubmit.po.ts
@@ -2,7 +2,7 @@ import { Page } from "@playwright/test";
 import { CheckYourAnswersPage } from "../checkYourAnswers.po.ts";
 import { CommonStaticText } from "../../../../common/commonStaticText.ts";
 
-export class AmendApplicantDetailsSubmit extends CheckYourAnswersPage {
+export class AmendRespondentDetailsSubmit extends CheckYourAnswersPage {
   constructor(page: Page) {
     super(page, "Amend respondent details", CommonStaticText.saveAndContinue);
   }
