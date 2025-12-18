aaec78790f952ce222cb04ce7208270b1cba9f38
Praveen Thottempudi <50847630+pcthottempudi@users.noreply.github.com>
2025-12-18 17:41:09 +0000

    Qa/fpvtl 1618 miamchanges (#656)
    * fpvtl-1618
    
    * fpvtl-1618
    
    * removed comment
    
    ---------
    
    Co-authored-by: Luciano-Marsilio <107107435+lucianotmarsilio@users.noreply.github.com>

--- e2e/fixtures/manageCases/createCase/C100/miamPolicyUpgrade/miamPolicyUpgrade1Content.ts
+++ e2e/fixtures/manageCases/createCase/C100/miamPolicyUpgrade/miamPolicyUpgrade1Content.ts
@@ -1,7 +1,7 @@
 export enum MiamPolicyUpgrade1Content {
   pageTitle = "MIAM",
   p = "You can save and return to this page at any time. Questions marked with a * need to be completed before you can send your application.",
-  formLabel1 = "*Are the children involved in any emergency protection, care or supervision proceedings (or have they been)? (Optional)",
+  formLabel1 = "*Has any application been made for a care order, a supervision order, an emergency protection order or an order requiring someone to disclose where a child is or to deliver the child to another person and which: a) is still going on? or b) has finished but the order is still in place? (Optional)",
   additionalFormLabel1 = "*Has the applicant attended a Mediation Information & Assessment Meeting (MIAM)? (Optional)",
   additionalFormLabel2 = "*Is the applicant claiming exemption from the requirement to attend a MIAM ? (Optional)",
   formLabelYes = "Yes",
