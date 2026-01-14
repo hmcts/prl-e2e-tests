eaa848b7f33e99d1fef641b66c39baec88356129
RobL681 <robert.lacey@justice.gov.uk>
2026-01-14 13:44:58 +0000

    Move code into common methods

--- e2e/tests/manageCases/caseProgression/caseFlags/fl401CaseFlags.spec.ts
+++ e2e/tests/manageCases/caseProgression/caseFlags/fl401CaseFlags.spec.ts
@@ -1,11 +1,14 @@
 import Config from "../../../../utils/config.utils.ts";
 import config from "../../../../utils/config.utils.ts";
 import { test } from "../../../fixtures.ts";
+import { SolicitorPagesGroup } from "../../../../pageObjects/roleBasedGroupedPages/solicitorPages.js";
+import { ReasonableAdjustment } from "../../../../common/types.js";
+import { CaseWorkerPagesGroup } from "../../../../pageObjects/roleBasedGroupedPages/caseWorkerPages.js";
+import { NavigationUtils } from "../../../../utils/navigation.utils.js";
 
 test.use({ storageState: Config.sessionStoragePath + "solicitor.json" });
-test.slow();
 
-test.describe("Case flags tests for DA case tests.", () => {
+test.describe("Request Support for DA case tests.", () => {
   let caseNumber: string = "";
 
   test.beforeEach(
@@ -46,115 +49,142 @@ test.describe("Case flags tests for DA case tests.", () => {
         caseWorker,
         navigationUtils,
       }): Promise<void> => {
-        await solicitor.summaryPage.goToPage();
-        // TODO: why is the functionality around this so flaky
-        await solicitor.summaryPage.chooseEventFromDropdown("Request support");
-
-        // complete support request
-        await solicitor.caseFlags.requestSupport1Page.assertPageContents();
-        await solicitor.caseFlags.requestSupport1Page.verifyAccessibility();
-        await solicitor.caseFlags.requestSupport1Page.selectSupportRecipient(
+        // request support as Solicitor
+        await requestSupport(
+          solicitor,
           recipient,
-        );
-        await solicitor.caseFlags.requestSupport1Page.clickContinue();
-
-        await solicitor.caseFlags.requestSupport2Page.assertPageContents();
-        await solicitor.caseFlags.requestSupport2Page.verifyAccessibility();
-        await solicitor.caseFlags.requestSupport2Page.selectSupportType(
           supportType,
-        );
-        await solicitor.caseFlags.requestSupport2Page.clickContinue();
-
-        await solicitor.caseFlags.requestSupport3Page.assertPageContents();
-        await solicitor.caseFlags.requestSupport3Page.verifyAccessibility();
-        await solicitor.caseFlags.requestSupport3Page.selectReasonableAdjustment(
           reasonableAdjustment,
-        );
-        await solicitor.caseFlags.requestSupport3Page.clickContinue();
-
-        await solicitor.caseFlags.requestSupport4Page.assertPageContents(
-          reasonableAdjustment,
-        );
-        await solicitor.caseFlags.requestSupport4Page.verifyAccessibility();
-        await solicitor.caseFlags.requestSupport4Page.selectAdjustment(
-          adjustment,
-        );
-        await solicitor.caseFlags.requestSupport4Page.clickContinue();
-
-        await solicitor.caseFlags.requestSupport5Page.assertPageContents();
-        await solicitor.caseFlags.requestSupport5Page.verifyAccessibility();
-        await solicitor.caseFlags.requestSupport5Page.enterReason(reason);
-        await solicitor.caseFlags.requestSupport5Page.clickContinue();
-
-        await solicitor.caseFlags.requestSupportSubmitPage.assertPageContents();
-        await solicitor.caseFlags.requestSupportSubmitPage.verifyAccessibility();
-        await solicitor.caseFlags.requestSupportSubmitPage.clickSubmit();
-        await solicitor.summaryPage.alertBanner.assertEventAlert(
-          caseNumber,
-          "Request support",
-        );
-
-        // check support request in tab
-        await solicitor.supportPage.goToPage();
-        await solicitor.supportPage.caseFlagSection.assertCaseFlagPresent(
-          recipient,
           adjustment,
           reason,
+          caseNumber,
         );
 
         // activate support request as HCA
-        await navigationUtils.goToCase(
-          caseWorker.page,
-          config.manageCasesBaseURLCase,
+        await reviewSupportRequest(
+          caseWorker,
+          navigationUtils,
           caseNumber,
-        );
-        await caseWorker.tasksPage.goToPage();
-        await caseWorker.tasksPage.chooseEventFromDropdown("Review RA Request");
-
-        await caseWorker.caseFlags.reviewRARequestPage1.assertPageContents(
           recipient,
           recipientRole,
           supportType,
           adjustment,
           reason,
-        );
-        await caseWorker.caseFlags.reviewRARequestPage1.verifyAccessibility();
-        await caseWorker.caseFlags.reviewRARequestPage1.selectSupportRequest(
-          recipient,
-        );
-        await caseWorker.caseFlags.reviewRARequestPage1.clickContinue();
-
-        await caseWorker.caseFlags.reviewRARequestPage2.assertPageContents(
-          adjustment,
-        );
-        // await caseWorker.caseFlags.reviewRARequestPage2.verifyAccessibility(); // Need to add a ticket - seems to be failing
-        await caseWorker.caseFlags.reviewRARequestPage2.updateFlagStatus(
           newStatus,
-        );
-        await caseWorker.caseFlags.reviewRARequestPage2.addReasonForChange(
           changeReason,
-        );
-        await caseWorker.caseFlags.reviewRARequestPage2.clickContinue();
-
-        await caseWorker.caseFlags.reviewRARequestPageSubmit.assertPageContents();
-        await caseWorker.caseFlags.reviewRARequestPageSubmit.verifyAccessibility();
-        await caseWorker.caseFlags.reviewRARequestPageSubmit.clickSubmit();
-        await caseWorker.summaryPage.alertBanner.assertEventAlert(
-          caseNumber,
-          "Review RA Request",
-        );
-        await caseWorker.summaryPage.notificationBanner.assertNotificationBannerPresent(
-          1,
-        );
-        await caseWorker.summaryPage.notificationBanner.clickViewCaseFlags();
-        await caseWorker.caseFlagsPage.caseFlagSection.assertCaseFlagPresent(
-          recipient,
-          adjustment,
-          reason,
-          newStatus,
-          true,
         );
       });
     },
   );
 });
+
+async function requestSupport(
+  solicitor: SolicitorPagesGroup,
+  recipient: string,
+  supportType: string,
+  reasonableAdjustment: ReasonableAdjustment,
+  adjustment: string,
+  reason: string,
+  caseNumber: string,
+): Promise<void> {
+  // TODO: why is the functionality around this so flaky
+  const { caseFlags, summaryPage, supportPage } = solicitor;
+  await summaryPage.chooseEventFromDropdown("Request support");
+
+  // complete support request
+  await caseFlags.requestSupport1Page.assertPageContents();
+  await caseFlags.requestSupport1Page.verifyAccessibility();
+  await caseFlags.requestSupport1Page.selectSupportRecipient(recipient);
+  await caseFlags.requestSupport1Page.clickContinue();
+
+  await caseFlags.requestSupport2Page.assertPageContents();
+  await caseFlags.requestSupport2Page.verifyAccessibility();
+  await caseFlags.requestSupport2Page.selectSupportType(supportType);
+  await caseFlags.requestSupport2Page.clickContinue();
+
+  await caseFlags.requestSupport3Page.assertPageContents();
+  await caseFlags.requestSupport3Page.verifyAccessibility();
+  await caseFlags.requestSupport3Page.selectReasonableAdjustment(
+    reasonableAdjustment,
+  );
+  await caseFlags.requestSupport3Page.clickContinue();
+
+  await caseFlags.requestSupport4Page.assertPageContents(reasonableAdjustment);
+  await caseFlags.requestSupport4Page.verifyAccessibility();
+  await caseFlags.requestSupport4Page.selectAdjustment(adjustment);
+  await caseFlags.requestSupport4Page.clickContinue();
+
+  await caseFlags.requestSupport5Page.assertPageContents();
+  await caseFlags.requestSupport5Page.verifyAccessibility();
+  await caseFlags.requestSupport5Page.enterReason(reason);
+  await caseFlags.requestSupport5Page.clickContinue();
+
+  await caseFlags.requestSupportSubmitPage.assertPageContents();
+  await caseFlags.requestSupportSubmitPage.verifyAccessibility();
+  await caseFlags.requestSupportSubmitPage.clickSubmit();
+  await summaryPage.alertBanner.assertEventAlert(caseNumber, "Request support");
+
+  // check support request in tab
+  await supportPage.goToPage();
+  await supportPage.caseFlagSection.assertCaseFlagPresent(
+    recipient,
+    adjustment,
+    reason,
+  );
+}
+
+async function reviewSupportRequest(
+  caseWorker: CaseWorkerPagesGroup,
+  navigationUtils: NavigationUtils,
+  caseNumber: string,
+  recipient: string,
+  recipientRole: string,
+  supportType: string,
+  adjustment: string,
+  reason: string,
+  newStatus: string,
+  changeReason: string,
+): Promise<void> {
+  const { page, tasksPage, caseFlags, summaryPage, caseFlagsPage } = caseWorker;
+  await navigationUtils.goToCase(
+    page,
+    config.manageCasesBaseURLCase,
+    caseNumber,
+  );
+  await tasksPage.goToPage();
+  await tasksPage.chooseEventFromDropdown("Review RA Request");
+
+  await caseFlags.reviewRARequestPage1.assertPageContents(
+    recipient,
+    recipientRole,
+    supportType,
+    adjustment,
+    reason,
+  );
+  await caseFlags.reviewRARequestPage1.verifyAccessibility();
+  await caseFlags.reviewRARequestPage1.selectSupportRequest(recipient);
+  await caseFlags.reviewRARequestPage1.clickContinue();
+
+  await caseFlags.reviewRARequestPage2.assertPageContents(adjustment);
+  // await caseFlags.reviewRARequestPage2.verifyAccessibility(); // Need to add a ticket - seems to be failing
+  await caseFlags.reviewRARequestPage2.updateFlagStatus(newStatus);
+  await caseFlags.reviewRARequestPage2.addReasonForChange(changeReason);
+  await caseFlags.reviewRARequestPage2.clickContinue();
+
+  await caseFlags.reviewRARequestPageSubmit.assertPageContents();
+  await caseFlags.reviewRARequestPageSubmit.verifyAccessibility();
+  await caseFlags.reviewRARequestPageSubmit.clickSubmit();
+  await summaryPage.alertBanner.assertEventAlert(
+    caseNumber,
+    "Review RA Request",
+  );
+  await summaryPage.notificationBanner.assertNotificationBannerPresent(1);
+  await summaryPage.notificationBanner.clickViewCaseFlags();
+  await caseFlagsPage.caseFlagSection.assertCaseFlagPresent(
+    recipient,
+    adjustment,
+    reason,
+    newStatus,
+    true,
+  );
+}
