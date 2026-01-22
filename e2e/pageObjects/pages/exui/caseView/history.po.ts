5a1fd3a54eebcacb8553b8a85765dce70fc0efcc
Jahnavi <jahnavi1satya@gmail.com>
2026-01-22 16:06:24 +0000

    Update history.po.ts

--- e2e/pageObjects/pages/exui/caseView/history.po.ts
+++ e2e/pageObjects/pages/exui/caseView/history.po.ts
@@ -1,24 +1,35 @@
-import { expect, Page } from "@playwright/test";
-import { CaseAccessViewPage } from "./caseAccessView.po.js"; 
+import { CaseAccessViewPage } from "./caseAccessView.po.js";
+import { expect, Locator, Page } from "@playwright/test";
 
 export class HistoryPage extends CaseAccessViewPage {
+  readonly historyTab: Locator = this.page.getByRole("tab", {
+    name: "History",
+  });
+  readonly eventLogDetails: Locator = this.page
+    .locator("ccd-event-log-details, ccd-case-event-log-details")
+    .first();
+  readonly endStateRow: Locator = this.eventLogDetails
+    .locator("tr")
+    .filter({ hasText: "End state" });
+  readonly endStateValue: Locator = this.endStateRow.locator("td").last();
+
   constructor(page: Page) {
     super(page);
   }
 
   async goToPage(): Promise<void> {
-    await this.page.locator("mat-tab-header").getByText("History", { exact: true }).click();
-    await expect(this.page.locator("ccd-event-log-details")).toBeVisible();
+    await this.historyTab.click();
+    await expect(this.eventLogDetails).toBeVisible();
   }
 
   async openEvent(eventName: RegExp): Promise<void> {
     await this.page.getByRole("link", { name: eventName }).click();
+    await expect(this.eventLogDetails).toBeVisible();
+    await expect(this.endStateRow).toBeVisible(); 
   }
 
   async assertEndState(expected: RegExp): Promise<void> {
-    const endStateValue = this.page.locator(
-      "//ccd-event-log-details//tr[th[normalize-space()='End state']]/td"
-    );
-    await expect(endStateValue).toContainText(expected);
+    await expect(this.endStateValue).toBeVisible();
+    await expect(this.endStateValue).toContainText(expected);
   }
 }
