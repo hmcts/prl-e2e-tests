f4d41e63eb1a39684e9d13a7b936c547c652115e
RobL681 <robert.lacey@justice.gov.uk>
2026-02-03 08:14:14 +0000

    Add test comments to trigger tests

--- e2e/tests/manageCases/caseProgression/statementOfService/fl401StatementOfService.spec.ts
+++ e2e/tests/manageCases/caseProgression/statementOfService/fl401StatementOfService.spec.ts
@@ -5,6 +5,7 @@ import { StatementOfService } from "../../../../journeys/manageCases/caseProgres
 import { jsonDatas } from "../../../../common/caseHelpers/jsonDatas.ts";
 
 test.use({ storageState: config.sessionStoragePath + "caseWorker.json" });
+// TEST COMMENT
 
 test.describe("Statement of Service event for DA Solicitor case tests.", () => {
   let ccdRef: string = "";
