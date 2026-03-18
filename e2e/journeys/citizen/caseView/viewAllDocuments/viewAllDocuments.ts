6f536d29ae695b2a10cade6dfc9454b1e0b89540
RobL681 <robert.lacey@justice.gov.uk>
2026-03-18 14:15:50 +0000

    Update view all documents tests

--- e2e/journeys/citizen/caseView/viewAllDocuments/viewAllDocuments.ts
+++ e2e/journeys/citizen/caseView/viewAllDocuments/viewAllDocuments.ts
@@ -1,16 +1,11 @@
-import { ActivateCase, CaseUser } from "../../activateCase/activateCase.ts";
-import { Browser, Page } from "@playwright/test";
+import { Page } from "@playwright/test";
 import { AllCategoriesPage } from "../../../../pages/citizen/caseView/viewAllDocuments/allCatergoriesPage.ts";
 import { ApplicationPackDocumentsPage } from "../../../../pages/citizen/caseView/viewAllDocuments/applicationPackDocumentsPage.ts";
-import { applicationSubmittedBy } from "../../../../common/types.ts";
 
 interface reasonableAdjustmentsParams {
   page: Page;
-  browser: Browser;
-  caseRef: string;
-  isApplicant: boolean;
   accessibilityTest: boolean;
-  applicationSubmittedBy: applicationSubmittedBy;
+  isApplicant: boolean;
 }
 
 enum UniqueSelectors {
@@ -18,32 +13,19 @@ enum UniqueSelectors {
 }
 
 export class ViewAllDocuments {
-  public static async applicantViewAllDocuments({
+  public static async viewAllDocuments({
     page,
-    browser,
-    caseRef,
-    isApplicant,
     accessibilityTest,
-    applicationSubmittedBy,
+    isApplicant,
   }: reasonableAdjustmentsParams): Promise<void> {
-    const caseUser: CaseUser = isApplicant ? "applicant" : "respondent";
-    page = await ActivateCase.activateCase({
-      page: page,
-      browser: browser,
-      caseRef: caseRef,
-      caseUser: caseUser,
-      accessibilityTest: accessibilityTest,
-      applicationSubmittedBy: applicationSubmittedBy,
-      isManualSOA: true, // power of arrest order is not created properly through the API due to generated IDs
-    });
-
-    await page.click(UniqueSelectors.viewAllDocuments);
+    await page.locator(UniqueSelectors.viewAllDocuments).click();
     await AllCategoriesPage.allCategoriesPage({
       page,
       accessibilityTest,
     });
     await ApplicationPackDocumentsPage.applicationPackDocumentsPage({
       page,
+      isApplicant,
       accessibilityTest,
     });
   }
