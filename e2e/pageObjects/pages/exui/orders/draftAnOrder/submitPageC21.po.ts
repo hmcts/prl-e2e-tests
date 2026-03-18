0f8dac8fdead834c4eb32af9f717ec0cd962e952
abhik275 <abhinav.kumar@justice.gov.uk>
2026-03-18 13:37:05 +0000

    deleted older C21 solicitor upload journey

--- e2e/pageObjects/pages/exui/orders/draftAnOrder/submitPageC21.po.ts
+++
@@ -1,9 +0,0 @@
-import { Page } from "@playwright/test";
-import { CheckYourAnswersPage } from "../../checkYourAnswers.po.ts";
-import { CommonStaticText } from "../../../../../common/commonStaticText.ts";
-
-export class SubmitPageC21 extends CheckYourAnswersPage {
-  constructor(page: Page) {
-    super(page, "Create/upload draft order", CommonStaticText.submit);
-  }
-}
