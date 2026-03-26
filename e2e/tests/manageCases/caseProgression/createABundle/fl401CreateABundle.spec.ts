11b89d67c0ede9ce5d2b29bf4375257bc5c9819a
RobL681 <robert.lacey@justice.gov.uk>
2026-03-26 16:22:17 +0000

    Add comments to trigger tests

--- e2e/tests/manageCases/caseProgression/createABundle/fl401CreateABundle.spec.ts
+++ e2e/tests/manageCases/caseProgression/createABundle/fl401CreateABundle.spec.ts
@@ -6,7 +6,7 @@ import { CreateABundleJourney } from "../../../../journeys/manageCases/caseProgr
 import { jsonDatas } from "../../../../common/caseHelpers/jsonDatas.ts";
 
 test.use({ storageState: Config.sessionStoragePath + "caseWorker.json" });
-
+// TEST COMMENT
 test.describe("Complete the Order task for DA Solicitor case tests.", () => {
   let ccdRef: string = "";
 
