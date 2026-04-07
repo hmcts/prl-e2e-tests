7f1499ec22d88388d20f21d84803a15fd397fee0
RobL681 <robert.lacey@justice.gov.uk>
2026-04-07 10:22:21 +0100

    Add comments to trigger tests in PR pipeline

--- e2e/tests/manageCases/caseProgression/createHearingRequest/c100CreateHearingRequest.spec.ts
+++ e2e/tests/manageCases/caseProgression/createHearingRequest/c100CreateHearingRequest.spec.ts
@@ -4,7 +4,7 @@ import { Helpers } from "../../../../common/helpers.ts";
 import { CreateHearingRequest } from "../../../../journeys/manageCases/caseProgression/createHearingRequest/createHearingRequest.ts";
 
 test.use({ storageState: config.sessionStoragePath + "caseWorker.json" });
-
+// TEST COMMENT
 test.describe("Complete the Hearing Request Order task for CA Solicitor case tests.", () => {
   test.skip(
     process.env.MANAGE_CASES_TEST_ENV === "preview",
