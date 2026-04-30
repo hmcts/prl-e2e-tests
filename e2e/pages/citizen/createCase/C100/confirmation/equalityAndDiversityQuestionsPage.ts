979092e937a58587ef2bfba5eb0e2d75028ffe08
RobL681 <robert.lacey@justice.gov.uk>
2026-04-30 12:00:28 +0100

    Another attempt at fixing the flaky behaviour

--- e2e/pages/citizen/createCase/C100/confirmation/equalityAndDiversityQuestionsPage.ts
+++ e2e/pages/citizen/createCase/C100/confirmation/equalityAndDiversityQuestionsPage.ts
@@ -1,41 +1,34 @@
-import { expect, Page } from "@playwright/test";
-import { Selectors } from "../../../../../common/selectors.ts";
-
-enum content {
-  button = "I don't want to answer these questions",
-}
+import { Page } from "@playwright/test";
+import { CommonReviewContent } from "../../../../../fixtures/citizen/createCase/C100/reviewPages/commonReviewContent.js";
 
 interface EqualityAndDiversityPageOptions {
   page: Page;
+  c100YesNoNeedHelpWithFees: boolean;
 }
 
 export class EqualityAndDiversityPage {
   public static async equalityAndDiversityPage({
     page,
+    c100YesNoNeedHelpWithFees,
   }: EqualityAndDiversityPageOptions): Promise<void> {
-    // Sometimes this service seems to be flaky so refresh the page if it is down and then fail the test if still not working
-    await expect
-      .poll(
-        async () => {
-          const serviceProblemVisible = await page
-            .getByRole("heading", {
-              name: "Sorry, there is a problem with the service",
-            })
-            .isVisible();
+    // this service seems to be flaky
+    const serviceProblemVisible = await page
+      .getByRole("heading", {
+        name: "Sorry, there is a problem with the service",
+      })
+      .isVisible();
 
-          if (serviceProblemVisible) {
-            await page.reload();
-          }
-          return serviceProblemVisible;
-        },
-        {
-          // Retry every 5 seconds
-          intervals: [5_000],
-          // Timeout after 15 seconds
-          timeout: 15_000,
-        },
-      )
-      .toBeFalsy();
-    await page.click(`${Selectors.GovukButton}:text-is("${content.button}")`);
+    if (serviceProblemVisible) {
+      // if there is a problem with the service click back and try to resubmit
+      await page.getByRole("link", { name: "Back", exact: true }).click();
+      const submitButtonName: string = c100YesNoNeedHelpWithFees
+        ? CommonReviewContent.submitButton
+        : CommonReviewContent.submitButtonPay;
+      await page.getByRole("button", { name: submitButtonName }).click();
+    } else {
+      await page
+        .getByRole("button", { name: "I don't want to answer these questions" })
+        .click();
+    }
   }
 }
