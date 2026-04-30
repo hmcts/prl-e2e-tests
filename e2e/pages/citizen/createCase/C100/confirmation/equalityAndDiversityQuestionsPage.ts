a0d2d9357f382f7e2cf7983e4e7c0bedef72df6d
RobL681 <robert.lacey@justice.gov.uk>
2026-04-30 08:38:18 +0100

    Add a poll to the equality and diversity page because it seems this is sometimes flaky in tests

--- e2e/pages/citizen/createCase/C100/confirmation/equalityAndDiversityQuestionsPage.ts
+++ e2e/pages/citizen/createCase/C100/confirmation/equalityAndDiversityQuestionsPage.ts
@@ -1,4 +1,4 @@
-import { Page } from "@playwright/test";
+import { expect, Page } from "@playwright/test";
 import { Selectors } from "../../../../../common/selectors.ts";
 
 enum content {
@@ -13,6 +13,29 @@ export class EqualityAndDiversityPage {
   public static async equalityAndDiversityPage({
     page,
   }: EqualityAndDiversityPageOptions): Promise<void> {
+    // Sometimes this service seems to be flaky so refresh the page if it is down and then fail the test if still not working
+    await expect
+      .poll(
+        async () => {
+          const serviceProblemVisible = await page
+            .getByRole("heading", {
+              name: "Sorry, there is a problem with the service",
+            })
+            .isVisible();
+
+          if (serviceProblemVisible) {
+            await page.reload();
+          }
+          return serviceProblemVisible;
+        },
+        {
+          // Retry every 5 seconds
+          intervals: [5_000],
+          // Timeout after 15 seconds
+          timeout: 15_000,
+        },
+      )
+      .toBeFalsy();
     await page.click(`${Selectors.GovukButton}:text-is("${content.button}")`);
   }
 }
