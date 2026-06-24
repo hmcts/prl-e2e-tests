809aa24f661f35219328b07773140bcf38f0bf91
sunilrochani <sunil.rochani@hotmail.com>
2026-06-24 14:18:50 +0100

    FPVTL-1357: Accessibility issue - Add barrister journey

--- e2e/pageObjects/pages/exui/addAndRemoveBarrister/c100AdminAddBarristerSubmit.po.ts
+++ e2e/pageObjects/pages/exui/addAndRemoveBarrister/c100AdminAddBarristerSubmit.po.ts
@@ -1,9 +1,17 @@
-import { Page } from "@playwright/test";
+import { expect, Page } from "@playwright/test";
 import { CommonStaticText } from "../../../../common/commonStaticText.ts";
 import { CheckYourAnswersPage } from "../checkYourAnswers.po.ts";
 
 export class C100AdminAddBarristerSubmit extends CheckYourAnswersPage {
   constructor(page: Page) {
     super(page, "Add barrister", CommonStaticText.submit);
+  }
+  async verifyAccessibility(): Promise<void> {
+    await this.page
+      .locator("button", {
+        hasText: "Submit",
+      })
+      .waitFor();
+    await this.axeUtils.audit();
   }
 }
