370f4cf00dc719c605cf46beafe04f8243dab2a8
RobL681 <robert.lacey@justice.gov.uk>
2026-07-15 08:51:55 +0100

    Update names of buttons

--- e2e/fixtures/manageCases/caseProgression/createBundle/createBundle1Content.ts
+++ e2e/fixtures/manageCases/caseProgression/createBundle/createBundle1Content.ts
@@ -3,4 +3,5 @@ export enum CreateBundle1Content {
   h1 = "Please continue and select CreateBundle button in the next page for generating the bundle",
   h2 = "What happens next",
   p = "Please wait for sometime and refresh the page manually to see the generated bundle if StitchedDocument field is not populated in the bundles Tab after clicking on CreateBundle button in the next page",
+  createBundleSubmitButton = "Create Bundle",
 }
