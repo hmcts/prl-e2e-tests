97570398d6114679272e9fafadcefd49794bca96
RobL681 <robert.lacey@justice.gov.uk>
2026-07-16 16:06:08 +0100

    Update create hearing request tests

--- e2e/tests/manageCases/caseProgression/createHearingRequest/fl401CreateHearingRequest.spec.ts
+++ e2e/tests/manageCases/caseProgression/createHearingRequest/fl401CreateHearingRequest.spec.ts
@@ -1,49 +1,29 @@
 import { test } from "../../../fixtures.ts";
 import config from "../../../../utils/config.utils.ts";
-import { Helpers } from "../../../../common/helpers.ts";
 import { CreateHearingRequest } from "../../../../journeys/manageCases/caseProgression/createHearingRequest/createHearingRequest.ts";
-import { jsonDatas } from "../../../../common/caseHelpers/jsonDatas.ts";
 
 test.use({ storageState: config.sessionStoragePath + "caseWorker.json" });
 
 test.describe("Complete the Hearing Request Order task for DA Solicitor case tests.", () => {
-  let ccdRef: string = "";
+  let caseRef: string = "";
 
-  test.beforeEach(async ({ page, browser, caseEventUtils }) => {
-    ccdRef = await caseEventUtils.createDACase(browser);
-    await Helpers.goToCase(
+  test.beforeEach(async ({ page, manageCasesEventUtils, navigationUtils }) => {
+    caseRef = (await manageCasesEventUtils.submitTSSolicitorCase("FL401"))
+      .caseRef;
+    await manageCasesEventUtils.sendToGatekeeper(caseRef, "FL401");
+    await navigationUtils.goToCase(
       page,
       config.manageCasesBaseURLCase,
-      ccdRef,
-      "tasks",
+      caseRef,
     );
   });
 
-  test("Complete Task - Create Hearing Request - Blank order (FL404B) without accessibility test. @nightly @regression", async ({
+  test("Complete Task - Create Hearing Request with accessibility test. @nightly @accessibility @regression", async ({
     page,
-    browser,
-  }): Promise<void> => {
-    await CreateHearingRequest.FL401CreateHearingRequest({
-      page: page,
-      accessibilityTest: false,
-      ccdRef: ccdRef,
-      createOrderFL401Options: "amend discharge varied order",
-      browser: browser,
-      manageOrderData: jsonDatas.manageOrderDataAmendDischargedVaried,
-    });
-  });
-
-  test("Complete Task - Create Hearing Request - Amended, discharged or varied order (FL404B) with accessibility test. @regression @accessibility", async ({
-    page,
-    browser,
   }): Promise<void> => {
     await CreateHearingRequest.FL401CreateHearingRequest({
       page: page,
       accessibilityTest: true,
-      ccdRef: ccdRef,
-      createOrderFL401Options: "amend discharge varied order",
-      browser: browser,
-      manageOrderData: jsonDatas.manageOrderDataAmendDischargedVaried,
     });
   });
 });
