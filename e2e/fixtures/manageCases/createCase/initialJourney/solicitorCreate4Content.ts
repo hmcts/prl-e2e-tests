df21a854c4277cf032612409c5ba471e38815218
abhik275 <abhinav.kumar@justice.gov.uk>
2026-07-27 14:59:17 +0100

    missed .

--- e2e/fixtures/manageCases/createCase/initialJourney/solicitorCreate4Content.ts
+++ e2e/fixtures/manageCases/createCase/initialJourney/solicitorCreate4Content.ts
@@ -1,7 +1,7 @@
 export enum SolicitorCreate4Content {
   pageTitle = "Solicitor application",
   dummyPageTitle = "TS-Solicitor application",
-  p1 = "If the applicant has applied for Help with Fees, you should continue to process their application online",
+  p1 = "If the applicant has applied for Help with Fees, you should continue to process their application online.",
   p2 = "You will still have to pay the fee. You can apply for a refund by contacting the court and providing the applicant's Help with Fees reference.",
   formLabel1 = "Case Name",
   formHint1 = "Enter the eldest child’s full name. For example, John Smith",
