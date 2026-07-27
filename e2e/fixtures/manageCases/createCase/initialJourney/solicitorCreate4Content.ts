958b2e0afbdaa179566dada42c14fb7752ae6b97
abhik275 <abhinav.kumar@justice.gov.uk>
2026-07-27 13:38:46 +0100

    updated labels as per changes

--- e2e/fixtures/manageCases/createCase/initialJourney/solicitorCreate4Content.ts
+++ e2e/fixtures/manageCases/createCase/initialJourney/solicitorCreate4Content.ts
@@ -1,8 +1,8 @@
 export enum SolicitorCreate4Content {
   pageTitle = "Solicitor application",
   dummyPageTitle = "TS-Solicitor application",
-  p1 = "If the applicant has applied for Help with Fees, you should email or post this application to the court instead.",
-  p2 = "If you continue online, you will still have to pay the fee. You can apply for a refund by contacting the court and providing the applicant's Help with Fees reference.",
+  p1 = "If the applicant has applied for Help with Fees, you should continue to process their application online",
+  p2 = "You will still have to pay the fee. You can apply for a refund by contacting the court and providing the applicant's Help with Fees reference.",
   formLabel1 = "Case Name",
   formHint1 = "Enter the eldest child’s full name. For example, John Smith",
   continue = "Continue",
