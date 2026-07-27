161beadaa7d49d44fa9cd3656e15f0a8053f02b8
poojithanag <poojitha.nagappa2@hmcts.net>
2026-07-27 19:04:15 +0100

    FPVTL-2976 Amend child details refactor

--- e2e/tests/manageCases/caseProgression/amendDetails/amendChildDetails.spec.ts
+++ e2e/tests/manageCases/caseProgression/amendDetails/amendChildDetails.spec.ts
@@ -1,50 +1,42 @@
 import config from "../../../../utils/config.utils.ts";
 import { test } from "../../../fixtures.ts";
 
+test.use({ storageState: config.sessionStoragePath + "courtAdminStoke.json" });
+
 test.describe("Complete amend Child details event as a court admin", () => {
   let caseRef: string;
 
-  test.beforeEach(
-    async ({ solicitor, courtAdminStoke, caseEventUtils, navigationUtils }) => {
-      caseRef = await caseEventUtils.createCACaseSubmitAndPayIndividualEvents(
-        solicitor.page,
-      );
-      await navigationUtils.goToCase(
-        courtAdminStoke.page,
-        config.manageCasesBaseURLCase,
-        caseRef,
-      );
-    },
-  );
+  test.beforeEach(async ({ solicitor, courtAdminStoke, browser, caseEventUtils, navigationUtils }) => {
+    caseRef = await caseEventUtils.createCACaseSubmitAndPayIndividualEvents(
+      solicitor.page,
+    );
+    await navigationUtils.goToCase(
+      courtAdminStoke.page,
+      config.manageCasesBaseURLCase,
+      caseRef,
+    );
+  });
 
-  [
-    {
-      scenario: "male and under 18",
-      fillInFieldsOptions: {
-        c100ChildGender: "male" as const,
-        under18: true,
-      },
-      confirmOptions: { yesNoDontKnow: "yes" as const },
-    },
-  ].forEach(({ scenario, fillInFieldsOptions, confirmOptions }) => {
-    test(`Amend the following Child details: firstname, lastname, date of birth, gender - ${scenario} @regression`, async ({
-      courtAdminStoke,
-    }): Promise<void> => {
-      const { summaryPage, amendChildDetails } = courtAdminStoke;
+  test(`Amend the following Child details: firstname, lastname, date of birth, gender @regression`, async ({
+    courtAdminStoke,
+  }): Promise<void> => {
+    const { summaryPage, amendChildDetails } = courtAdminStoke;
 
-      await summaryPage.chooseEventFromDropdown("Amend Child details");
+    await summaryPage.chooseEventFromDropdown("Amend Child details");
 
-      await amendChildDetails.page1.assertPageContents();
-      await amendChildDetails.page1.fillInFields(fillInFieldsOptions);
-
-      await amendChildDetails.page2.assertPageContents();
-      await amendChildDetails.page2.fillInFields(confirmOptions);
-
-      await amendChildDetails.submitPage.assertPageContents(
-        ["caseProgression", "amendDetails", "amendChildDetails"],
-        "amend-child-details",
-      );
-      await amendChildDetails.submitPage.clickSaveAndContinue();
+    await amendChildDetails.page1.assertPageContents();
+    await amendChildDetails.page1.fillInFields({
+      c100ChildGender: "male",
+      under18: true,
     });
+
+    await amendChildDetails.page2.assertPageContents();
+    await amendChildDetails.page2.fillInFields({ yesNoDontKnow: "yes" });
+
+    await amendChildDetails.submitPage.assertPageContents(
+      ["caseProgression", "amendDetails", "amendChildDetails"],
+      "amend-child-details",
+    );
+    await amendChildDetails.submitPage.clickSaveAndContinue();
   });
 });
