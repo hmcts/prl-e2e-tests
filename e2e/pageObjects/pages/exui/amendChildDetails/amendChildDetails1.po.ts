4da742813209ad6bfb87ec8e9a424d33741f65f3
poojithanag <poojitha.nagappa2@hmcts.net>
2026-07-31 14:49:11 +0100

    FPVTL-2976
    Fixing lint issues

--- e2e/pageObjects/pages/exui/amendChildDetails/amendChildDetails1.po.ts
+++ e2e/pageObjects/pages/exui/amendChildDetails/amendChildDetails1.po.ts
@@ -18,11 +18,15 @@ export class AmendChildDetails1Page extends EventPage {
   private readonly lastNameField: Locator = this.page.locator(
     "#newChildDetails_0_lastName",
   );
-  private readonly dobDayField: Locator = this.page.locator("#dateOfBirth-day").first();
-  private readonly dobMonthField: Locator = this.page.locator(
-    "#dateOfBirth-month").first();
-;
-  private readonly dobYearField: Locator = this.page.locator("#dateOfBirth-year").first();
+  private readonly dobDayField: Locator = this.page
+    .locator("#dateOfBirth-day")
+    .first();
+  private readonly dobMonthField: Locator = this.page
+    .locator("#dateOfBirth-month")
+    .first();
+  private readonly dobYearField: Locator = this.page
+    .locator("#dateOfBirth-year")
+    .first();
   private readonly genderRadio = (gender: C100ChildGender) =>
     this.page.locator(`#newChildDetails_0_gender-${gender}`);
   private readonly otherGenderField: Locator = this.page.locator(
@@ -51,39 +55,57 @@ export class AmendChildDetails1Page extends EventPage {
   async assertPageContents(): Promise<void> {
     await this.assertPageHeadings();
     await expect(
-        this.page.locator(Selectors.p, {
+      this.page.locator(Selectors.p, {
         hasText: AmendChildDetailsRevised1Content.p,
-    }),
+      }),
     ).toBeVisible();
 
     await expect(
-        this.page.locator(Selectors.h3, { hasText: AmendChildDetailsRevised1Content.h3AddNewChild }).first() // Isolates the first heading globally
+      this.page
+        .locator(Selectors.h3, {
+          hasText: AmendChildDetailsRevised1Content.h3AddNewChild,
+        })
+        .first(), // Isolates the first heading globally
     ).toBeVisible();
 
     await expect(
-        this.page.locator(Selectors.GovukFormLabel, { hasText: AmendChildDetailsRevised1Content.formLabelFirstName }).first()
+      this.page
+        .locator(Selectors.GovukFormLabel, {
+          hasText: AmendChildDetailsRevised1Content.formLabelFirstName,
+        })
+        .first(),
     ).toBeVisible();
     await expect(
-        this.page.locator(Selectors.GovukFormLabel, {
-        hasText: AmendChildDetailsRevised1Content.formLabelLastName,
-    }).first()
+      this.page
+        .locator(Selectors.GovukFormLabel, {
+          hasText: AmendChildDetailsRevised1Content.formLabelLastName,
+        })
+        .first(),
     ).toBeVisible();
     await expect(
-        this.page.locator(Selectors.GovukFormLabel, {
-        hasText: AmendChildDetailsRevised1Content.formLabelDOB,
-    }).first()
+      this.page
+        .locator(Selectors.GovukFormLabel, {
+          hasText: AmendChildDetailsRevised1Content.formLabelDOB,
+        })
+        .first(),
     ).toBeVisible();
     await expect(
-      this.page.locator(Selectors.GovukFormLabel, {
-        hasText: AmendChildDetailsRevised1Content.formLabelGender,
-      }).first()
+      this.page
+        .locator(Selectors.GovukFormLabel, {
+          hasText: AmendChildDetailsRevised1Content.formLabelGender,
+        })
+        .first(),
     ).toBeVisible();
   }
 
   async fillInFields({ c100ChildGender, under18 }: FillInFieldsOptions) {
     const [day, month, year] = Helpers.generateDOB(under18);
-    await this.firstNameField.fill(AmendChildDetailsRevised1Content.childFirstName);
-    await this.lastNameField.fill(AmendChildDetailsRevised1Content.childLastName);
+    await this.firstNameField.fill(
+      AmendChildDetailsRevised1Content.childFirstName,
+    );
+    await this.lastNameField.fill(
+      AmendChildDetailsRevised1Content.childLastName,
+    );
     await this.dobDayField.fill(day);
     await this.dobMonthField.fill(month);
     await this.dobYearField.fill(year);
