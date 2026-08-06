71b46a69bbf230e1ce9f7a181f8032ca0bc7e286
RobL681 <robert.lacey@justice.gov.uk>
2026-08-06 16:06:48 +0100

    Remove dummy comment

--- e2e/tests/manageCases/createCase/C100/c100ViewPDFApplication.spec.ts
+++ e2e/tests/manageCases/createCase/C100/c100ViewPDFApplication.spec.ts
@@ -2,7 +2,6 @@ import { test } from "../../../fixtures.ts";
 import config from "../../../../utils/config.utils.ts";
 import { C100ViewPDFApplication } from "../../../../journeys/manageCases/createCase/C100ViewPDFApplication/c100ViewPDFApplication.ts";
 
-// dummy comment
 test.describe("C100 view PDF application tests", (): void => {
   let caseRef: string;
 
