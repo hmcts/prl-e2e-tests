c4095eac8a815d6727315e2ffa72c11c25e07f56
RobL681 <robert.lacey@justice.gov.uk>
2026-08-13 15:48:35 +0100

    Update case flags label

--- e2e/fixtures/manageCases/caseProgression/caseFlags/manageFlagsUpdateCaseFlagContent.ts
+++ e2e/fixtures/manageCases/caseProgression/caseFlags/manageFlagsUpdateCaseFlagContent.ts
@@ -2,8 +2,8 @@ export enum ManageFlagsUpdateCaseFlagContent {
   govUkHeadingL = "Review RA Request",
   govUkLabelMReasonableAdjustment = 'Update flag "Documents in a specified colour"',
   govUkLabelMLanguageInterpreter = 'Update flag "Language Interpreter, Korean"',
-  govUkLabelSReasonableAdjustment = 'Update flag "Documents in a specified colour" comments',
-  govUkLabelSLanguageInterpreter = 'Update flag "Language Interpreter, Korean" comments',
+  govUkLabelSReasonableAdjustment = 'Update flag "Documents in a specified colour"comments',
+  govUkLabelSLanguageInterpreter = 'Update flag "Language Interpreter, Korean"comments',
   govUkHint1 = "Update the comments describing the user's support needs or flag description. Do not include any sensitive information such as personal details.",
   govUkWarningText = "The details entered here MAY be visible to the party in the future.",
   govUkHint2 = "You can enter up to 200 characters", // x2
