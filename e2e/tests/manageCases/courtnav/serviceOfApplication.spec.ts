e79c6d8ca69ddabf60f31d1105b7f22e3918d6d5
RobL681 <robert.lacey@justice.gov.uk>
2026-09-18 15:25:05 +0100

    Uncomment

--- e2e/tests/manageCases/courtnav/serviceOfApplication.spec.ts
+++ e2e/tests/manageCases/courtnav/serviceOfApplication.spec.ts
@@ -17,11 +17,11 @@ const scenarios: ServiceOfApplicationScenario[] = [
     serviceOptions: { personallyServed: "yes", servedBy: "courtBailiff" },
     snapshotName: "courtnav-fl406-personally-served-by-court-bailiff",
   },
-  // {
-  //   orderType: "Amended, discharged or varied order (FL404B)",
-  //   serviceOptions: { personallyServed: "no" },
-  //   snapshotName: "courtnav-fl404b-not-personally-served",
-  // },
+  {
+    orderType: "Amended, discharged or varied order (FL404B)",
+    serviceOptions: { personallyServed: "no" },
+    snapshotName: "courtnav-fl404b-not-personally-served",
+  },
 ];
 
 test.describe("Service of Application task for DA Citizen case tests.", () => {
